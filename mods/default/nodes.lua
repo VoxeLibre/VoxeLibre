082706441a7a87d0bbab953f604ba2357b9312f8
Wuzzy <almikes@aol.com>
2017-01-04 13:03:41 +0100

    Fix bookshelf drop

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -780,7 +780,7 @@ minetest.register_node("default:bookshelf", {
 	tiles = {"default_wood.png", "default_wood.png", "default_bookshelf.png"},
 	stack_max = 64,
 	groups = {choppy=3,oddly_breakable_by_hand=2,flammable=3},
-	drop = {"default:book 3"},
+	drop = "default:book 3",
 	sounds = default.node_sound_wood_defaults(),
 })
 
