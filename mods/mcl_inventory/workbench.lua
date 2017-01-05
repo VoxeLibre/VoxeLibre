ed25434c34ec8c538edb1d607d52060eb4f04f6a
Wuzzy <almikes@aol.com>
2017-01-05 16:02:07 +0100

    Rename inventory to mcl_inventory

--- mods/mcl_inventory/workbench.lua
+++ mods/mcl_inventory/workbench.lua
@@ -1,7 +1,7 @@
-minetest.register_node("inventory:crafting_table", {
+minetest.register_node("mcl_inventory:crafting_table", {
 	description = "Crafting Table",
-	tiles = {"inventory_crafting_table_top.png", "default_wood.png", "inventory_crafting_table_side.png",
-		"inventory_crafting_table_side.png", "inventory_crafting_table_front.png", "inventory_crafting_table_front.png"},
+	tiles = {"mcl_inventory_crafting_table_top.png", "default_wood.png", "mcl_inventory_crafting_table_side.png",
+		"mcl_inventory_crafting_table_side.png", "mcl_inventory_crafting_table_front.png", "mcl_inventory_crafting_table_front.png"},
 	paramtype2 = "facedir",
 	paramtype = "light",
 	is_ground_content = false,
@@ -12,12 +12,12 @@ minetest.register_node("inventory:crafting_table", {
 		clicker:get_inventory():set_size("craft", 9)
 		clicker:get_inventory():set_width("main", 9)
 		clicker:get_inventory():set_size("main", 36)
-		minetest.show_formspec(clicker:get_player_name(), "inventory:craftin_table", CRAFTING_FORMSPEC)
+		minetest.show_formspec(clicker:get_player_name(), "mcl_inventory:crafting_table", CRAFTING_FORMSPEC)
 	end,
 })
 
 minetest.register_craft({
-	output = "inventory:crafting_table",
+	output = "mcl_inventory:crafting_table",
 	recipe = {
 		{"group:wood", "group:wood"},
 		{"group:wood", "group:wood"},
