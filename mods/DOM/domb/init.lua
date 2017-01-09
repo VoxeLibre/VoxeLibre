2e732c908589390f3501af51b1683a07091f90a0
Wuzzy <almikes@aol.com>
2017-01-09 03:02:23 +0100

    Refactor watch mod to mcl_clock

--- mods/DOM/domb/init.lua
+++
@@ -1,6 +0,0 @@
-domb = {}
-
---DOM base system library
-dofile(minetest.get_modpath("domb").."/domb.lua")
-
-DOM_registra_comandos_de_uso_geral()
---
+++ mods/mcl_clock/init.lua
@@ -0,0 +1,8 @@
+--[[
+  mcl_clock, renew of the renew of the watch mod
+
+  Original from Echo, here: http://forum.minetest.net/viewtopic.php?id=3795
+]]--
+
+dofile(minetest.get_modpath("mcl_clock").."/clock.lua")
+dofile(minetest.get_modpath("mcl_clock").."/items.lua")
