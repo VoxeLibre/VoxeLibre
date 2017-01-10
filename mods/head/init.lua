0dd42ebda3d9861aee951bb18973a996c122db57
Wuzzy <almikes@aol.com>
2017-01-10 05:35:44 +0100

    Refactor head mod and add skeleton heads

--- mods/head/init.lua
+++ mods/head/init.lua
@@ -1,35 +1,37 @@
 -- head system
 
-function addhead(node, desc)
+local function addhead(node, desc)
 	minetest.register_node("head:"..node, {
-			description = ""..desc,
-	    		drawtype = "nodebox",
-			is_ground_content = false,
-			node_box = {
-				type = "fixed",
-				fixed = {       
-					{ -0.25, -0.5, -0.25, 0.25, 0.0, 0.25, },   			
-				},
+		description = ""..desc,
+    		drawtype = "nodebox",
+		is_ground_content = false,
+		node_box = {
+			type = "fixed",
+			fixed = {       
+				{ -0.25, -0.5, -0.25, 0.25, 0.0, 0.25, },   			
 			},
-			groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,head=1},
-			tiles = {
-				node.."_top.png",
-				node.."_top.png",
-				node.."_left.png",
-				node.."_right.png",
-				node.."_back.png",
-				node.."_face.png",
-			},	    
-			paramtype = "light",
-			stack_max = 16,
-			paramtype2 = "facedir",
-			sunlight_propagates = true,
-			walkable = true,
-			selection_box = {
-				type = "fixed",
-				fixed = { -0.25, -0.5, -0.25, 0.25, 0.0, 0.25, },
-			},
-			
+		},
+		groups = {oddly_breakable_by_hand=3, head=1},
+		tiles = {
+			"head_"..node.."_top.png",
+			"head_"..node.."_top.png",
+			"head_"..node.."_left.png",
+			"head_"..node.."_right.png",
+			"head_"..node.."_back.png",
+			"head_"..node.."_face.png",
+		},	    
+		paramtype = "light",
+		stack_max = 16,
+		paramtype2 = "facedir",
+		sunlight_propagates = true,
+		walkable = true,
+		selection_box = {
+			type = "fixed",
+			fixed = { -0.25, -0.5, -0.25, 0.25, 0.0, 0.25, },
+		},
+		sounds = default.node_sound_defaults({
+			footstep = {name="default_hard_footstep", gain=0.3}
+		}),
 	})
 end
 
@@ -37,3 +39,5 @@ end
 addhead("zombie", "Zombie Head")
 addhead("creeper", "Creeper Head")
 addhead("steve", "Steve Head")
+addhead("skeleton", "Skeleton Skull")
+addhead("wither_skeleton", "Wither Skeleton Skull")
