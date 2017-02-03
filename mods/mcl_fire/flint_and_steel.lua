708217b57d36fddefc40456ee8b19ed9b41a9d67
Wuzzy <almikes@aol.com>
2017-02-03 02:20:47 +0100

    Fix bad tool definition of flint and steel

--- mods/mcl_fire/flint_and_steel.lua
+++ mods/mcl_fire/flint_and_steel.lua
@@ -5,24 +5,17 @@ minetest.register_tool("mcl_fire:flint_and_steel", {
 	liquids_pointable = false,
 	stack_max = 1,
 	groups = { tool = 1 },
-	tool_capabilities = {
-		full_punch_interval = 1.0,
-		max_drop_level=0,
-		groupcaps={
-			flamable = {uses=65, maxlevel=1},
-		}
-	},
 	on_use = function(itemstack, user, pointed_thing)
 		if pointed_thing.type == "node" then
 			if minetest.get_node(pointed_thing.under).name == "mcl_tnt:tnt" then
 				tnt.ignite(pointed_thing.under)
 				if not minetest.setting_getbool("creative_mode") then
-					itemstack:add_wear(66000/65) -- 65 uses
+					itemstack:add_wear(65535/65) -- 65 uses
 				end
 			else
 				mcl_fire.set_fire(pointed_thing)
 				if not minetest.setting_getbool("creative_mode") then
-					itemstack:add_wear(66000/65) -- 65 uses
+					itemstack:add_wear(65535/65) -- 65 uses
 				end
 			end
 		end
