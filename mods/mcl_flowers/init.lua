65fea54e1e1194295e44ce26b85ed8f0ef522fb7
Wuzzy <almikes@aol.com>
2017-02-10 21:15:36 +0100

    Remove the old horribly broken flower pot

--- mods/mcl_flowers/init.lua
+++ mods/mcl_flowers/init.lua
@@ -6,7 +6,6 @@ flower_tmp={}
 
 -- Map Generation
 dofile(minetest.get_modpath("mcl_flowers").."/mapgen.lua")
-dofile(minetest.get_modpath("mcl_flowers").."/func.lua")
 
 
 
@@ -275,69 +274,6 @@ function register_large(name, desc, inv_img, bot_img, colr) --change in function
     })
 end
 
-
---
--- Flower Pot
---
-
-minetest.register_node("mcl_flowers:pot",{
-	description = "Flower Pot",
-	drawtype = "nodebox",
-	is_ground_content = false,
-	node_box = { type = "fixed", fixed = {
-		{-0.125,-0.125,-0.187500,-0.187500,-0.500,0.1875}, --Wall 1
-		{0.1875,-0.125,-0.125,0.125,-0.500,0.1875}, --Wall 2
-		{-0.1875,-0.125,-0.125,0.1875,-0.500,-0.1875}, --Wall 3
-		{0.1875,-0.125,0.125,-0.1875,-0.500,0.1875}, --Wall 4
-		{-0.125,-0.500,-0.125,0.125,-0.250,0.125}, --Dirt 5
-	}},
-	selection_box = { type = "fixed", fixed = {-0.125,-0.5,-0.125,0.125,-0.25,0.125 }},
-	tiles = {"flowers_pot_top.png", "flowers_pot_bottom.png", "flowers_pot_top.png"},
-	inventory_image="flowers_pot_inventory.png",
-	paramtype = "light",
-	groups = {dig_immediate=3,deco_block=1},
-	stack_max = 64,
-	sounds = mcl_core.node_sound_defaults(),
-	after_place_node = function(pos, placer, itemstack)
-		local meta = minetest.get_meta(pos)
-		meta:set_string("owner",placer:get_player_name())
-	end,
-	on_rightclick = function(pos, node, clicker, itemstack)
-		if not itemstack then return end
-		local meta = minetest.get_meta(pos)
-		if clicker:get_player_name() == meta:get_string("owner") then
-			flower_pot_drop_item(pos,node)
-			local s = itemstack:take_item()
-			meta:set_string("item",s:to_string())
-			flower_pot_update_item(pos,node)
-		end
-		return itemstack
-	end,
-	on_punch = function(pos,node,puncher)
-		local meta = minetest.get_meta(pos)
-		if puncher:get_player_name() == meta:get_string("owner") then
-			flower_pot_drop_item(pos,node)
-		end
-	end,
-	can_dig = function(pos,player)
-		local meta = minetest.get_meta(pos)
-		return player:get_player_name() == meta:get_string("owner")
-	end,
-	on_destruct = function(pos)
-		local node = minetest.get_node(pos)
-		flower_pot_drop_item(pos,node)
-		minetest.add_node(pos, {name="air"})
-		minetest.add_item(pos, "mcl_flowers:pot")
-	end,
-})
-
-minetest.register_craft({
-	output = "mcl_flowers:pot",
-	recipe = {
-		{ "mcl_core:brick", "", "mcl_core:brick", },
-		{ "", "mcl_core:brick", "" },
-	},
-})
 
 -- Lily Pad
 minetest.register_node("mcl_flowers:waterlily", {
