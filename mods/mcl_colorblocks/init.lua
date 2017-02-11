6f31f3e815688901f342c4e22f2cde0c1a203eae
Wuzzy <almikes@aol.com>
2017-02-11 23:33:06 +0100

    Add concrete and concrete powder blocks
    From Minecraft 17w06a

--- mods/mcl_colorblocks/init.lua
+++ mods/mcl_colorblocks/init.lua
@@ -41,24 +41,54 @@ for _, row in ipairs(block.dyes) do
 	local desc = row[2]
 	local craft_color_group = row[3]
 	-- Node Definition
-		minetest.register_node("mcl_colorblocks:hardened_clay_"..name, {
-			description = desc.." Hardened Clay",
-			tiles = {"hardened_clay_stained_"..name..".png"},
-			groups = {cracky=3,hardened_clay=1,building_block=1},
-			stack_max = 64,
-			sounds = mcl_sounds.node_sound_stone_defaults(),
-		})
+	minetest.register_node("mcl_colorblocks:hardened_clay_"..name, {
+		description = desc.." Hardened Clay",
+		tiles = {"hardened_clay_stained_"..name..".png"},
+		groups = {cracky=3,hardened_clay=1,building_block=1},
+		stack_max = 64,
+		sounds = mcl_sounds.node_sound_stone_defaults(),
+	})
+
+	minetest.register_node("mcl_colorblocks:concrete_powder_"..name, {
+		description = desc.." Concrete Powder",
+		tiles = {"mcl_colorblocks_concrete_powder_"..name..".png"},
+		groups = {crumbly=3,concrete_powder=1,building_block=1,falling_node=1},
+		stack_max = 64,
+		is_ground_content = false,
+		sounds = mcl_sounds.node_sound_sand_defaults(),
+	})
+
+	minetest.register_node("mcl_colorblocks:concrete_"..name, {
+		description = desc.." Concrete",
+		tiles = {"mcl_colorblocks_concrete_"..name..".png"},
+		groups = {cracky=3,conrete=1,building_block=1},
+		stack_max = 64,
+		is_ground_content = false,
+		sounds = mcl_sounds.node_sound_stone_defaults(),
+	})
+
+	-- Crafting recipes
 	if craft_color_group then
 		minetest.register_craft({
-			output = 'mcl_colorblocks:'..name..' 8',
+			output = 'mcl_colorblocks:hardened_clay_'..name..' 8',
 			recipe = {
 					{'mcl_colorblocks:hardened_clay', 'mcl_colorblocks:hardened_clay', 'mcl_colorblocks:hardened_clay'},
 					{'mcl_colorblocks:hardened_clay', 'mcl_dye:'..craft_color_group, 'mcl_colorblocks:hardened_clay'},
 					{'mcl_colorblocks:hardened_clay', 'mcl_colorblocks:hardened_clay', 'mcl_colorblocks:hardened_clay'},
 			},
 		})
+		minetest.register_craft({
+			output = 'mcl_colorblocks:concrete_powder_'..name..' 8',
+			recipe = {
+					{'mcl_core:sand', 'mcl_core:gravel', 'mcl_core:sand'},
+					{'mcl_core:gravel', 'mcl_dye:'..craft_color_group, 'mcl_core:gravel'},
+					{'mcl_core:sand', 'mcl_core:gravel', 'mcl_core:sand'},
+			},
+		})
 	end
 end
+
+-- TODO: ABM: Concrete Powder + Water = Concrete
 
 local time_to_load= os.clock() - init
 print(string.format("[MOD] "..minetest.get_current_modname().." loaded in %.4f s", time_to_load))
