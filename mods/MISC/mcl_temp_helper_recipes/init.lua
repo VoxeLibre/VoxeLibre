f7eecbcf27c9beac0148d5af36a2efa13c0e2b42
Wuzzy <almikes@aol.com>
2017-02-20 07:37:37 +0100

    Remove some temporary crafting recipes, not needed

--- mods/MISC/mcl_temp_helper_recipes/init.lua
+++ mods/MISC/mcl_temp_helper_recipes/init.lua
@@ -3,15 +3,6 @@
 -- TODO: Remove recipes when they become accessible by regular means
 
 minetest.register_craft({
-	type = "shapeless",
-	output = "mcl_mobitems:gunpowder",
-	recipe = {
-		'group:sand',
-		'mcl_core:gravel',
-	}
-})
-
-minetest.register_craft({
 	output = "mcl_sponges:sponge",
 	recipe = {
 		{ "mcl_farming:hay_block", "mcl_farming:hay_block", "mcl_farming:hay_block" },
@@ -46,21 +37,6 @@ minetest.register_craft({
 	output = "mcl_dye:black 2",
 	recipe = {{"mcl_core:coal_lump"}},
 })
-minetest.register_craft({
-	output = "mcl_dye:brown",
-	recipe = {
-		{"mcl_core:jungletree"},
-		{"mcl_core:jungletree"}
-	},
-})
-minetest.register_craft({
-	output = "mcl_dye:white",
-	recipe = {
-		{"mcl_core:dirt", "mcl_core:dirt", "mcl_core:dirt"},
-		{"mcl_core:dirt", "mcl_core:iron_nugget", "mcl_core:dirt"},
-		{"mcl_core:dirt", "mcl_core:dirt", "mcl_core:dirt"},
-	},
-})
 
 minetest.register_craft({
 	output = "mcl_end:chorus_fruit",
@@ -87,25 +63,6 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = "mcl_mobitems:string",
-	recipe = {
-		{ "mcl_core:reeds"},
-		{ "mcl_core:reeds"}
-	},
-})
-minetest.register_craft({
-	type = "shapeless",
-	output = "mcl_mobitems:leather",
-	recipe = { "mcl_core:paper", "mcl_core:paper" },
-})
-minetest.register_craft({
-	output = "mcl_mobitems:feather 3",
-	recipe = {
-		{ "mcl_flowers:oxeye_daisy" },
-		{ "mcl_flowers:oxeye_daisy" },
-	}
-})
-minetest.register_craft({
 	output = "mcl_mobitems:blaze_rod",
 	recipe = {
 		 { "mcl_fire:flint_and_steel", "mcl_fire:flint_and_steel", "mcl_fire:flint_and_steel"},
@@ -120,13 +77,6 @@ minetest.register_craft({
 		 { "mcl_end:purpur_block", "mcl_core:emerald", "mcl_end:purpur_block", },
 		 { "mcl_end:purpur_block", "", "mcl_end:purpur_block", },
 	}
-})
-
-minetest.register_craft({
-	output = 'mcl_mobitems:slimeball',
-	type = "cooking",
-	recipe = "mcl_core:sapling",
-	cooktime = 10,
 })
 
 minetest.register_craft({
