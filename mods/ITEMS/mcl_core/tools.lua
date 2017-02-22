e94b941dd8ce7318462b7e7cf61c2228d224e57c
Wuzzy <almikes@aol.com>
2017-02-22 20:10:06 +0100

    Slightly reduce pointing range

--- mods/ITEMS/mcl_core/tools.lua
+++ mods/ITEMS/mcl_core/tools.lua
@@ -25,6 +25,7 @@ minetest.register_item(":", {
 	type = "none",
 	wield_image = "wieldhand.png",
 	wield_scale = {x=1,y=1,z=2.5},
+	range = 3.975,
 	tool_capabilities = {
 		full_punch_interval = 0.25,
 		max_drop_level = 0,
