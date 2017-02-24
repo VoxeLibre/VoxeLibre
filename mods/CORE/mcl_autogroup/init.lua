b5dcd46193cd84d06a9410a12a258c96bb531f93
Wuzzy <almikes@aol.com>
2017-02-24 02:51:50 +0100

    Remove ignore node from solid node group
    So mobs don't spawn on ignore...  xD

--- mods/CORE/mcl_autogroup/init.lua
+++ mods/CORE/mcl_autogroup/init.lua
@@ -1,7 +1,8 @@
 -- Automatically assign the “solid” group for solid nodes
 local overwrite = function()
 	for nname, ndef in pairs(minetest.registered_nodes) do
-		if (ndef.walkable == nil or ndef.walkable == true)
+		if (nname ~= "ignore")
+				and (ndef.walkable == nil or ndef.walkable == true)
 				and (ndef.collision_box == nil or ndef.collision_box.type == "regular")
 				and (ndef.node_box == nil or ndef.node_box.type == "regular")
 				and (ndef.groups.falling_node == 0 or ndef.groups.falling_node == nil)
