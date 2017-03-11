5b7e969a71b76c0d5fd9f933b92636ebce46cc91
Wuzzy <almikes@aol.com>
2017-03-11 07:15:39 +0100

    Remove unused mesecons_extrawires mod

--- mods/ITEMS/REDSTONE/mesecons_extrawires/init.lua
+++
@@ -1,3 +0,0 @@
--- dofile(minetest.get_modpath("mesecons_extrawires").."/crossing.lua");
--- The crossing code is not active right now because it is hard to maintain
-dofile(minetest.get_modpath("mesecons_extrawires").."/mesewire.lua");
