e0856fbdfcf4e31687153606adb44a8d7ab0ca97
Wuzzy <almikes@aol.com>
2017-03-11 23:36:38 +0100

    Make redstone repeaters attached nodes

--- mods/ITEMS/REDSTONE/mesecons_delayer/init.lua
+++ mods/ITEMS/REDSTONE/mesecons_delayer/init.lua
@@ -46,9 +46,9 @@ end
 for i = 1, 4 do
 local groups = {}
 if i == 1 then 
-	groups = {dig_immediate=3,dig_by_water=1}
+	groups = {dig_immediate=3,dig_by_water=1,attached_node=1}
 else
-	groups = {dig_immediate=3,dig_by_water=1, not_in_creative_inventory=1}
+	groups = {dig_immediate=3,dig_by_water=1,attached_node=1, not_in_creative_inventory=1}
 end
 
 local delaytime
@@ -172,7 +172,7 @@ minetest.register_node("mesecons_delayer:delayer_on_"..tostring(i), {
 		type = "fixed",
 		fixed = boxes
 	},
-	groups = {dig_immediate = 3, dig_by_water=1, not_in_creative_inventory = 1},
+	groups = {dig_immediate = 3, dig_by_water=1, attached_node=1, not_in_creative_inventory = 1},
 	paramtype = "light",
 	paramtype2 = "facedir",
 	sunlight_propagates = true,
