bbfa8d95749a20a7c27f0d5f6c82db144c3fec22
Wuzzy <almikes@aol.com>
2017-03-15 17:58:13 +0100

    Carpets are now attached nodes

--- mods/ITEMS/mcl_wool/init.lua
+++ mods/ITEMS/mcl_wool/init.lua
@@ -52,7 +52,7 @@ for _, row in ipairs(wool.dyes) do
 			tiles = {"wool_"..texture..".png"},
 			wield_image = "wool_"..name..".png",
 			wield_scale = { x=1, y=1, z=0.5 },
-			groups = {handy=1, carpet=1,deco_block=1},
+			groups = {handy=1, carpet=1,attached_node=1,deco_block=1},
 			sounds = mcl_sounds.node_sound_defaults(),
 			paramtype = "light",
 			stack_max = 64,
