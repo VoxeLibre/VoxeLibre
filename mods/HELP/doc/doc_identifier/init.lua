566bec5272dd74471211cf2d6508e11cf233a58f
Wuzzy <almikes@aol.com>
2017-03-18 01:19:15 +0100

    Rename lookup tool in Minecraft style

--- mods/HELP/doc/doc_identifier/init.lua
+++ mods/HELP/doc/doc_identifier/init.lua
@@ -143,7 +143,7 @@ function doc_identifier.liquid_mode(itemstack, user, pointed_thing)
 end
 
 minetest.register_tool("doc_identifier:identifier_solid", {
-	description = S("Lookup tool"),
+	description = S("Lookup Tool"),
 	_doc_items_longdesc = S("This useful little helper can be used to quickly learn more about about one's closer environment. It identifies and analyzes blocks, items and other things and it shows extensive information about the thing on which it is used."),
 	_doc_items_usagehelp = S("Punch any block, item or other thing about you wish to learn more about. This will open up the appropriate help entry. The tool comes in two modes which are changed by a rightclick. In liquid mode (blue) this tool points to liquids as well while in solid mode (red) this is not the case. Liquid mode is required if you want to identify a liquid."),
 	_doc_items_hidden = false,
@@ -157,7 +157,7 @@ minetest.register_tool("doc_identifier:identifier_solid", {
 	on_secondary_use = doc_identifier.liquid_mode,
 })
 minetest.register_tool("doc_identifier:identifier_liquid", {
-	description = S("Lookup tool"),
+	description = S("Lookup Tool"),
 	_doc_items_create_entry = false,
 	tool_capabilities = {},
 	range = 10,
