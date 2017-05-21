2197a48974df75e5f54c9819d7f6b9840c6e1616
Wuzzy <almikes@aol.com>
2017-05-21 05:31:30 +0200

    Add TNT explosion damage exhaust

--- mods/ITEMS/mcl_tnt/init.lua
+++ mods/ITEMS/mcl_tnt/init.lua
@@ -24,8 +24,9 @@ local function do_tnt_physics(tnt_np,tntr)
             if v ~= nil then
                 obj:setvelocity({x=(p.x - tnt_np.x) + (tntr / 4) + v.x, y=(p.y - tnt_np.y) + (tntr / 2) + v.y, z=(p.z - tnt_np.z) + (tntr / 4) + v.z})
             else
-                if obj:get_player_name() ~= nil then
+                if obj:is_player() ~= nil then
                     obj:set_hp(obj:get_hp() - 1)
+                    mcl_hunger.exhaust(obj:get_player_name(), mcl_hunger.EXHAUST_DAMAGE)
                 end
             end
         end
