f551eb42cea38a787d40657356e92eb12c8eebce
Wuzzy <almikes@aol.com>
2017-05-27 14:34:56 +0200

    Fix eternal fire help mistakes

--- mods/ITEMS/mcl_fire/init.lua
+++ mods/ITEMS/mcl_fire/init.lua
@@ -10,7 +10,7 @@ mcl_fire = {}
 -- Flame nodes
 
 local fire_help = "Fire is a damaging and destructive but short-lived kind of block. It will destroy and spread towards near flammable blocks, but fire will disappear when there is nothing to burn left. It will be extinguished by nearby water and rain. Fire can be destroyed safely by punching it, but it is hurtful if you stand directly in it."
-local eternal_fire_help = "Eternal fire is a damaging and destructive block. It will create fire around it when flammable blocks are nearby. Other than (normal) fire, an eternal fire will not go out by time alone. An eternal fire is still extinguished by punching it, by nearby water or by rain. Punching is is safe, but it hurts if you stand inside."
+local eternal_fire_help = "Eternal fire is a damaging and destructive block. It will create fire around it when flammable blocks are nearby. Eternal fire can be extinguished by punches and nearby water blocks. Other than (normal) fire, eternal fire does not get extinguished on its own and also continues to burn under rain. Punching eternal fire is safe, but it hurts if you stand inside."
 
 minetest.register_node("mcl_fire:fire", {
 	description = "Fire",
