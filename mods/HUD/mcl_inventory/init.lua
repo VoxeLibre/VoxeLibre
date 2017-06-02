5e0b4d749f670747c2f229885250bf9f6d490e67
Wuzzy <almikes@aol.com>
2017-06-03 00:51:28 +0200

    Fix armor tab not updating in Creative

--- mods/HUD/mcl_inventory/init.lua
+++ mods/HUD/mcl_inventory/init.lua
@@ -163,9 +163,10 @@ minetest.register_on_joinplayer(function(player)
 		end
 		armor.update_inventory = function(self, player)
 			update_inventory_original(self, player)
+			set_inventory(player, true)
 		end
 		armor:set_player_armor(player)
-		armor:update_inventory(player)
+		update_inventory_original(self, player)
 	end
 
 	-- In Creative Mode, the initial inventory setup is handled in creative.lua
