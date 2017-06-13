3fb5ce8ab00f2a35d50a480113d79b704203f39d
Wuzzy <almikes@aol.com>
2017-06-13 17:41:59 +0200

    Fix armor destroy bugs on death

--- mods/PLAYER/mcl_death/init.lua
+++ mods/PLAYER/mcl_death/init.lua
@@ -1,22 +1,37 @@
 minetest.register_on_dieplayer(function(player)
 	local keep = minetest.setting_getbool("mcl_keepInventory") or false
 	if keep == false then
+		-- Drop inventory, crafting grid and armor
 		local inv = player:get_inventory()
 		local pos = player:getpos()
-		local lists = { "main", "craft", "armor" }
+		local name, player_armor_inv, armor_armor_inv, pos = armor:get_valid_player(player, "[on_dieplayer]")
+		local lists = {
+			{ inv = inv, listname = "main", drop = true },
+			{ inv = inv, listname = "craft", drop = true },
+			{ inv = player_armor_inv, listname = "armor", drop = true },
+			{ inv = armor_armor_inv, listname = "armor", drop = false },
+		}
 		for l=1,#lists do
-			for i,stack in ipairs(inv:get_list(lists[l])) do
-				local x = math.random(0, 9)/3
-				local z = math.random(0, 9)/3
-				pos.x = pos.x + x
-				pos.z = pos.z + z
-				minetest.add_item(pos, stack)
-				stack:clear()
-				inv:set_stack(lists[l], i, stack)
-				pos.x = pos.x - x
-				pos.z = pos.z - z
+			local inv = lists[l].inv
+			local listname = lists[l].listname
+			local drop = lists[l].drop
+			if inv ~= nil then
+				for i, stack in ipairs(inv:get_list(listname)) do
+					local x = math.random(0, 9)/3
+					local z = math.random(0, 9)/3
+					pos.x = pos.x + x
+					pos.z = pos.z + z
+					if drop then
+						minetest.add_item(pos, stack)
+					end
+					stack:clear()
+					inv:set_stack(listname, i, stack)
+					pos.x = pos.x - x
+					pos.z = pos.z - z
+				end
 			end
 		end
+		armor:set_player_armor(player)
 		armor:update_inventory(player)
 	end
 
