845b5871d4dac5abaae198c89e08824f4eb66146
Wuzzy <almikes@aol.com>
2017-09-05 03:51:53 +0200

    Remove mcl_temp_schematic_aliases

--- mods/MISC/mcl_temp_schematic_aliases/init.lua
+++
@@ -1,6 +0,0 @@
--- Temporary node aliases for some legacy schematics in mcl_core.
--- Because these legacy schematics still use Minetest Game node names.
--- TODO: Update the offending schematics and delete this mod.
-
-minetest.register_alias("default:pine_tree", "mcl_core:sprucetree")
-minetest.register_alias("default:pine_needles", "mcl_core:spruceleaves")
