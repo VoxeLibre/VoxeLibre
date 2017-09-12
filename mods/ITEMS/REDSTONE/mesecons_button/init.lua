be135b7066005919f1801241b66e3752290422c9
Wuzzy <almikes@aol.com>
2017-09-12 05:37:03 +0200

    Tweak button/pressure plate connection sides

--- mods/ITEMS/REDSTONE/mesecons_button/init.lua
+++ mods/ITEMS/REDSTONE/mesecons_button/init.lua
@@ -10,9 +10,22 @@ local button_get_output_rules = function(node)
 		{x = 0, y = 0, z = -1},
 		{x = 0, y = 0, z = 1},
 		{x = 0, y = -1, z = 0},
+		{x = 0, y = 1, z = 0},
 	}
 	if minetest.wallmounted_to_dir(node.param2).y == 1 then
-		table.insert(rules, {x=0, y=1, z=1})
+		table.insert(rules, {x=0, y=2, z=0})
+	elseif minetest.wallmounted_to_dir(node.param2).y == -1 then
+		table.insert(rules, {x=0, y=-2, z=0})
+	end
+	if minetest.wallmounted_to_dir(node.param2).x == 1 then
+		table.insert(rules, {x=2, y=0, z=0})
+	elseif minetest.wallmounted_to_dir(node.param2).x == -1 then
+		table.insert(rules, {x=-2, y=0, z=0})
+	end
+	if minetest.wallmounted_to_dir(node.param2).z == 1 then
+		table.insert(rules, {x=0, y=0, z=2})
+	elseif minetest.wallmounted_to_dir(node.param2).z == -1 then
+		table.insert(rules, {x=-0, y=0, z=-2})
 	end
 	return rules
 end
