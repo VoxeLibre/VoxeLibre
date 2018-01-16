d61da938f4720ccdfe860d6c131800f9b9eca83c
Wuzzy <almikes@aol.com>
2018-01-16 20:58:54 +0100

    Fix /seed giving wrong seed

--- mods/MISC/mcl_commands/init.lua
+++ mods/MISC/mcl_commands/init.lua
@@ -141,7 +141,7 @@ minetest.register_chatcommand("seed", {
 	params = "",
 	privs = {},
 	func = function(name)
-		minetest.chat_send_player(name, string.format("%d", minetest.get_mapgen_setting("seed")))
+		minetest.chat_send_player(name, minetest.get_mapgen_setting("seed"))
 	end
 })
 
