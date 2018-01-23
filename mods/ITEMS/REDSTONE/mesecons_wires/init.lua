0405b2aa2caaa7db27da0bac912bf33a91d7ad76
Wuzzy <almikes@aol.com>
2018-01-23 05:19:05 +0100

    Fix some Lua warnings

--- mods/ITEMS/REDSTONE/mesecons_wires/init.lua
+++ mods/ITEMS/REDSTONE/mesecons_wires/init.lua
@@ -90,6 +90,7 @@ local update_on_place_dig = function (pos, node)
 	end
 
 	-- Update nodes around it
+	local rules
 	if minetest.registered_nodes[node.name]
 	and minetest.registered_nodes[node.name].mesecon_wire then
 		rules = wire_rules
