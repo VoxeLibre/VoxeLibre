42f5426fe911f93d0cfe62816959aa36cc9485fe
Wuzzy <almikes@aol.com>
2018-01-27 15:26:34 +0100

    Update repeater help (locking feature)

--- mods/ITEMS/REDSTONE/mesecons_delayer/init.lua
+++ mods/ITEMS/REDSTONE/mesecons_delayer/init.lua
@@ -170,8 +170,9 @@ end
 local help, longdesc, usagehelp, icon, on_construct
 if i == 1 then
 	help = true
-	longdesc = "Redstone repeaters are versatile redstone components which delay redstone signals and only allow redstone signals to travel through one direction. The delay of the signal is indicated by the redstone torches and is between 0.1 and 0.4 seconds long."
-	usagehelp = "To power a redstone repeater, send a signal in “arrow” direction. To change the delay, rightclick the redstone repeater. The delay is changed in steps of 0.1 seconds."
+	longdesc = "Redstone repeaters are versatile redstone components with multiple purposes: 1. They only allow signals to travel in one direction. 2. They delay the signal. 3. Optionally, they can lock their output in one state."
+	usagehelp = "To power a redstone repeater, send a signal in “arrow” direction (the input). The signal goes out on the opposite side (the output) with a delay. To change the delay, rightclick the redstone repeater. The delay is between 0.1 and 0.4 seconds long and can be changed in steps of 0.1 seconds. It is indicated by the position of the moving redstone torch.".."\n"..
+			"To lock a repeater, send a signal from an adjacent repeater into one of its sides. While locked, the moving redstone torch disappears, the output doesn't change and the input signal is ignored."
 	icon = "mesecons_delayer_item.png"
 	on_construct = function(pos)
 		local node = minetest.get_node(pos)
