40f468f18fecb1c5fad47cc77ad6161c4f900dda
Wuzzy <Wuzzy2@mail.ru>
2018-02-02 02:07:09 +0100

    Add non-functional anvil formspec

--- mods/ITEMS/mcl_anvils/init.lua
+++ mods/ITEMS/mcl_anvils/init.lua
@@ -17,6 +17,28 @@ local anvildef = {
 	sounds = mcl_sounds.node_sound_metal_defaults(),
 	_mcl_blast_resistance = 6000,
 	_mcl_hardness = 5,
+	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		local inv = meta:get_inventory()
+		inv:set_size("input1", 1)
+		inv:set_size("input2", 1)
+		inv:set_size("output", 1)
+		local form = "size[9,8.75]"..
+		"background[-0.19,-0.25;9.41,9.49;mcl_anvils_inventory.png]"..
+		mcl_vars.inventory_header..
+		"list[current_player;main;0,4.5;9,3;9]"..
+		"list[current_player;main;0,7.74;9,1;]"..
+		"list[context;input1;1,2.5;1,1;]"..
+		"list[current_name;input2;4,2.5;1,1;]"..
+		"list[current_name;output;8,2.5;1,1;]"..
+		"listring[current_name;output]"..
+		"listring[current_player;main]"..
+		"listring[current_name;input1]"..
+		"listring[current_player;main]"..
+		"listring[current_name;input2]"..
+		"listring[current_player;main]"
+		meta:set_string("formspec", form)
+	end,
 }
 if minetest.get_modpath("screwdriver") then
 	anvildef.on_rotate = screwdriver.rotate_simple
