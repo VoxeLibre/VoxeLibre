fceee430b3526eff8f170abf3f7bf0fccc004be1
Wuzzy <Wuzzy2@mail.ru>
2018-05-16 18:27:19 +0200

    Remove minetest.inventorycube call from rs lamps

--- mods/ITEMS/REDSTONE/mesecons_lightstone/init.lua
+++ mods/ITEMS/REDSTONE/mesecons_lightstone/init.lua
@@ -1,6 +1,5 @@
 minetest.register_node("mesecons_lightstone:lightstone_off", {
 	tiles = {"jeija_lightstone_gray_off.png"},
-	inventory_image = minetest.inventorycube("jeija_lightstone_gray_off.png"),
 	groups = {handy=1, mesecon_effector_off = 1, mesecon = 2},
 	is_ground_content = false,
 	description= "Redstone Lamp",
@@ -18,7 +17,6 @@ minetest.register_node("mesecons_lightstone:lightstone_off", {
 
 minetest.register_node("mesecons_lightstone:lightstone_on", {
 	tiles = {"jeija_lightstone_gray_on.png"},
-	inventory_image = minetest.inventorycube("jeija_lightstone_gray_off.png"),
 	groups = {handy=1, not_in_creative_inventory=1, mesecon = 2, opaque = 1},
 	drop = "node mesecons_lightstone:lightstone_off",
 	is_ground_content = false,
