26c915059174913834c4a866ede68674d9785b58
Wuzzy <Wuzzy2@mail.ru>
2019-03-07 12:42:06 +0100

    Revert "Use formspec prepend for real"
    This reverts commit 3ca01c31498eef534c21c1b5b941c10b0256c732.

--- mods/CORE/mcl_formspec_prepend/init.lua
+++ mods/CORE/mcl_formspec_prepend/init.lua
@@ -1,6 +1,6 @@
 minetest.register_on_joinplayer(function(player)
-	local bg = "bgcolor[#080808BB;true]"
+	local bg = ""--"bgcolor[#080808BB;true]"
 	local slots = "listcolors[#9990;#FFF7;#FFF0;#000;#FFF]"
 	local prepend = bg .. slots
-	player:set_formspec_prepend(prepend)
+--	player:set_formspec_prepend(prepend)
 end)
