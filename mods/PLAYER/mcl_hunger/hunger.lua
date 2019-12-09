7175ffb975c77dbef3f3444368d18dfbbe192243
Wuzzy <Wuzzy2@mail.ru>
2019-12-09 08:36:34 +0100

    Add workaround for broken eat sound

--- mods/PLAYER/mcl_hunger/hunger.lua
+++ mods/PLAYER/mcl_hunger/hunger.lua
@@ -148,7 +148,9 @@ function mcl_hunger.item_eat(hunger_change, replace_with_item, poisontime, poiso
 					max_hear_distance = 12,
 					gain = 1.0,
 					pitch = 1 + math.random(-10, 10)*0.005,
-					object = user,
+					-- FIXME: Should be linked to object, but it's broken in Minetest 5.1.0
+					-- See https://github.com/minetest/minetest/issues/9183
+					pos = pos,
 				})
 			else
 				-- Assume the item is a food
@@ -187,7 +189,9 @@ function mcl_hunger.item_eat(hunger_change, replace_with_item, poisontime, poiso
 					max_hear_distance = 12,
 					gain = 1.0,
 					pitch = 1 + math.random(-10, 10)*0.005,
-					object = user,
+					-- FIXME: Should be linked to object, but it's broken in Minetest 5.1.0
+					-- See https://github.com/minetest/minetest/issues/9183
+					pos = pos,
 				})
 			end
 
