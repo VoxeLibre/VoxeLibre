a03cc0604eca149ca2de57f2675ae907c6d5b220
Wuzzy <Wuzzy2@mail.ru>
2019-12-22 18:08:45 +0100

    End rods no longer fall

--- mods/ITEMS/mcl_end/building.lua
+++ mods/ITEMS/mcl_end/building.lua
@@ -75,7 +75,7 @@ minetest.register_node("mcl_end:end_rod", {
 	paramtype2 = "facedir",
 	light_source = minetest.LIGHT_MAX,
 	sunlight_propagates = true,
-	groups = { dig_immediate=3, deco_block=1, destroy_by_lava_flow=1, falling_node=1 },
+	groups = { dig_immediate=3, deco_block=1, destroy_by_lava_flow=1, },
 	node_box = {
 		type = "fixed",
 		fixed = {
