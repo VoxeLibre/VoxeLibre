b77307b7ac73e5c00417180f36e7ce2829425553
Wuzzy <Wuzzy2@mail.ru>
2019-12-27 15:24:59 +0100

    Add sea pickles

--- mods/ITEMS/mcl_ocean/init.lua
+++ mods/ITEMS/mcl_ocean/init.lua
@@ -9,3 +9,6 @@ dofile(minetest.get_modpath(minetest.get_current_modname()).."/seagrass.lua")
 
 -- Kelp
 dofile(minetest.get_modpath(minetest.get_current_modname()).."/kelp.lua")
+
+-- Sea Pickle
+dofile(minetest.get_modpath(minetest.get_current_modname()).."/sea_pickle.lua")
