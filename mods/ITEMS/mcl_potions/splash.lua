7766368f64ed8c191dd01c2d3e57b3b6d88cf3ab
bzoss <brandonzoss@gmail.com>
2020-06-13 17:50:33 -0400

    Change splash effect with distance.

--- mods/ITEMS/mcl_potions/splash.lua
+++ mods/ITEMS/mcl_potions/splash.lua
@@ -36,6 +36,7 @@ local function register_splash(name, descr, color, def)
           local node = minetest.get_node(pos)
           local n = node.name
 					local d = 2
+					local redux_map = {7/8,0.5,0.25}
           if n ~= "air" then
 						minetest.sound_play("mcl_potions_breaking_glass")
 						minetest.add_particlespawner({
@@ -56,8 +57,15 @@ local function register_splash(name, descr, color, def)
 																				texture = "mcl_potions_sprite.png^[colorize:"..color..":127",
 																			})
             self.object:remove()
-						for i, obj in ipairs(minetest.get_objects_inside_radius(pos, 3)) do
-							if minetest.is_player(obj) then def.potion_fun(obj) end
+						for i, obj in ipairs(minetest.get_objects_inside_radius(pos, 4)) do
+							if minetest.is_player(obj) then
+
+								pos2 = obj:get_pos()
+								local rad = math.floor(math.sqrt((pos2.x-pos.x)^2 + (pos2.y-pos.y)^2 + (pos2.z-pos.z)^2))
+								if rad > 0 then def.potion_fun(obj, redux_map[rad]) else def.potion_fun(obj, 1) end
+								print(obj:get_player_name().." "..math.floor(rad))
+
+							end
 						end
 					end
         end,
@@ -65,81 +73,81 @@ local function register_splash(name, descr, color, def)
 end
 
 register_splash("healing", "Splash Healing", "#AA0000", {
-    potion_fun = function(player) player:set_hp(player:get_hp() + 3) end,
+    potion_fun = function(player, redx) player:set_hp(player:get_hp() + 3*redx) end,
 })
 
 register_splash("healing_2", "Splash Healing II", "#DD0000", {
-    potion_fun = function(player) player:set_hp(player:get_hp() + 6) end,
+    potion_fun = function(player, redx) player:set_hp(player:get_hp() + 6*redx) end,
 })
 
 register_splash("harming", "Splash Harming", "#660099", {
-    potion_fun = function(player) mcl_potions.healing_func(player, -4) end,
+    potion_fun = function(player, redx) mcl_potions.healing_func(player, -4*redx) end,
 })
 
 register_splash("harming_2", "Splash Harming II", "#330066", {
-    potion_fun = function(player) mcl_potions.healing_func(player, -6) end,
+    potion_fun = function(player, redx) mcl_potions.healing_func(player, -6*redx) end,
 })
 
 register_splash("leaping", "Splash Leaping", "#00CC33", {
-		potion_fun = function(player) mcl_potions.leaping_func(player, 1.2, 135) end
+		potion_fun = function(player, redx) mcl_potions.leaping_func(player, 1.2, 135*redx) end
 })
 
 register_splash("leaping_2", "Splash Leaping II", "#00EE33", {
-		potion_fun = function(player) mcl_potions.leaping_func(player, 1.4, 135) end
+		potion_fun = function(player, redx) mcl_potions.leaping_func(player, 1.4, 135*redx) end
 })
 
 register_splash("leaping_plus", "Splash Leaping +", "#00DD33", {
-		potion_fun = function(player) mcl_potions.leaping_func(player, 1.2, 360) end
+		potion_fun = function(player, redx) mcl_potions.leaping_func(player, 1.2, 360*redx) end
 })
 
 register_splash("swiftness", "Splash Swiftness", "#009999", {
-		potion_fun = function(player) mcl_potions.swiftness_func(player, 1.2, 135) end
+		potion_fun = function(player, redx) mcl_potions.swiftness_func(player, 1.2, 135*redx) end
 })
 
 register_splash("swiftness_2", "Splash Swiftness II", "#00BBBB", {
-		potion_fun = function(player) mcl_potions.swiftness_func(player, 1.4, 135) end
+		potion_fun = function(player, redx) mcl_potions.swiftness_func(player, 1.4, 135*redx) end
 })
 
 register_splash("swiftness_plus", "Splash Swiftness +", "#00BBBB", {
-		potion_fun = function(player) mcl_potions.swiftness_func(player, 1.2, 360) end
+		potion_fun = function(player, redx) mcl_potions.swiftness_func(player, 1.2, 360*redx) end
 })
 
 register_splash("slowness", "Splash Slowness ", "#000080", {
-		potion_fun = function(player) mcl_potions.swiftness_func(player, 0.85, 68) end
+		potion_fun = function(player, redx) mcl_potions.swiftness_func(player, 0.85, 68*redx) end
 })
 
 register_splash("slowness_plus", "Splash Slowness +", "#000066", {
-		potion_fun = function(player) mcl_potions.swiftness_func(player, 0.85, 180) end
+		potion_fun = function(player, redx) mcl_potions.swiftness_func(player, 0.85, 180*redx) end
 })
 
 register_splash("poison", "Splash Poison", "#335544", {
-		potion_fun = function(player) mcl_potions.poison_func(player, 0.85, 180) end
+		potion_fun = function(player, redx) mcl_potions.poison_func(player, 0.85, 180*redx) end
 })
 
 register_splash("poison_2", "Splash Poison II", "#446655", {
-		potion_fun = function(player) mcl_potions.poison_func(player, 0.85, 180) end
+		potion_fun = function(player, redx) mcl_potions.poison_func(player, 0.85, 180*redx) end
 })
 
 register_splash("poison_plus", "Splash Poison II", "#557766", {
-		potion_fun = function(player) mcl_potions.poison_func(player, 0.85, 180) end
+		potion_fun = function(player, redx) mcl_potions.poison_func(player, 0.85, 180*redx) end
 })
 
 register_splash("regeneration", "Splash Regeneration", "#A52BB2", {
-		potion_fun = function(player) mcl_potions.regeneration_func(player, 0.85, 180) end
+		potion_fun = function(player, redx) mcl_potions.regeneration_func(player, 0.85, 180*redx) end
 })
 
 register_splash("regeneration_2", "Splash Regeneration II", "#B52CC2", {
-		potion_fun = function(player) mcl_potions.regeneration_func(player, 0.85, 180) end
+		potion_fun = function(player, redx) mcl_potions.regeneration_func(player, 0.85, 180*redx) end
 })
 
 register_splash("regeneration_plus", "Splash Regeneration +", "#C53DD3", {
-		potion_fun = function(player) mcl_potions.regeneration_func(player, 0.85, 180) end
+		potion_fun = function(player, redx) mcl_potions.regeneration_func(player, 0.85, 300*redx) end
 })
 
 register_splash("invisibility", "Splash Invisibility", "#B0B0B0", {
-	potion_fun = function(player) mcl_potions.invisiblility_func(player, 135) end
+	potion_fun = function(player, redx) mcl_potions.invisiblility_func(player, 135*redx) end
 })
 
 register_splash("invisibility", "Splash Invisibility", "#A0A0A0", {
-	potion_fun = function(player) mcl_potions.invisiblility_func(player, 300) end
+	potion_fun = function(player, redx) mcl_potions.invisiblility_func(player, 300*redx) end
 })
