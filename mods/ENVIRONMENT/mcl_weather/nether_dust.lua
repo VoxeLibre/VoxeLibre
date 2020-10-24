196569682930128a16821abdb621ac6c75895f69
kay27 <kay27@bk.ru>
2020-10-24 21:49:11 +0400

    Merge kay27_textures branch: more recongizable obsidian texture; better NP texture; Nether particles

---
+++ mods/ENVIRONMENT/mcl_weather/nether_dust.lua
@@ -0,0 +1,39 @@
+mcl_weather.nether_dust = {}
+mcl_weather.nether_dust.particles_count = 99
+
+-- calculates coordinates and draw particles for Nether dust
+mcl_weather.nether_dust.add_dust_particles = function(player)
+	for i=mcl_weather.nether_dust.particles_count, 1,-1 do
+		local rpx, rpy, rpz = mcl_weather.get_random_pos_by_player_look_dir(player)
+		minetest.add_particle({
+			pos = {x = rpx, y = rpy - math.random(6, 18), z = rpz},
+			velocity = {x = math.random(-30,30)*0.01, y = math.random(-15,15)*0.01, z = math.random(-30,30)*0.01},
+			acceleration = {x = math.random(-50,50)*0.02, y = math.random(-20,20)*0.02, z = math.random(-50,50)*0.02},
+			expirationtime = 3,
+			size = math.random(6,20)*0.01,
+			collisiondetection = false,
+			object_collision = false,
+			vertical = false,
+			glow = math.random(0,minetest.LIGHT_MAX),
+			texture = "mcl_particles_nether_dust"..tostring(i%3+1)..".png",
+			playername = player:get_player_name()
+		})
+	end
+end
+
+local timer = 0
+minetest.register_globalstep(function(dtime)
+	timer = timer + dtime
+	if timer >= 0.7 then
+		timer = 0
+	else
+		return
+	end
+
+	for _, player in ipairs(minetest.get_connected_players()) do
+		if (mcl_weather.is_underwater(player) or not mcl_worlds.has_dust(player:get_pos())) then
+			return false
+		end
+		mcl_weather.nether_dust.add_dust_particles(player)
+	end
+end)
--- tools/create_texture__mcl_portals_portal.py
+++ tools/create_texture__mcl_portals_portal.py
@@ -1,6 +1,43 @@
 import png
-w, h = 64, 256;
+w, h = 16, 128;
 s = [[int(0) for c in range(w)] for c in range(h)] 
+
+def drawpixel(x, y, t):
+	if (x >= 0) and (x < w) and (y >= 0) and (y < h):
+		if(t == 1):
+			s[y][x] = 1
+		elif t==2:
+			s[y][x] = 1 - s[y][x]
+		elif t==3:
+			s[y][x] = s[y][x] + 1
+			if s[y][x] > 3:
+				s[y][x] =s[y][x]-4
+
+def circle(X1, Y1, R, t):
+	x = 0
+	y = R
+	delta = 1 - 2 * R
+	error = 0
+	while y >= 0:
+		if Y1%w + y < w:
+			drawpixel(X1 + x, Y1 + y, t)
+		if Y1%w - y >= 0:
+			drawpixel(X1 + x, Y1 - y, t)
+		if Y1%w + y < w:
+			drawpixel(X1 - x, Y1 + y, t)
+		if Y1%w - y >= 0:
+			drawpixel(X1 - x, Y1 - y, t)
+		error = 2 * (delta + y) - 1
+		if ((delta < 0) and (error <= 0)):
+			x = x + 1
+			delta = delta + 2 * x + 1
+		elif ((delta > 0) and (error > 0)):
+			y = y - 1
+			delta = delta - 2 * y + 1
+		else:
+			x = x + 1
+			y = y - 1
+			delta = delta + 2 * (x - y)
 
 def line(y1, x1, y2, x2, v):
 	signx = 1
@@ -19,10 +56,14 @@ def line(y1, x1, y2, x2, v):
 	if dx >= dy:
 		dir1 = 1
 	for i in range(max(dx, dy)+1):
-		if v==2:
-			s[x1][y1]=1-s[x1][y1]
-		else:
-			s[x1][y1] = v
+		if(v == 1):
+			s[x1][y1] = 1
+		elif v==2:
+			s[x1][y1] = 1 - s[x1][y1]
+		elif v==3 or (v==4 and ((x1^y1)&1)) or (v==5 and (((x1|y1)&1)==0)) or (v==7 and ((((x1+1)|y1)&1)==0)) or (v==8 and ((((x1+1)|(y1+1))&1)==0)) or (v==6 and (((x1|(y1+1))&1)==0)):
+			s[x1][y1] = s[x1][y1] + 1
+			if s[x1][y1] > 3:
+				s[x1][y1] = s[x1][y1] - 4
 		if dir1 == 1:
 			x1 += signx
 			offsy += dy
@@ -37,22 +78,37 @@ def line(y1, x1, y2, x2, v):
 				offsx -= dy
 
 # R, G, B, Alpha (0xFF = opaque):
-palette=[(0x00,0x00,0xaf,0xa0), (0x7f,0x0f,0xaf,0xb8)]
+palette=[
+	(0x30,0x03,0xaf,0xa4),
+	(0x4f,0x1c,0xaf,0xb4),
+	(0x7f,0x3d,0xa0,0xc8),
+	(0x6d,0x5d,0x99,0xb1)
+]
 
-for j in range(16):
-	i = j * 4
-	line(i, 0, 63-i, 63, 2)
-	line(63, i, 0, 63-i, 2)
-	i+=1
-	line(i, 64, 63-i, 127, 2)
-	line(63, 64+i, 0, 127-i, 2)
-	i+=1
-	line(i, 128, 63-i, 191, 2)
-	line(63, 128+i, 0, 191-i, 2)
-	i+=1
-	line(i, 192, 63-i, 255, 2)
-	line(63, 192+i, 0, 255-i, 2)
+circles = h//w
+maxr = w//2
+for i in [1,2,3,5,9,10,11,13]:
+	for c in range(circles):
+		q = ((circles-c-1)+i)%w
+		circle(maxr, maxr+c*w, q, 3)
 
-w = png.Writer(len(s[0]), len(s), palette=palette, bitdepth=1)
+
+linesperside = 2
+linestarts = round(w / linesperside) # 8
+lineoffset = round(w / linestarts) # 2
+wminus = w - 1
+
+for j in range(linesperside):
+	for k in range(linestarts):
+		offset = k * w
+		for q in [0,1,3,4]:
+#		for q in [1]:
+			i = j*linestarts + ((k+q)%linestarts)
+			line(i, offset, wminus-i, offset+wminus, 5+(k&3))
+			line(wminus, offset+i, 0, offset+wminus-i, 5+(k&3))
+
+
+
+w = png.Writer(len(s[0]), len(s), palette=palette, bitdepth=2)
 f = open('mcl_portals_portal.png', 'wb')
 w.write(f, s)
