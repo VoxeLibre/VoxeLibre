c7a4f7cbfb8729dd86cdb915ae21c69b46919ac8
Wuzzy <Wuzzy2@mail.ru>
2020-11-07 01:11:42 +0100

    Fix brewstand not showing up in craftguide

--- mods/ITEMS/mcl_brewing/init.lua
+++ mods/ITEMS/mcl_brewing/init.lua
@@ -965,7 +965,7 @@ minetest.register_node("mcl_brewing:stand_111", {
 })
 
 minetest.register_craft({
-	output = "mcl_brewing:stand",
+	output = "mcl_brewing:stand_000",
 	recipe = {
 		{ "", "mcl_mobitems:blaze_rod", "" },
 		{ "mcl_core:cobble", "mcl_core:cobble", "mcl_core:cobble" },
