f68a75f184faee1e6d57b2e7c9408e27ec4952bd
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-01-06 13:28:58 +0100

    Fix #750

--- mods/ENTITIES/mobs_mc/zombiepig.lua
+++ mods/ENTITIES/mobs_mc/zombiepig.lua
@@ -19,7 +19,6 @@ local pigman = {
 	hp_max = 20,
 	xp_min = 6,
 	xp_max = 6,
-	breath_max = -1,
 	armor = {undead = 90, fleshy = 90},
 	attack_type = "dogfight",
 	group_attack = { "mobs_mc:pigman", "mobs_mc:baby_pigman" },
