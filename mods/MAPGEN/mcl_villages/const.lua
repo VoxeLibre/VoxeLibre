ff38a44454eaa51c909c2b1553f92871c2d752a1
MysticTempest <molecularmarc.he@gmail.com>
2021-01-28 01:24:43 -0600

    Give 'mcl_villages' the proper loot table, fix blacksmith shop catching fire/missing chest, add butcher shop, remove chest from large_houses, and remove screenshots.

--- mods/MAPGEN/mcl_villages/const.lua
+++ mods/MAPGEN/mcl_villages/const.lua
@@ -54,12 +54,13 @@ schem_path = settlements.modpath.."/schematics/"
 --
 schematic_table = { 
 	{name = "large_house",	mts = schem_path.."large_house.mts",	hwidth = 11, hdepth = 12, hheight = 9, hsize = 14, max_num = 0.08, rplc = "n"},
-	{name = "blacksmith",	mts = schem_path.."blacksmith.mts",	hwidth = 7, hdepth = 7, hheight = 13, hsize = 13, max_num = 0.050, rplc = "n"},
+	{name = "blacksmith",	mts = schem_path.."blacksmith.mts",	hwidth = 7, hdepth = 7, hheight = 13, hsize = 13, max_num = 0.055, rplc = "n"},
+	{name = "butcher",		mts = schem_path.."butcher.mts",		hwidth = 11, hdepth = 8, hheight = 10, hsize = 14, max_num = 0.03, rplc = "n"},
 	{name = "church",	mts = schem_path.."church.mts",		hwidth = 13, hdepth = 13, hheight = 14, hsize = 15, max_num = 0.04, rplc = "n"},
 	{name = "farm",		mts = schem_path.."farm.mts",		hwidth = 7, hdepth = 7, hheight = 13, hsize = 13, max_num = 0.1, rplc = "n"},
 	{name = "lamp",		mts = schem_path.."lamp.mts",		hwidth = 3, hdepth = 3, hheight = 13, hsize = 10, max_num = 0.1, rplc = "n"},
 	{name = "library",	mts = schem_path.."library.mts",	hwidth = 12, hdepth = 12, hheight = 8, hsize = 13, max_num = 0.04, rplc = "n"},
-	{name = "medium_house",	mts = schem_path.."medium_house.mts",	hwidth = 8, hdepth = 12, hheight = 8, hsize = 14, max_num = 0.09, rplc = "n"},
+	{name = "medium_house",	mts = schem_path.."medium_house.mts",	hwidth = 8, hdepth = 12, hheight = 8, hsize = 14, max_num = 0.08, rplc = "n"},
 	{name = "small_house",	mts = schem_path.."small_house.mts",	hwidth = 9, hdepth = 7, hheight = 8, hsize = 13, max_num = 0.7, rplc = "n"},
 	{name = "tavern",	mts = schem_path.."tavern.mts",		hwidth = 11, hdepth = 10, hheight = 10, hsize = 13, max_num = 0.050, rplc = "n"},
 	{name = "well",		mts = schem_path.."well.mts",		hwidth = 6, hdepth = 8, hheight = 6, hsize = 10, max_num = 0.045, rplc = "n"},
