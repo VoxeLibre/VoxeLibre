ba9866db4916571039be1059855e40d4904ff1ef
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-04-08 09:52:50 -0400

    merge

--- mods/ENTITIES/mobs_mc/0_gameconfig.lua
+++ mods/ENTITIES/mobs_mc/0_gameconfig.lua
@@ -290,9 +290,9 @@ mobs_mc.spawn = {
 mobs_mc.spawn_height = {
 	water = tonumber(minetest.settings:get("water_level")) or 0, -- Water level in the Overworld
 
-	-- Overworld boundaries (inclusive) --had to make this reasonable otherwise mob spawning would go nuts using data
+	-- Overworld boundaries (inclusive) --I adjusted this to be more reasonable
 	overworld_min = -64,-- -2999,
-	overworld_max = 256,-- 31000,
+	overworld_max = 31000,
 
 	-- Nether boundaries (inclusive)
 	nether_min = -29067,-- -3369,
