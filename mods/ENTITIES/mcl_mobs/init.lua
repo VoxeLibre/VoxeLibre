da045c207d3bd5931e3cf73c5459b45d86596c12
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2021-04-08 02:07:15 -0400

    Refactor spawning into it's own file

--- mods/ENTITIES/mcl_mobs/init.lua
+++ mods/ENTITIES/mcl_mobs/init.lua
@@ -4,6 +4,9 @@ local path = minetest.get_modpath(minetest.get_current_modname())
 -- Mob API
 dofile(path .. "/api.lua")
 
+-- Spawning Algorithm
+dofile(path .. "/spawning.lua")
+
 -- Rideable Mobs
 dofile(path .. "/mount.lua")
 
