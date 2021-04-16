f94ab6c8a547098822e19cb21e08b021112ec74e
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-04-16 18:34:29 +0200

    Massive mcl_burning performance improvement

--- mods/ENTITIES/mcl_mobs/api.lua
+++ mods/ENTITIES/mcl_mobs/api.lua
@@ -3454,7 +3454,7 @@ end
 -- main mob function
 local mob_step = function(self, dtime)
 
-	if not self.fire_resistant then
+	if not self.fire_resistant and self._mcl_burning_burn_time and self._mcl_burning_burn_time > 0 then
 		mcl_burning.tick(self.object, dtime)
 	end
 
@@ -3913,7 +3913,7 @@ minetest.register_entity(name, {
 		--default built in engine collision detection
 		self.object:set_properties({
 			collide_with_objects = false,
-		})	
+		})
 		return mob_activate(self, staticdata, def, dtime)
 	end,
 
