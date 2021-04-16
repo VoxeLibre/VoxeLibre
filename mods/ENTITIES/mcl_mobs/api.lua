01c79c5a18d335e620777bff8e2d1e485c2337bb
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-04-16 18:37:07 +0200

    Fix mcl_burning.tick not being called at all for mobs

--- mods/ENTITIES/mcl_mobs/api.lua
+++ mods/ENTITIES/mcl_mobs/api.lua
@@ -3447,7 +3447,7 @@ end
 -- main mob function
 local mob_step = function(self, dtime)
 
-	if not self.fire_resistant and self._mcl_burning_burn_time and self._mcl_burning_burn_time > 0 then
+	if not self.fire_resistant and self.mcl_burning_burn_time and self.mcl_burning_burn_time > 0 then
 		mcl_burning.tick(self.object, dtime)
 	end
 
