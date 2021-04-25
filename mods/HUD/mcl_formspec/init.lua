8fac3d11895d891df9a5b22a58de836e2ec2c2fb
AFCMS <afcm.contact@gmail.com>
2021-04-25 23:35:27 +0200

    add basic temp function to get v4 itemslots

--- mods/HUD/mcl_formspec/init.lua
+++ mods/HUD/mcl_formspec/init.lua
@@ -9,3 +9,15 @@ function mcl_formspec.get_itemslot_bg(x, y, w, h)
 	end
 	return out
 end
+
+--This function will replace mcl_formspec.get_itemslot_bg then every formspec will be upgrade to version 4
+function mcl_formspec.get_itemslot_bg_v4(x, y, w, h)
+	local out = ""
+	for i = 0, w - 1, 1 do
+		for j = 0, h - 1, 1 do
+			out = out .."image["..x+i+(i*0.25)..","..y+j+(j*0.25)..";1,1;mcl_formspec_itemslot.png]"
+		end
+	end
+	return out
+end
+
