81ee51b0c2663db964f6801a1c1e163941be0281
AFCMS <afcm.contact@gmail.com>
2021-05-10 09:58:26 +0200

    document script

--- tools/create_luacheck.py
+++ tools/create_luacheck.py
@@ -2,12 +2,18 @@ import os
 import re
 from pathlib import Path
 
+# Just run this script from mineclone2 directory to get a list of every global vars to use in luacheck configuration files
+
 path =  "./mods/"
-# pattern = re.compile(r'^(?P<nomtableau>[^ \t\]]+)[ ]*=[ ]*\{')
-pattern = re.compile(r'^(?P<nomtableau>[A-Za-z_0-9]+)[ ]*=[ ]*\{')
-pattern_local = re.compile(r'local (?P<nomvar>[A-Za-z_0-9]+)')
+
+pattern = re.compile(r'^(?P<global_var>[A-Za-z_0-9]+)[ ]*=[ ]*\{')
+pattern_local = re.compile(r'local (?P<local_var>[A-Za-z_0-9]+)')
 
 global_vars = []
+
+
+print("---Copy/Paste output in your luacheck conf file---\n")
+
 
 pathlist = Path(path).rglob('*.lua')
 for path in pathlist:
@@ -15,31 +21,24 @@ for path in pathlist:
     # print(path_in_str)
     trouve = False
     with open(path_in_str) as f:
-        variables_locales = []
+        local_vars = []
         for i, line in enumerate(f.readlines()):
             m = pattern.match(line)
             if m:
-                nomtableau = m.group('nomtableau')
-                if nomtableau not in variables_locales:
-                    print(path_in_str, ":", i+1, ":", m.group('nomtableau').strip())
-                    global_vars.append(m.group('nomtableau').strip())
-                    trouve = True
+                global_name = m.group('global_var')
+                if global_name not in local_vars:
+                    #print(path_in_str, ":", i+1, ":", m.group('global_var').strip())
+                    global_vars.append(m.group('global_var').strip())
+                    found = True
                     break
 
             else:
                 n = pattern_local.match(line)
                 if n:
-                    variables_locales.append(n.group('nomvar'))
+                    local_vars.append(n.group('local_var'))
 
-        if not trouve:
+        if not found:
             nb_varloc = len(variables_locales)
             #print(path_in_str, ": -", "({} variables locales)".format(nb_varloc) if nb_varloc > 0 else '')
-
-""" for subdir, dirs, files in os.walk(path):
-    for file in files:
-        print(os.path.join(subdir, file))
-        filepath = subdir + os.sep + file
-        if filepath.endswith(".lua"):
-            print(filepath) """
 
 print(', '.join(['"{}"'.format(v) for v in global_vars]))
