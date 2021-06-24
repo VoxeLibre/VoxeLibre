528f1cb81fb8c16b5ca50d1cbe5eca917bb6875f
AFCMS <afcm.contact@gmail.com>
2021-06-24 10:03:05 +0200

    move some files to the right location

--- mods/CORE/mcl_commands/init.lua
+++ mods/CORE/mcl_commands/init.lua
@@ -1,11 +1,11 @@
 local modpath = minetest.get_modpath(minetest.get_current_modname())
 
-dofile(modpath.."/kill.lua")
-dofile(modpath.."/setblock.lua")
-dofile(modpath.."/seed.lua")
-dofile(modpath.."/summon.lua")
-dofile(modpath.."/say.lua")
-dofile(modpath.."/list.lua")
-dofile(modpath.."/sound.lua")
+dofile(modpath.."/register/kill.lua")
+dofile(modpath.."/register/setblock.lua")
+dofile(modpath.."/register/seed.lua")
+dofile(modpath.."/register/summon.lua")
+dofile(modpath.."/register/say.lua")
+dofile(modpath.."/register/list.lua")
+dofile(modpath.."/register/sound.lua")
 
 dofile(modpath.."/alias.lua")
