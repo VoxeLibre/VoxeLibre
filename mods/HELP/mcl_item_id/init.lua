75b425ffd77b85ba3081ddf2e47f8b6695ec8fa5
NO11 <no11@noreply.git.minetest.land>
2021-07-23 12:23:30 +0000

    Fix #1842 make other mods not using "mineclone" name space for item ids

--- mods/HELP/mcl_item_id/init.lua
+++ mods/HELP/mcl_item_id/init.lua
@@ -1,4 +1,5 @@
 local game = "mineclone"
+local mcl_mods = {}
 
 local same_id = {
     heads = { "skeleton", "zombie", "creeper", "wither_skeleton" },
@@ -10,17 +11,34 @@ local same_id = {
         "stonebrick", "stonebrickmossy", 
     },
     wool = {
-        "black", "blue",  "brown", "cyan", "green",
+        "black", "blue", "brown", "cyan", "green",
         "grey", "light_blue", "lime", "magenta", "orange",
         "pink", "purple", "red", "silver", "white", "yellow",
     },
 }
+
+local worldmt = io.open(minetest.get_worldpath() .. "/world.mt", "r")
+local gameid = worldmt:read("*a"):match("gameid%s*=%s*(%S+)\n")
+worldmt:close()
+
+for _, mod in pairs(minetest.get_modnames()) do
+    if minetest.get_modpath(mod):match("/games/" .. gameid .. "/") then
+        table.insert(mcl_mods, mod)
+    end
+end
+
+local function item_id(id)
+    if minetest.settings:get_bool("mcl_item_id_debug", false) then
+        return id, "#555555"
+    end
+end
 
 tt.register_snippet(function(itemstring)
     local def = minetest.registered_items[itemstring]
     local desc = def.description
     local item_split = itemstring:find(":")
     local new_id = game .. itemstring:sub(item_split)
+    local mcl_mod = itemstring:sub(1, item_split)
     for mod, ids in pairs(same_id) do
         for _, id in pairs(ids) do
             if itemstring == "mcl_" .. mod .. ":" .. id  then
@@ -28,12 +46,15 @@ tt.register_snippet(function(itemstring)
             end
         end
     end
-    if new_id ~= game .. ":book_enchanted" then
-        minetest.register_alias_force(new_id, itemstring)
+    for _, modname in pairs(mcl_mods) do
+        if modname .. ":" == mcl_mod then
+            if new_id ~= game .. ":book_enchanted" and new_id ~= itemstring then
+                minetest.register_alias_force(new_id, itemstring)
+            end
+            return item_id(new_id)
+        end
     end
-    if minetest.settings:get_bool("mcl_item_id_debug", false) then
-        return new_id, "#555555"
-    end
+    return item_id(itemstring)
 end)
 
 minetest.register_alias_force(game .. ":book_enchanted", "mcl_enchanting:book_enchanted")
