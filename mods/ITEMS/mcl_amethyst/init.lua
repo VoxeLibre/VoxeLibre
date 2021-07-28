ebdf944ddadd067708ef52a82c8cf5652548f618
Emojigit <55009343+Emojigit@users.noreply.github.com>
2021-07-28 12:40:40 +0800

    amethyst bud

--- mods/ITEMS/mcl_amethyst/init.lua
+++ mods/ITEMS/mcl_amethyst/init.lua
@@ -82,6 +82,45 @@ minetest.register_node("mcl_amethyst:tinted_glass",{
   _doc_items_longdesc = S("Tinted Glass is a type of glass which blocks lights while it is visually transparent."),
 })
 
+-- Amethyst Cluster
+local bud_def = {
+  {"small","Small","mcl_amethyst:medium_amethyst_bud"},
+  {"medium","Medium","mcl_amethyst:large_amethyst_bud"},
+  {"large","Large","mcl_amethyst:amethyst_cluster"},
+}
+for x,y in pairs(bud_def) do
+  minetest.register_node("mcl_amethyst:" .. y[1] .. "_amethyst_bud",{
+    description = y[2] .. "Amethyst Bud",
+    _mcl_hardness = 1.5,
+    _mcl_blast_resistance = 1.5,
+    drop = "",
+    tiles = {y[1] .. "_amethyst_bud.png",},
+    paramtype2 = "wallmounted",
+    drawtype = "plantlike",
+    use_texture_alpha = "clip",
+    sunlight_propagates = true,
+    groups = {
+      dig_by_water = 1,
+      destroy_by_lava_flow = 1,
+      dig_by_piston = 1,
+      pickaxey = 1,
+      deco_block = 1,
+    },
+    selection_box = {
+      type = "fixed",
+      -- fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+      fixed = { -7/16, -8/16, -7/16, 7/16, -7/16, 7/16 },
+    },
+    collision_box = {
+      type = "fixed",
+      -- fixed = {-0.5, -0.5, -0.5, 0.5, 0, 0.5},
+      fixed = { -7/16, -8/16, -7/16, 7/16, -7/16, 7/16 },
+    },
+    _mcl_silk_touch_drop = true,
+    _mcl_amethyst_next_grade = y[3],
+  })
+end
+
 -- Register Crafts
 minetest.register_craft({
   output = "mcl_amethyst:amethyst_block",
