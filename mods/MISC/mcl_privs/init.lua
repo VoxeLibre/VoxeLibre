0e15accada59737c415a0920e93b65a39e5aa2ae
NO11 <no11@noreply.git.minetest.land>
2021-09-19 11:15:19 +0000

    Enable fly in creative mode

--- mods/MISC/mcl_privs/init.lua
+++ mods/MISC/mcl_privs/init.lua
@@ -3,3 +3,14 @@ local S = minetest.get_translator(minetest.get_current_modname())
 minetest.register_privilege("maphack", {
 	description = S("Can place and use advanced blocks like mob spawners, command blocks and barriers."),
 })
+
+minetest.register_on_joinplayer(function(player)
+	local name = user:get_player_name()
+	local fly = false
+	if minetest.is_creative_enabled(name) then
+		fly = true
+	end
+	minetest.set_player_privs(name, {
+		fly = fly,
+	})
+end)
