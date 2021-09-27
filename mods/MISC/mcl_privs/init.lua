8697b38d60325f289eb75fd19b83e86e877946bc
NO11 <no11@noreply.git.minetest.land>
2021-09-27 20:00:11 +0000

    Remove on_newplayer ...

--- mods/MISC/mcl_privs/init.lua
+++ mods/MISC/mcl_privs/init.lua
@@ -4,8 +4,11 @@ minetest.register_privilege("maphack", {
 	description = S("Can place and use advanced blocks like mob spawners, command blocks and barriers."),
 })
 
-minetest.register_on_newplayer(function(player)
+minetest.register_on_joinplayer(function(player)
 	local name = player:get_player_name()
+
+	local meta = player:get_meta()
+	if meta:get_int("fly_changed") == 1 then return end
 
 	local fly = nil
 	if minetest.is_creative_enabled(name) then
@@ -15,3 +18,13 @@ minetest.register_on_newplayer(function(player)
 		fly = fly,
 	})
 end)
+
+for _, action in pairs({ "grant", "revoke" }) do
+	minetest["register_on_priv_" .. action](function(name, _, priv)
+		if priv == "fly" then
+			local player = minetest.get_player_by_name(name)
+			local meta = player:get_meta()
+			meta:set_int("fly_changed", 1)
+		end
+	end)
+end
