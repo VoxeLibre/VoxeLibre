e4feb233e077ee4878c4bf85a88d24bc709c20b2
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-12-09 18:20:24 +0100

    Fix hopper dupe

--- mods/ITEMS/mcl_hoppers/init.lua
+++ mods/ITEMS/mcl_hoppers/init.lua
@@ -350,7 +350,7 @@ minetest.register_abm({
 		local inv = meta:get_inventory()
 
 		for _,object in pairs(minetest.get_objects_inside_radius(pos, 2)) do
-			if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "__builtin:item" then
+			if not object:is_player() and object:get_luaentity() and object:get_luaentity().name == "__builtin:item" and not object:get_luaentity()._removed then
 				if inv and inv:room_for_item("main", ItemStack(object:get_luaentity().itemstring)) then
 					-- Item must get sucked in when the item just TOUCHES the block above the hopper
 					-- This is the reason for the Y calculation.
