810051c5918c5e5eff43b05cedf90fa37f61c486
kabou <noreply@example.org>
2022-05-02 00:58:31 +0200

    Move cocoa beans item to mcl_cocoas.
    * Add `mcl_cocoas:coca_beans` craftitem to mcl_cocoas.
    * Remove `mcl_dye:brown` craftitem from mcl_dye.
    * Move cocoa beans translations from mcl_dye to mcl_cocoas.
    * Add `mcl_dye:brown` alias for `mcl_cocoas:cocoa_beans` to
      mcl_dye.
    * Abstract cocoa pod node registration into a loop.
    * Update chocolate cookies crafting recipe in mcl_farming.

--- mods/ITEMS/mcl_cocoas/init.lua
+++ mods/ITEMS/mcl_cocoas/init.lua
@@ -2,29 +2,33 @@ local S = minetest.get_translator(minetest.get_current_modname())
 
 mcl_cocoas = {}
 
--- Place cocoa
-local function cocoa_place(itemstack, placer, pt, plantname)
+--- Place a cocoa pod.
+-- Attempt to place a cocoa pod on a jungle tree.  Checks if attachment
+-- point is a jungle tree and sets the correct orientation of the stem.
+--
+function mcl_cocoas.place(itemstack, placer, pt, plantname)
 	-- check if pointing at a node
 	if not pt or pt.type ~= "node" then
 		return
 	end
 
-	local under = minetest.get_node(pt.under)
+	local node = minetest.get_node(pt.under)
 
 	-- return if any of the nodes are not registered
-	if not minetest.registered_nodes[under.name] then
+	local def = minetest.registered_nodes[node.name]
+	if not def then
 		return
 	end
 
 	-- Am I right-clicking on something that has a custom on_rightclick set?
 	if placer and not placer:get_player_control().sneak then
-		if minetest.registered_nodes[under.name] and minetest.registered_nodes[under.name].on_rightclick then
-			return minetest.registered_nodes[under.name].on_rightclick(pt.under, under, placer, itemstack) or itemstack
+		if def and def.on_rightclick then
+			return def.on_rightclick(pt.under, node, placer, itemstack) or itemstack
 		end
 	end
 
 	-- Check if pointing at jungle tree
-	if under.name ~= "mcl_core:jungletree"
+	if node.name ~= "mcl_core:jungletree"
 	or minetest.get_node(pt.above).name ~= "air" then
 		return
 	end
@@ -39,9 +43,7 @@ local function cocoa_place(itemstack, placer, pt, plantname)
 
 	-- Add the node, set facedir and remove 1 item from the itemstack
 	minetest.set_node(pt.above, {name = plantname, param2 = minetest.dir_to_facedir(clickdir)})
-
 	minetest.sound_play("default_place_node", {pos = pt.above, gain = 1.0}, true)
-
 	if not minetest.is_creative_enabled(placer:get_player_name()) then
 		itemstack:take_item()
 	end
@@ -49,117 +51,138 @@ local function cocoa_place(itemstack, placer, pt, plantname)
 	return itemstack
 end
 
--- Attempts to grow a cocoa at pos, returns true when grown, returns false if there's no cocoa
--- or it is already at full size
+--- Grows cocoa pod one size larger.
+-- Attempts to grow a cocoa at pos, returns true when grown, returns false
+-- if there's no cocoa or it is already at full size.
+--
 function mcl_cocoas.grow(pos)
 	local node = minetest.get_node(pos)
 	if node.name == "mcl_cocoas:cocoa_1" then
 		minetest.set_node(pos, {name = "mcl_cocoas:cocoa_2", param2 = node.param2})
 	elseif node.name == "mcl_cocoas:cocoa_2" then
 		minetest.set_node(pos, {name = "mcl_cocoas:cocoa_3", param2 = node.param2})
-		return true
+	else
+		return false
 	end
-	return false
+	return true
 end
 
--- Cocoa definition
--- 1st stage
-local crop_def = {
-	description = S("Premature Cocoa Pod"),
-	_doc_items_create_entry = true,
-	_doc_items_longdesc = S("Cocoa pods grow on the side of jungle trees in 3 stages."),
-	drawtype = "mesh",
-	mesh = "mcl_cocoas_cocoa_stage_0.obj",
-	tiles = {"mcl_cocoas_cocoa_stage_0.png"},
-	use_texture_alpha = minetest.features.use_texture_alpha_string_modes and "clip" or true,
-	paramtype = "light",
-	sunlight_propagates = true,
-	paramtype2 = "facedir",
-	walkable = true,
-	drop = "mcl_cocoas:cocoa_beans",
-	collision_box = {
-		type = "fixed",
-		fixed = {
-			{-0.125, -0.0625, 0.1875, 0.125, 0.25, 0.4375},  -- Pod
+-- only caller was mcl_dye, now these can be local functions.
+-- TODO: remove aliases, replace global functions with local functions.
+local cocoa_place = mcl_cocoas.place
+local cocoa_grow = mcl_cocoas.grow
+
+-- Cocoa pod variant definitions.
+--[[ TODO: Use a mesh for cocoas for perfect texture compability. ]]
+local podinfo = {
+	{	desc = S("Premature Cocoa Pod"),
+		longdesc = S("Cocoa pods grow on the side of jungle trees in 3 stages."),
+		tiles = {
+			"[combine:16x16:6,1=mcl_cocoas_cocoa_stage_0.png",
+			"[combine:16x16:6,11=mcl_cocoas_cocoa_stage_0.png",
+			"mcl_cocoas_cocoa_stage_0.png",
+			"mcl_cocoas_cocoa_stage_0.png^[transformFX",
+			"[combine:16x16:-5,0=mcl_cocoas_cocoa_stage_0.png",
+			"[combine:16x16:-5,0=mcl_cocoas_cocoa_stage_0.png",
 		},
+		n_box = {-0.125, -0.0625, 0.1875, 0.125, 0.25, 0.4375},
+		s_box = {-0.125, -0.0625, 0.1875, 0.125, 0.5,  0.5   },
 	},
-	selection_box = {
-		type = "fixed",
-		fixed = {
-			{-0.125, -0.0625, 0.1875, 0.125, 0.5, 0.5},  -- Pod
+	{	desc = S("Medium Cocoa Pod"),
+		tiles = {
+			"[combine:16x16:5,1=mcl_cocoas_cocoa_stage_1.png",
+			"[combine:16x16:5,9=mcl_cocoas_cocoa_stage_1.png",
+			"mcl_cocoas_cocoa_stage_1.png",
+			"mcl_cocoas_cocoa_stage_1.png^[transformFX",
+			"[combine:16x16:-4,0=mcl_cocoas_cocoa_stage_1.png",
+			"[combine:16x16:-4,0=mcl_cocoas_cocoa_stage_1.png",
 		},
+		n_box = {-0.1875, -0.1875, 0.0625, 0.1875, 0.25, 0.4375},
+		s_box = {-0.1875, -0.1875, 0.0625, 0.1875, 0.5,  0.5   },
 	},
-	groups = {
-		handy = 1, axey = 1,
-		dig_by_water=1, destroy_by_lava_flow=1, dig_by_piston=1,
-		attached_node_facedir=1,
-		not_in_creative_inventory=1,
-		cocoa=1
+	{	desc = S("Mature Cocoa Pod"),
+		longdesc = S("A mature cocoa pod grew on a jungle tree to its full size and it is ready to be harvested for cocoa beans. It won't grow any further."),
+		tiles = {
+		-- The following 2 textures were derived from the original
+		-- because the size of the top/bottom is slightly different :-(
+		-- TODO: Find a way to *only* use the base texture
+			"mcl_cocoas_cocoa_top_stage_2.png",
+			"mcl_cocoas_cocoa_top_stage_2.png^[transformFY",
+			"mcl_cocoas_cocoa_stage_2.png",
+			"mcl_cocoas_cocoa_stage_2.png^[transformFX",
+			"[combine:16x16:-3,0=mcl_cocoas_cocoa_stage_2.png",
+			"[combine:16x16:-3,0=mcl_cocoas_cocoa_stage_2.png",
+		},
+		n_box = {-0.25, -0.3125, -0.0625, 0.25, 0.25, 0.4375},
+		s_box = {-0.25, -0.3125, -0.0625, 0.25, 0.5,  0.5   },
 	},
-	sounds = mcl_sounds.node_sound_wood_defaults(),
-	on_rotate = false,
-	_mcl_blast_resistance = 3,
-	_mcl_hardness = 0.2,
-	_mcl_on_bonemealing = function(pointed_thing, placer)
-		local pos = pointed_thing.under
-		mcl_cocoas.grow(pos)
-		return true
+}
+
+for i = 1, 3 do
+	local def = {
+		description = podinfo[i].desc,
+		_doc_items_create_entry = true,
+		_doc_items_longdesc = podinfo[i].longdesc,
+		paramtype = "light",
+		paramtype2 = "facedir",
+		drawtype = "nodebox",
+		tiles = podinfo[i].tiles,
+		use_texture_alpha = "clip",
+		node_box = {
+			type = "fixed",
+			fixed = {
+				podinfo[i].n_box, -- Pod
+				-- FIXME: This has a thickness of 0. Is this OK in Minetest?
+				{ 0, 0.25, 0.25, 0, 0.5, 0.5 }, }, -- Stem
+		},
+		collision_box = {
+			type = "fixed",
+			fixed = podinfo[i].n_box
+		},
+		selection_box = {
+			type = "fixed",
+			fixed = podinfo[i].s_box
+		},
+		groups = {
+			handy = 1, axey = 1, attached_node_facedir = 1,
+			dig_by_water = 1, destroy_by_lava_flow = 1, dig_by_piston = 1,
+			cocoa = i, not_in_creative_inventory = 1,
+		},
+		sunlight_propagates = true,
+		walkable = true,
+		drop = "mcl_cocoas:cocoa_beans",
+		sounds = mcl_sounds.node_sound_wood_defaults(),
+		on_rotate = false,
+		_mcl_blast_resistance = 3,
+		_mcl_hardness = 0.2,
+		_mcl_on_bonemealing = function(pointed_thing, placer)
+			local pos = pointed_thing.under
+			return cocoa_grow(pos)
+		end,
+	}
+
+	if i == 2 then
+		def._doc_items_longdesc = nil
+		def._doc_items_create_entry = false
 	end
-}
+	if i == 3 then
+		def.drop = "mcl_cocoas:cocoa_beans 3"
+	end
 
--- 2nd stage
-minetest.register_node("mcl_cocoas:cocoa_1", table.copy(crop_def))
-
-crop_def.description = S("Medium Cocoa Pod")
-crop_def._doc_items_create_entry = false
-crop_def.groups.cocoa = 2
-crop_def.mesh = "mcl_cocoas_cocoa_stage_1.obj"
-crop_def.tiles = {"mcl_cocoas_cocoa_stage_1.png"}
-crop_def.collision_box = {
-	type = "fixed",
-	fixed = {
-		{-0.1875, -0.1875, 0.0625, 0.1875, 0.25, 0.4375},  -- Pod
-	},
-}
-crop_def.selection_box = {
-	type = "fixed",
-	fixed = {
-		{-0.1875, -0.1875, 0.0625, 0.1875, 0.5, 0.5},
-	},
-}
-
-minetest.register_node("mcl_cocoas:cocoa_2", table.copy(crop_def))
-
--- Final stage
-crop_def.description = S("Mature Cocoa Pod")
-crop_def._doc_items_longdesc = S("A mature cocoa pod grew on a jungle tree to its full size and it is ready to be harvested for cocoa beans. It won't grow any further.")
-crop_def._doc_items_create_entry = true
-crop_def.groups.cocoa = 3
-crop_def.mesh = "mcl_cocoas_cocoa_stage_2.obj"
-crop_def.tiles = {"mcl_cocoas_cocoa_stage_2.png"}
-crop_def.collision_box = {
-	type = "fixed",
-	fixed = {
-		{-0.25, -0.3125, -0.0625, 0.25, 0.25, 0.4375},  -- Pod
-	},
-}
-crop_def.selection_box = {
-	type = "fixed",
-	fixed = {
-		{-0.25, -0.3125, -0.0625, 0.25, 0.5, 0.5},
-	},
-}
-crop_def.drop = "mcl_cocoas:cocoa_beans 3"
-crop_def._mcl_on_bonemealing = nil
-minetest.register_node("mcl_cocoas:cocoa_3", table.copy(crop_def))
+	minetest.register_node("mcl_cocoas:cocoa_" .. i, table.copy(def))
+end
 
 minetest.register_craftitem("mcl_cocoas:cocoa_beans", {
-	description = S("Cocoa Beans"),
+	inventory_image = "mcl_cocoa_beans.png",
 	_tt_help = S("Grows at the side of jungle trees"),
-	_doc_items_longdesc = S("Cocoa beans can be used to plant cocoa, bake cookies or craft brown dye."),
-	_doc_items_usagehelp = S("Right click on the side of a jungle tree trunk (Jungle Wood) to plant a young cocoa."),
-	inventory_image = "mcl_cocoas_cocoa_beans.png",
-	groups = {craftitem = 1, compostability = 65},
+	_doc_items_longdesc = S("Cocoa beans can be used to plant cocoa, bake cookies or cract brown dye."),
+	_doc_items_usagehelp = S("Rightclick a sheep to turn its wool brown. Rightclick on the side of a jungle tree trunk (Jungle Wood) to plant a young cocoa."),
+	description = S("Cocoa Beans"),
+	stack_max = 64,
+	groups = {
+		dye = 1, craftitem = 1, compostability = 65,
+		basecolor_brown = 1, excolor_orange = 1, unicolor_dark_orange = 1,
+	},
 	on_place = function(itemstack, placer, pointed_thing)
 		return cocoa_place(itemstack, placer, pointed_thing, "mcl_cocoas:cocoa_1")
 	end,
