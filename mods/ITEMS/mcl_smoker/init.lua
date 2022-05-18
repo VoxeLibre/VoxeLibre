8426e7826a96b11301e175fb633b1284efd1fb9e
cora <coradelamouche@gmx.ch>
2022-05-18 13:30:00 +0200

    Fix smoker fuel not being burned at twice the rate.

--- mods/ITEMS/mcl_smoker/init.lua
+++ mods/ITEMS/mcl_smoker/init.lua
@@ -329,7 +329,7 @@ local function smoker_node_timer(pos, elapsed)
 		elseif active then
 			el = math.min(el, fuel_totaltime - fuel_time)
 			-- The furnace is currently active and has enough fuel
-			fuel_time = fuel_time + el
+			fuel_time = ( fuel_time + el ) * 2
 		end
 
 		-- If there is a cookable item then check if it is ready yet
