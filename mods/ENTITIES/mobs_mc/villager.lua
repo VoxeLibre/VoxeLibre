ad486a56fda60e5f40e6f867419914cb6317f550
epCode <64379263+epCode@users.noreply.github.com>
2022-06-06 12:09:14 -0700

    Add more Villager sounds

--- mods/ENTITIES/mobs_mc/villager.lua
+++ mods/ENTITIES/mobs_mc/villager.lua
@@ -1227,6 +1227,7 @@ mcl_mobs:register_mob("mobs_mc:villager", {
 	-- TODO: sounds
 	sounds = {
 		random = "mobs_mc_villager",
+		damage = "mobs_mc_villager_hurt",
 		distance = 10,
 	},
 	animation = {
