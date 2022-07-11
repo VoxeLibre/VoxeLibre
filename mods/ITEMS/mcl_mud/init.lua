408c72e4bf235852da73c019f94290067347f213
TheRandomLegoBrick <legomatthew.516@gmail.com>
2022-07-11 08:24:42 -0700

    Mud fixes

--- mods/ITEMS/mcl_mud/init.lua
+++ mods/ITEMS/mcl_mud/init.lua
@@ -11,13 +11,13 @@ minetest.register_node("mcl_mud:mud", {
 		dug = {name="mud_place_dug", gain=1},
 		place = {name="mud_place_dug", gain=1},
 	},
-	groups = {handy=1,shovely=1, enderman_takable=1, building_block=1},
+	groups = {handy=1, shovely=1, enderman_takable=1, grass_block=1, soil_sugarcane=1, building_block=1},
 	_mcl_blast_resistance = 0.5,
 	_mcl_hardness = 0.5,
 	collision_box = {
 		type = "fixed",
 		fixed = {
-			{-8 / 16, -8 / 16, -8 / 16, 8 / 16, 7 / 16, 8 / 16},
+			{-8 / 16, -8 / 16, -8 / 16, 8 / 16, 6 / 16, 8 / 16},
 		},
 	},
 })
