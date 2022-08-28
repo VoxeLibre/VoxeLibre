5520f81404ebe63af5f1def66a07c86064c68f77
cora <coradelamouche@gmx.ch>
2022-08-28 15:02:02 +0200

    Fix grindston warnings

--- mods/ITEMS/mcl_grindstone/init.lua
+++ mods/ITEMS/mcl_grindstone/init.lua
@@ -177,6 +177,18 @@ local function drop_grindstone_items(pos, meta)
 	end
 end
 
+local node_box = {
+	type = "fixed",
+	-- created with nodebox editor
+	fixed = {
+		{-0.25, -0.25, -0.375, 0.25, 0.5, 0.375},
+		{-0.375, -0.0625, -0.1875, -0.25, 0.3125, 0.1875},
+		{0.25, -0.0625, -0.1875, 0.375, 0.3125, 0.1875},
+		{0.25, -0.5, -0.125, 0.375, -0.0625, 0.125},
+		{-0.375, -0.5, -0.125, -0.25, -0.0625, 0.125},
+	}
+}
+
 minetest.register_node("mcl_grindstone:grindstone", {
 	description = S("Grindstone"),
 	_tt_help = S("Used to disenchant/fix tools"),
@@ -196,17 +208,7 @@ minetest.register_node("mcl_grindstone:grindstone", {
 	},
 	drawtype = "nodebox",
 	paramtype2 = "facedir",
-	node_box = {
-		type = "fixed",
-		-- created with nodebox editor
-		fixed = {
-			{-0.25, -0.25, -0.375, 0.25, 0.5, 0.375},
-			{-0.375, -0.0625, -0.1875, -0.25, 0.3125, 0.1875},
-			{0.25, -0.0625, -0.1875, 0.375, 0.3125, 0.1875},
-			{0.25, -0.5, -0.125, 0.375, -0.0625, 0.125},
-			{-0.375, -0.5, -0.125, -0.25, -0.0625, 0.125},
-		}
-	},
+	node_box = node_box,
 	selection_box = node_box,
 	collision_box = node_box,
 	sounds = mcl_sounds.node_sound_stone_defaults(),
