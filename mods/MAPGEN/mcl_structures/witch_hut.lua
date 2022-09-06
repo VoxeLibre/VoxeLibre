962425faa574cf268b15753d45799c8ba41c7b8a
cora <coradelamouche@gmx.ch>
2022-09-06 12:45:38 +0200

    Don't spawn structure mobs in peaceful mode

--- mods/MAPGEN/mcl_structures/witch_hut.lua
+++ mods/MAPGEN/mcl_structures/witch_hut.lua
@@ -2,14 +2,19 @@ local modname = minetest.get_current_modname()
 local S = minetest.get_translator(modname)
 local modpath = minetest.get_modpath(modname)
 
+local peaceful = minetest.settings:get_bool("only_peaceful_mobs", false)
+
 local function spawn_witch(p1,p2)
 	local c = minetest.find_node_near(p1,15,{"mcl_cauldrons:cauldron"})
 	if c then
 		local nn = minetest.find_nodes_in_area_under_air(vector.new(p1.x,c.y-1,p1.z),vector.new(p2.x,c.y-1,p2.z),{"mcl_core:sprucewood"})
-		local witch = minetest.add_entity(vector.offset(nn[math.random(#nn)],0,1,0),"mobs_mc:witch"):get_luaentity()
+		local witch
+		if not peaceful then
+			witch = minetest.add_entity(vector.offset(nn[math.random(#nn)],0,1,0),"mobs_mc:witch"):get_luaentity()
+			witch._home = c
+			witch.can_despawn = false
+		end
 		local cat = minetest.add_entity(vector.offset(nn[math.random(#nn)],0,1,0),"mobs_mc:cat"):get_luaentity()
-		witch._home = c
-		witch.can_despawn = false
 		cat.object:set_properties({textures = {"mobs_mc_cat_black.png"}})
 		cat.owner = "!witch!" --so it's not claimable by player
 		cat._home = c
