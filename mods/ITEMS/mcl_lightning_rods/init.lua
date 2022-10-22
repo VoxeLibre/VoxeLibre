e56fa0d8098aaef6b5e325aafe8cf9248f50986a
AFCMS <afcm.contact@gmail.com>
2022-10-23 00:38:56 +0200

    Lightning Rod Fixes - give node a proper nodebox - add better texture compatible with MC texture packs - set use_texture_alpha = "opaque" - fix invalid translator - disable node placement prediction - simplificate placement code

--- mods/ITEMS/mcl_lightning_rods/init.lua
+++ mods/ITEMS/mcl_lightning_rods/init.lua
@@ -1,34 +1,39 @@
-local S = minetest.get_translator("mobs_mc")
+local S = minetest.get_translator("mcl_lightning_rods")
 
+---@type nodebox
 local cbox = {
 	type = "fixed",
 	fixed = {
-		{ 0/16, -8/16, 0/16,  2/16, 4/16,  2/16 },
-		{ 0/16, 4/16, 0/16,  3/16,  8/16,  3/16 },
-	}
+		{ -0.0625, -0.5, -0.0625, 0.0625, 0.25, 0.0625 },
+		{ -0.125, 0.25, -0.125, 0.125, 0.5, 0.125 },
+	},
 }
+
+local text_top = "[combine:16x16:6,6=mcl_lightning_rods_rod.png"
+local text_side = "[combine:16x16:7,0=mcl_lightning_rods_rod.png:-6,0=mcl_lightning_rods_rod.png\\^[transformR270"
 
 minetest.register_node("mcl_lightning_rods:rod", {
 	description = S("Lightning Rod"),
 	_doc_items_longdesc = S("A block that attracts lightning"),
-	--inventory_image = "mcl_lightning_rods_rod_inv.png",
 	tiles = {
-		"mcl_lightning_rods_rod.png",
-		"mcl_lightning_rods_rod.png",
-		"mcl_lightning_rods_rod.png",
-		"mcl_lightning_rods_rod.png",
-		"mcl_lightning_rods_rod.png",
-		"mcl_lightning_rods_rod.png",
+		text_top,
+		text_top,
+		text_side,
+		text_side,
+		text_side,
+		text_side,
 	},
 	drawtype = "nodebox",
 	is_ground_content = false,
 	paramtype = "light",
 	paramtype2 = "facedir",
-	groups = {pickaxey=2,attracts_lightning=1},
+	use_texture_alpha = "opaque",
+	groups = { pickaxey = 2, attracts_lightning = 1 },
 	sounds = mcl_sounds.node_sound_metal_defaults(),
 	node_box = cbox,
 	selection_box = cbox,
 	collision_box = cbox,
+	node_placement_prediction = "",
 	on_place = function(itemstack, placer, pointed_thing)
 		if pointed_thing.type ~= "node" then
 			return itemstack
@@ -40,12 +45,7 @@ minetest.register_node("mcl_lightning_rods:rod", {
 
 		local placer_pos = placer:get_pos()
 		if placer_pos then
-			local dir = {
-				x = p1.x - placer_pos.x,
-				y = p1.y - placer_pos.y,
-				z = p1.z - placer_pos.z
-			}
-			param2 = minetest.dir_to_facedir(dir)
+			param2 = minetest.dir_to_facedir(vector.subtract(p1, placer_pos))
 		end
 
 		if p0.y - 1 == p1.y then
@@ -63,20 +63,19 @@ minetest.register_node("mcl_lightning_rods:rod", {
 		return minetest.item_place(itemstack, placer, pointed_thing, param2)
 	end,
 
-	sounds = mcl_sounds.node_sound_glass_defaults(),
 	_mcl_blast_resistance = 0,
 })
 
-lightning.register_on_strike(function(pos,pos2,objects)
-	local lr = minetest.find_node_near(pos,128,{"group:attracts_lightning"},true)
-	return lr,nil
+lightning.register_on_strike(function(pos, pos2, objects)
+	local lr = minetest.find_node_near(pos, 128, { "group:attracts_lightning" }, true)
+	return lr, nil
 end)
 
 minetest.register_craft({
 	output = "mcl_lightning_rods:rod",
 	recipe = {
-		{"", "mcl_copper:copper_ingot",""},
-		{"", "mcl_copper:copper_ingot",""},
-		{"", "mcl_copper:copper_ingot",""},
-	}
+		{ "", "mcl_copper:copper_ingot", "" },
+		{ "", "mcl_copper:copper_ingot", "" },
+		{ "", "mcl_copper:copper_ingot", "" },
+	},
 })
