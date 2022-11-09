3bbae86bafd476dc4838987381dc7dcb91ae130d
AFCMS <afcm.contact@gmail.com>
2022-11-09 20:48:51 +0100

    Fix this stupid merge conflict

--- mods/PLAYER/mcl_gamemode/init.lua
+++ mods/PLAYER/mcl_gamemode/init.lua
@@ -69,7 +69,7 @@ minetest.register_on_punchnode(function(pos, node, puncher, pointed_thing)
 	local def = minetest.registered_nodes[node.name]
 	if def then
 		if def.on_destruct then def.on_destruct(pos) end
-		minetest.dig_node(pos)
+		minetest.node_dig(pos, node, puncher)
 		return true
 	end
 end)
