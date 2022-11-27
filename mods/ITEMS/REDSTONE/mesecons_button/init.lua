8d5aea9743dddb3da30068c7f29efe7084774985
PrairieWind <prairie.astronomer1@gmail.com>
2022-11-26 20:34:04 -0700

    Fix New Button Names

--- mods/ITEMS/REDSTONE/mesecons_button/init.lua
+++ mods/ITEMS/REDSTONE/mesecons_button/init.lua
@@ -224,9 +224,9 @@ local woods = {
 	{ "sprucewood", "mcl_core:sprucewood", "mcl_core_planks_spruce.png", S("Spruce Button") },
 	{ "junglewood", "mcl_core:junglewood", "default_junglewood.png", S("Jungle Button") },
 
-	{ "mangrove_wood", "mcl_mangrove:mangrove_wood", "mcl_mangrove_planks.png", S("Mangrove Pressure Plate") },
-	{ "crimson_hyphae_wood", "mcl_crimson:crimson_hyphae_wood", "crimson_hyphae_wood.png", S("Crimson Pressure Plate") },
-	{ "warped_hyphae_wood", "mcl_crimson:warped_hyphae_wood", "warped_hyphae_wood.png", S("Warped Pressure Plate") },
+	{ "mangrove_wood", "mcl_mangrove:mangrove_wood", "mcl_mangrove_planks.png", S("Mangrove Button") },
+	{ "crimson_hyphae_wood", "mcl_crimson:crimson_hyphae_wood", "crimson_hyphae_wood.png", S("Crimson Button") },
+	{ "warped_hyphae_wood", "mcl_crimson:warped_hyphae_wood", "warped_hyphae_wood.png", S("Warped Button") },
 }
 
 for w=1, #woods do
