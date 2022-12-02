7f59611914e68fd2c996a7e7026ef4291212b341
chmodsayshello <chmodsayshello@hotmail.com>
2022-12-02 15:40:18 +0100

    add crafting recipie

--- mods/ITEMS/mcl_compass/init.lua
+++ mods/ITEMS/mcl_compass/init.lua
@@ -271,6 +271,16 @@ minetest.register_craft({
 	}
 })
 
+minetest.register_craft({ --TODO: update once echo shards are a thing
+	output = "mcl_compass:" .. random_frame .. "_recovery",
+	recipe = {
+		{"","mcl_nether:netherite_ingot",""},
+		{"mcl_core:diamondblock","mcl_compass:" .. stereotype_frame ,"mcl_core:diamondblock"},
+		{"mcl_core:diamondblock","mcl_core:diamondblock","mcl_core:diamondblock"}
+
+	}
+})
+
 minetest.register_alias("mcl_compass:compass", "mcl_compass:" .. stereotype_frame)
 
 
