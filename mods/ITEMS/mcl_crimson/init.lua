b3a8ba57e73c1440595309355ae56b754fc4a333
Wbjitscool <wilsonjonathan131@gmail.com>
2022-12-19 00:43:32 +0000

    update 'mods/items/mcl_crimson/init.lua'
    some new changes

--- mods/ITEMS/mcl_crimson/init.lua
+++ mods/ITEMS/mcl_crimson/init.lua
@@ -307,10 +307,10 @@ minetest.register_node("mcl_crimson:warped_nylium", {
 		"mcl_nether_netherrack.png^warped_nylium_side.png",
 		"mcl_nether_netherrack.png^warped_nylium_side.png",
 	},
+	paramtype2 = "facedir",
 	is_ground_content = true,
 	drop = "mcl_nether:netherrack",
 	groups = {pickaxey=1, building_block=1, material_stone=1},
-	sounds = mcl_sounds.node_sound_stone_defaults(),
 	_mcl_hardness = 0.4,
 	_mcl_blast_resistance = 0.4,
 	_mcl_silk_touch_drop = true,
@@ -559,7 +559,7 @@ minetest.register_node("mcl_crimson:crimson_nylium", {
 		"mcl_nether_netherrack.png^crimson_nylium_side.png",
 	},
 	groups = {pickaxey = 1, building_block = 1, material_stone = 1},
-	sounds = mcl_sounds.node_sound_stone_defaults(),
+	paramtype2 = "facedir",
 	is_ground_content = true,
 	drop = "mcl_nether:netherrack",
 	_mcl_hardness = 0.4,
@@ -605,8 +605,8 @@ mcl_doors:register_door("mcl_crimson:crimson_door", {
 	groups = {handy=1,axey=1, material_wood=1, flammable=-1},
 	_mcl_hardness = 3,
 	_mcl_blast_resistance = 3,
-	tiles_bottom = {"mcl_crimson_crimson_door_bottom.png", "crimson_hyphae_wood.png"},
-	tiles_top = {"mcl_crimson_crimson_door_top.png", "crimson_hyphae_wood.png"},
+	tiles_bottom = {"mcl_crimson_crimson_door_bottom.png", "mcl_doors_door_crimson_side_lower.png"},
+	tiles_top = {"mcl_crimson_crimson_door_top.png", "mcl_doors_door_crimson_side_upper.png"},
 	sounds = mcl_sounds.node_sound_wood_defaults(),
 })
 
@@ -643,8 +643,8 @@ mcl_doors:register_door("mcl_crimson:warped_door", {
 	groups = {handy=1,axey=1, material_wood=1, flammable=-1},
 	_mcl_hardness = 3,
 	_mcl_blast_resistance = 3,
-	tiles_bottom = {"mcl_crimson_warped_door_bottom.png", "warped_hyphae_wood.png"},
-	tiles_top = {"mcl_crimson_warped_door_top.png", "warped_hyphae_wood.png"},
+	tiles_bottom = {"mcl_crimson_warped_door_bottom.png", "mcl_doors_door_warped_side_lower.png"},
+	tiles_top = {"mcl_crimson_warped_door_top.png", "mcl_doors_door_warped_side_upper.png"},
 	sounds = mcl_sounds.node_sound_wood_defaults(),
 })
 
@@ -741,3 +741,4 @@ minetest.register_craft({
 		{"mcl_core:stick", warped_wood, "mcl_core:stick"},
 	}
 })
+
