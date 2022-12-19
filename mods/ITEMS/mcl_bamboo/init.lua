f737df79e5992d0168ef868d39c79dd3bd22689a
Michieal <michieal@noreply.git.minetest.land>
2022-12-19 04:06:33 +0000

    revert 7a3f37342544f7f6b18da787834ee056c89f19ac
    revert Misc. Fixes.
    Added in burn times for slabs.
    And then amend the commit to remove an oops.

--- mods/ITEMS/mcl_bamboo/init.lua
+++ mods/ITEMS/mcl_bamboo/init.lua
@@ -729,6 +729,23 @@ local function register_craftings()
 		burntime = 7.5,
 	})
 
+
+	minetest.register_craft({
+		type = "fuel",
+		recipe = "mcl_stairs:slab_bamboo_plank",
+		burntime = 7.5,
+	})
+	minetest.register_craft({
+		type = "fuel",
+		recipe = "mcl_stairs:slab_bamboo_block",
+		burntime = 7.5,
+	})
+	minetest.register_craft({
+		type = "fuel",
+		recipe = "mcl_stairs:slab_bamboo_stripped",
+		burntime = 7.5,
+	})
+
 end
 
 create_nodes()
