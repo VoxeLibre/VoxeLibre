17e02aec3c96bbfd21e7ecfa1e13ca86977665fd
CyberMango <Cyber@Mango.fakeemail.com>
2023-01-01 23:02:38 +0200

    Applying bone meal on a stage 3 sweet berry no longer grows it.
    It now ignores the bone meal and harvests the sweet berry as normal.

--- mods/ITEMS/mcl_farming/sweet_berry.lua
+++ mods/ITEMS/mcl_farming/sweet_berry.lua
@@ -51,7 +51,8 @@ for i=0, 3 do
 				minetest.record_protection_violation(pos, pn)
 				return itemstack
 			end
-			if mcl_dye and clicker:get_wielded_item():get_name() == "mcl_bone_meal:bone_meal" then
+			if 3 ~= i and mcl_dye and 
+					clicker:get_wielded_item():get_name() == "mcl_bone_meal:bone_meal" then
 				mcl_dye.apply_bone_meal({under=pos},clicker)
 				if not minetest.is_creative_enabled(pn) then
 					itemstack:take_item()
