36231241baec051c5b436692c9f248ce01ff53b8
SmokeyDope <smokey@tilde.team>
2023-01-23 00:06:46 +0000

    make hoes effective on warped wart blocks and shroomlight blocks

--- mods/ITEMS/mcl_crimson/init.lua
+++ mods/ITEMS/mcl_crimson/init.lua
@@ -264,14 +264,14 @@ mcl_flowerpots.register_potted_flower("mcl_crimson:warped_roots", {
 minetest.register_node("mcl_crimson:warped_wart_block", {
 	description = S("Warped Wart Block"),
 	tiles = {"warped_wart_block.png"},
-	groups = {handy = 1, hoe = 7, swordy = 1, deco_block = 1},
+	groups = {handy = 1, hoey = 7, swordy = 1, deco_block = 1},
 	_mcl_hardness = 2,
 })
 
 minetest.register_node("mcl_crimson:shroomlight", {
 	description = S("Shroomlight"),
 	tiles = {"shroomlight.png"},
-	groups = {handy = 1, hoe = 7, swordy = 1, deco_block = 1},
+	groups = {handy = 1, hoey = 7, swordy = 1, deco_block = 1},
 	light_source = minetest.LIGHT_MAX,
 	_mcl_hardness = 2,
 })
