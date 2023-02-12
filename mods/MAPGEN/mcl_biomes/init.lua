5da6c25ccf1aee5cece9935c6fc64e1ecc03b37d
PrairieWind <prairie.astronomer1@gmail.com>
2023-02-12 13:59:52 -0700

    Fix cornflower biomes and change seed values for cornflowers and lilies of the valley

--- mods/MAPGEN/mcl_biomes/init.lua
+++ mods/MAPGEN/mcl_biomes/init.lua
@@ -5288,8 +5288,8 @@ local function register_decorations()
 	register_flower("allium", nil, 0) -- flower Forest only
 	register_flower("blue_orchid", {"Swampland"}, 64500, false)
 
-	register_flower("lily_of_the_valley", nil, 436)
-	register_flower("cornflower", {"Plains", "SunflowerPlains"}, 536)
+	register_flower("lily_of_the_valley", nil, 325)
+	register_flower("cornflower", flower_biomes2, 486)
 end
 
 -- Decorations in non-Overworld dimensions
