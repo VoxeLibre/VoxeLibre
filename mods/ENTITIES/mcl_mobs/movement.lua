df8592df41cc8c902aca128f9932863e0cb61f4b
epCode <eeliperry@gmail.com>
2023-06-04 21:53:39 -0700

    Revert add punch node

--- mods/ENTITIES/mcl_mobs/movement.lua
+++ mods/ENTITIES/mcl_mobs/movement.lua
@@ -525,7 +525,6 @@ function mob_class:replace_node(pos)
 				minetest.after(self.replace_delay, function()
 					if self and self.object and self.object:get_velocity() and self.health > 0 then
 						minetest.set_node(pos, newnode)
-						minetest.punch_node(pos)
 					end
 				end)
 			end
