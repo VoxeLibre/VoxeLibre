10441637176783f59d9a5411d6428d61f5353611
the-real-herowl <wiktor_t-i@proton.me>
2023-11-27 02:22:39 +0000

    Fixed the ambiguous /clear command being dangerous (#4026)
    Implements #3826
    
    Reviewed-on: https://git.minetest.land/MineClone2/MineClone2/pulls/4026
    Co-authored-by: the-real-herowl <wiktor_t-i@proton.me>
    Co-committed-by: the-real-herowl <wiktor_t-i@proton.me>

--- mods/MISC/mcl_commands/alias.lua
+++ mods/MISC/mcl_commands/alias.lua
@@ -18,13 +18,23 @@ if minetest.settings:get_bool("mcl_builtin_commands_overide", true) then
 	register_chatcommand_alias("tell", "msg")
 	register_chatcommand_alias("w", "msg")
 	register_chatcommand_alias("tp", "teleport")
-	rename_chatcommand("clear", "clearinv")
+	register_chatcommand_alias("clearinventory", "clearinv")
 
 	minetest.register_chatcommand("banlist", {
 		description = S("List bans"),
 		privs = minetest.chatcommands["ban"].privs,
 		func = function(name)
 			return true, S("Ban list: @1", minetest.get_ban_list())
+		end,
+	})
+
+	minetest.register_chatcommand("clear", {
+		description = S("List clear commands"),
+		func = function(name)
+			return true, S("To clear inventory use /clearinv or /clearinventory").."\n"..
+						 S("To clear mobs use /clearmobs").."\n"..
+						 S("To clear the weather use /weather clear").."\n"..
+						 S("Clearing the chat is not possible, you can hide the chat using \"Toggle chat log\" key (default F2) on PC or the chat icon on the mobile version")
 		end,
 	})
 end
