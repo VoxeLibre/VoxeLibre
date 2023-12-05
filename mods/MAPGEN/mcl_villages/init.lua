4127d120d25a7fa18dfd1d49feaf0ef2110489d6
Eliy21 <eliy21@noreply.git.minetest.land>
2023-12-05 12:49:49 +0000

    Add server privs restriction to mcl_villages build tool (#4043)
    <!--
    Please follow our contributing guidelines first:
    https://git.minetest.land/MineClone2/MineClone2/src/branch/master/CONTRIBUTING.md#how-you-can-help-as-a-programmer
    
    By submitting this pull request, you agree to follow our Code of Conduct:
    https://git.minetest.land/MineClone2/MineClone2/src/branch/master/CODE_OF_CONDUCT.md
    -->
    
    Fixes #4022
    
    ### Testing
    -Make a new world, check host server and go into creative mode
    -Since you're the server you have the server privs so get the mcl_villages build tool item in creative inventory and use it to see if it works
    -Have someone join your server and give them the same item to use and see if they are restricted from using it
    
    Reviewed-on: https://git.minetest.land/MineClone2/MineClone2/pulls/4043
    Reviewed-by: chmodsayshello <chmodsayshello@hotmail.com>
    Co-authored-by: Eliy21 <eliy21@noreply.git.minetest.land>
    Co-committed-by: Eliy21 <eliy21@noreply.git.minetest.land>

--- mods/MAPGEN/mcl_villages/init.lua
+++ mods/MAPGEN/mcl_villages/init.lua
@@ -128,6 +128,10 @@ if minetest.is_creative_enabled("") then
 		-- build ssettlement
 		on_place = function(itemstack, placer, pointed_thing)
 			if not pointed_thing.under then return end
+			if not minetest.check_player_privs(placer, "server") then
+				minetest.chat_send_player(placer:get_player_name(), S("Placement denied. You need the “server” privilege to place villages."))
+				return
+			end
 			local minp = vector.subtract(	pointed_thing.under, half_map_chunk_size)
 		        local maxp = vector.add(	pointed_thing.under, half_map_chunk_size)
 			build_a_settlement(minp, maxp, math.random(0,32767))
