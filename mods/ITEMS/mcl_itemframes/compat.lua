04de7cd9b04d27cf436a2e924445f006ed530e1d
cora <coradelamouche@gmx.ch>
2024-01-17 01:58:28 +0100

    Make compat conversion more reliable, convert facedir to wallmounted p2

--- mods/ITEMS/mcl_itemframes/compat.lua
+++ mods/ITEMS/mcl_itemframes/compat.lua
@@ -1,4 +1,3 @@
-
 local entity_stub = {
 	on_activate = function(self)
 		local pos = self.object:get_pos()
@@ -7,6 +6,24 @@ local entity_stub = {
 	end
 }
 
-minetest.register_entity("mcl_itemframes:map", entity_stub)
-minetest.register_entity("mcl_itemframes:glow_item", entity_stub)
-minetest.register_entity("mcl_itemframes:glow_map", entity_stub)
+minetest.register_entity("mcl_itemframes:item_frame_item", entity_stub)
+minetest.register_entity("mcl_itemframes:item_frame_map", entity_stub)
+minetest.register_entity("mcl_itemframes:glow_item_frame_item", entity_stub)
+minetest.register_entity("mcl_itemframes:glow_item_frame_map", entity_stub)
+
+minetest.register_alias("mcl_itemframes:item_frame", "mcl_itemframes:frame")
+minetest.register_alias("mcl_itemframes:glow_item_frame", "mcl_itemframes:glow_frame")
+
+minetest.register_lbm({
+	label = "Convert old itemframes",
+	name = "mcl_itemframes:convert_old_itemframes",
+	nodenames = { "mcl_itemframes:item_frame", "mcl_itemframes:glow_item_frame" },
+	run_at_every_load = false,
+	action = function(pos, node)
+		node.name = node.name:gsub("item_","")
+		node.param2 = minetest.dir_to_wallmounted(minetest.facedir_to_dir(node.param2))
+		minetest.swap_node(pos, node)
+		mcl_itemframes.remove_entity(pos)
+		mcl_itemframes.update_entity(pos)
+	end
+})
