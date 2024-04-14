312ad5b63bc26bf6c5d9337ed47e1f2838bd888e
Mikita Wiśniewski <rudzik8@protonmail.com>
2024-04-14 13:30:44 +0700

    Fix creative inventory search not working on Android (fixes #3402)

--- mods/HUD/mcl_inventory/creative.lua
+++ mods/HUD/mcl_inventory/creative.lua
@@ -595,6 +595,7 @@ function mcl_inventory.set_creative_formspec(player)
 		formspec = formspec .. table.concat({
 			"field[5.325,0.15;6.1,0.6;search;;" .. minetest.formspec_escape(filter) .. "]",
 			"field_close_on_enter[search;false]",
+			"field_enter_after_edit[search;true]",
 			"set_focus[search;true]",
 		})
 	end
