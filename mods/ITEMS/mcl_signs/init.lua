55c87966a6f3aeb9452d5a9e7472588a181e2d93
cora <coradelamouche@gmx.ch>
2024-06-12 23:53:33 +0200

    Fix crash when signs text get_properties returns are nil

--- mods/ITEMS/mcl_signs/init.lua
+++ mods/ITEMS/mcl_signs/init.lua
@@ -390,7 +390,8 @@ minetest.register_entity("mcl_signs:text", {
 	on_activate = function(self, staticdata)
 		local pos = self.object:get_pos()
 		mcl_signs.update_sign(pos)
-		local t = self.object:get_properties().textures
+		local props = self.object:get_properties()
+		local t = props and props.textures
 		if type(t) ~= "table" or #t == 0 then self.object:remove() end
 	end,
 })
