af30f8d6d8bca3a2dfdc69d1a17aff2d6e2c60b2
teknomunk <teknomunk@protonmail.com>
2024-06-18 19:35:03 -0500

    Implement respawnBlocksExplode gamerule

--- mods/CORE/vl_tuning/settings.lua
+++ mods/CORE/vl_tuning/settings.lua
@@ -1,6 +1,12 @@
+local modname = minetest.get_current_modname()
+local S = minetest.get_translator(modname)
 local mod = vl_tuning
 
 mod.keep_inventory = vl_tuning.setting("gamerule:keepInventory", "bool", {
 	default = minetest.settings:get_bool("mcl_keepInventory", false),
+})
+mod.respawn_blocks_explode = vl_tuning.setting("gamerule:respawnBlocksExplode", "bool", {
+	description = S("Prevents beds/respawn anchors from exploding in other dimensions."),
+	default = true,
 })
 
