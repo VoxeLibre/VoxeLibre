e407e2e290e5e4c84613e017d8de2a1943910912
teknomunk <teknomunk@protonmail.com>
2024-06-21 19:47:58 -0500

    Force has_mcl_potions to boolean

--- mods/ENVIRONMENT/mcl_weather/skycolor/effects.lua
+++ mods/ENVIRONMENT/mcl_weather/skycolor/effects.lua
@@ -5,7 +5,7 @@ local DIM_ALLOW_NIGHT_VISION = {
 
 local NIGHT_VISION_RATIO = mcl_weather.skycolor.NIGHT_VISION_RATIO
 local effects_handlers = {}
-local has_mcl_potions = minetest.get_modpath("mcl_potions")
+local has_mcl_potions = mcl_util.to_bool(minetest.get_modpath("mcl_potions"))
 
 function effects_handlers.darkness(player, meta, effect, sky_data)
 	-- No darkness effect if visited by shepherd
