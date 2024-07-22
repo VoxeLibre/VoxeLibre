247e888e71d9c5cf6230358d0727fe7c0d16c07d
kno10 <erich.schubert@gmail.com>
2024-07-23 01:09:17 +0200

    more voxelmanipulator, more MCLA

--- mods/MAPGEN/mcl_villages/const.lua
+++ mods/MAPGEN/mcl_villages/const.lua
@@ -1,28 +1,9 @@
 -- switch for debugging
 function mcl_villages.debug(message)
-	-- minetest.chat_send_all(message)
-	-- minetest.log("warning", "[mcl_villages] "..message)
 	minetest.log("verbose", "[mcl_villages] "..message)
 end
 
---[[ Manually set in 'buildings.lua'
--- material to replace cobblestone with
-local wallmaterial = {
-	"mcl_core:junglewood",
-	"mcl_core:sprucewood",
-	"mcl_core:wood",
-	"mcl_core:birchwood",
-	"mcl_core:acaciawood",
-	"mcl_core:stonebrick",
-	"mcl_core:cobble",
-	"mcl_core:sandstonecarved",
-	"mcl_core:sandstone",
-	"mcl_core:sandstonesmooth2"
-}
---]]
---
 -- possible surfaces where buildings can be built
---
 mcl_villages.surface_mat = {}
 mcl_villages.surface_mat["mcl_core:andesite"] = true
 mcl_villages.surface_mat["mcl_core:diorite"] = true
@@ -40,7 +21,7 @@ mcl_villages.surface_mat["mcl_core:sandstone"] = true
 mcl_villages.surface_mat["mcl_core:sandstonesmooth"] = true
 mcl_villages.surface_mat["mcl_core:sandstonesmooth2"] = true
 --mcl_villages.surface_mat["mcl_core:silver_sand"] = true
-mcl_villages.surface_mat["mcl_core:snow"] = true
+--mcl_villages.surface_mat["mcl_core:snow"] = true
 mcl_villages.surface_mat["mcl_core:stone"] = true
 mcl_villages.surface_mat["mcl_core:stone_with_coal"] = true
 mcl_villages.surface_mat["mcl_core:stone_with_iron"] = true
@@ -49,68 +30,10 @@ mcl_villages.surface_mat["mcl_colorblocks:hardened_clay_orange"] = true
 mcl_villages.surface_mat["mcl_colorblocks:hardened_clay_red"] = true
 mcl_villages.surface_mat["mcl_colorblocks:hardened_clay_white"] = true
 
---
--- path to schematics
---
-schem_path = mcl_villages.modpath.."/schematics/"
---
--- list of schematics
---
-local basic_pseudobiome_villages = minetest.settings:get_bool("basic_pseudobiome_villages", true)
-
-mcl_villages.schematic_table = {
-	{name = "belltower",	mts = schem_path.."new_villages/belltower.mts",	hwidth = 9, hdepth = 9, hheight =  7, hsize = 12, max_num = 0.01 , rplc = basic_pseudobiome_villages, yadjust = 1, yadjust = 1 },
-	--{name = "old_belltower",	mts = schem_path.."belltower.mts",	hwidth = 5, hdepth = 5, hheight =  6, hsize = 8, max_num = 0, rplc = basic_pseudobiome_villages, yadjust = 1 },
-	--{name = "large_house",	mts = schem_path.."large_house.mts",	hwidth = 12, hdepth = 12, hheight =  10, hsize = 18, max_num = 0.08 , rplc = basic_pseudobiome_villages },
-	--{name = "blacksmith",	mts = schem_path.."blacksmith.mts",	hwidth = 8, hdepth = 11, hheight = 8, hsize = 15, max_num = 0.01 , rplc = basic_pseudobiome_villages },
-	{name = "new_blacksmith",	mts = schem_path.."new_villages/blacksmith.mts",	hwidth = 9, hdepth = 11, hheight = 8, hsize = 15, max_num = 0.01 , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "weaponsmith",	mts = schem_path.."new_villages/weaponsmith.mts",	hwidth = 11, hdepth = 9, hheight = 6, hsize = 15, max_num = 0.01 , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "toolsmith",	mts = schem_path.."new_villages/toolsmith.mts",	hwidth = 9, hdepth = 11, hheight = 6, hsize = 15, max_num = 0.01 , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "tannery",	mts = schem_path.."new_villages/leather_worker.mts",	hwidth = 8, hdepth = 8, hheight = 7, hsize = 12, max_num = 0.01 , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	--{name = "butcher",	mts = schem_path.."butcher.mts",	hwidth = 12, hdepth =  8, hheight = 10, hsize = 15, max_num = 0.01 , rplc = basic_pseudobiome_villages },
-	--{name = "church",	mts = schem_path.."church.mts",		hwidth = 13, hdepth = 14, hheight = 15, hsize = 20, max_num = 0.01 , rplc = basic_pseudobiome_villages },
-	{name = "newchurch",	mts = schem_path.."new_villages/church.mts",		hwidth = 14, hdepth = 16, hheight = 13, hsize = 22, max_num = 0.01 , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "chapel",	mts = schem_path.."new_villages/chapel.mts",		hwidth = 9, hdepth = 10, hheight = 6, hsize = 14, max_num = 0.01 , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	--{name = "farm",		mts = schem_path.."farm.mts",		hwidth =  9, hdepth =  7, hheight = 8, hsize = 12, max_num = 0.1  , rplc = basic_pseudobiome_villages, yadjust = 0 },
-	--{name = "lamp",		mts = schem_path.."lamp.mts",		hwidth =  3, hdepth =  4, hheight = 6, hsize = 6, max_num = 0.001  , rplc = false                      },
-	{name = "lamp_1",	mts = schem_path.."new_villages/lamp_1.mts",	hwidth =  1, hdepth =  1, hheight = 4, hsize = 4, max_num = 0.001  , rplc = false, yadjust = 1 },
-	{name = "lamp_2",	mts = schem_path.."new_villages/lamp_2.mts",	hwidth =  1, hdepth =  2, hheight = 6, hsize = 5, max_num = 0.001  , rplc = false, yadjust = 1 },
-	{name = "lamp_3",	mts = schem_path.."new_villages/lamp_3.mts",	hwidth =  3, hdepth =  3, hheight = 4, hsize = 6, max_num = 0.001  , rplc = false, yadjust = 1 },
-	{name = "lamp_4",	mts = schem_path.."new_villages/lamp_4.mts",	hwidth =  1, hdepth =  2, hheight = 5, hsize = 5, max_num = 0.001  , rplc = false, yadjust = 1 },
-	{name = "lamp_5",	mts = schem_path.."new_villages/lamp_5.mts",	hwidth =  1, hdepth =  1, hheight = 2, hsize = 4, max_num = 0.001  , rplc = false, yadjust = 1 },
-	{name = "lamp_6",	mts = schem_path.."new_villages/lamp_6.mts",	hwidth =  1, hdepth =  1, hheight = 3, hsize = 4, max_num = 0.001  , rplc = false, yadjust = 1 },
-	--{name = "library",	mts = schem_path.."library.mts",	hwidth = 12, hdepth = 12, hheight =  9, hsize = 18, max_num = 0.01 , rplc = basic_pseudobiome_villages },
-	{name = "newlibrary",	mts = schem_path.."new_villages/library.mts",	hwidth = 14, hdepth = 14, hheight =  7, hsize = 21, max_num = 0.01 , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	--{name = "medium_house",	mts = schem_path.."medium_house.mts",	hwidth =  9, hdepth = 12, hheight =  9, hsize = 16, max_num = 0.08 , rplc = basic_pseudobiome_villages },
-	--{name = "small_house",	mts = schem_path.."small_house.mts",	hwidth =  9, hdepth =  8, hheight =  9, hsize = 13, max_num = 0.3  , rplc = basic_pseudobiome_villages },
-	{name = "house_1_bed",	mts = schem_path.."new_villages/house_1_bed.mts",	hwidth =  9, hdepth =  8, hheight =  7, hsize = 13, max_num = 0.3  , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "house_2_bed",	mts = schem_path.."new_villages/house_2_bed.mts",	hwidth =  11, hdepth =  8, hheight =  7, hsize = 15, max_num = 0.2  , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "house_3_bed",	mts = schem_path.."new_villages/house_3_bed.mts",	hwidth =  11, hdepth =  13, hheight =  9, hsize = 18, max_num = 0.1  , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "house_4_bed",	mts = schem_path.."new_villages/house_4_bed.mts",	hwidth =  11, hdepth =  13, hheight =  10, hsize = 18, max_num = 0.1  , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "mason",	mts = schem_path.."new_villages/mason.mts",	hwidth =  8, hdepth =  8, hheight =  7, hsize = 12, max_num = 0.01  , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "mill",		mts = schem_path.."new_villages/mill.mts",	hwidth =  8, hdepth =  8, hheight =  7, hsize = 12, max_num = 0.01  , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "cartographer",	mts = schem_path.."new_villages/cartographer.mts",	hwidth =  9, hdepth = 12, hheight =  6, hsize = 16, max_num = 0.01  , rplc = basic_pseudobiome_villages, yadjust = 2 },
-	{name = "fletcher",	mts = schem_path.."new_villages/fletcher.mts",	hwidth =  8, hdepth =  8, hheight =  7, hsize = 12, max_num = 0.01  , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "new_butcher",	mts = schem_path.."new_villages/butcher.mts",	hwidth =  8, hdepth = 14, hheight =  9, hsize = 17, max_num = 0.01  , rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "fish_farm",	mts = schem_path.."new_villages/fishery.mts",	hwidth =  10, hdepth =  7, hheight =  9, hsize = 13, max_num = 0.01  , rplc = basic_pseudobiome_villages, yadjust=-2 },
-	--{name = "tavern",	mts = schem_path.."tavern.mts",		hwidth = 12, hdepth = 10, hheight = 13, hsize = 17, max_num = 0.050, rplc = basic_pseudobiome_villages },
-	--{name = "well",		mts = schem_path.."well.mts",		hwidth =  6, hdepth =  8, hheight =  7, hsize = 11, max_num = 0.01, rplc = basic_pseudobiome_villages },
-	{name = "new_well",	mts = schem_path.."new_villages/well.mts",	hwidth =  6, hdepth =  6, hheight =  8, hsize = 9, max_num = 0.01, rplc = basic_pseudobiome_villages, yadjust=-1 },
-	{name = "new_farm",	mts = schem_path.."new_villages/farm.mts", hwidth=10, hdepth=9, hheight=6, hsize=14, max_num = 0.1, rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "farm_small",	mts = schem_path.."new_villages/farm_small_1.mts", hwidth=10, hdepth=9, hheight=6, hsize=14, max_num = 0.1, rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "farm_small2",	mts = schem_path.."new_villages/farm_small_2.mts", hwidth=9, hdepth=9, hheight=3, hsize=14, max_num = 0.1, rplc = basic_pseudobiome_villages, yadjust = 1 },
-	{name = "farm_large",	mts = schem_path.."new_villages/farm_large_1.mts", hwidth=13, hdepth=13, hheight=4, hsize=19, max_num = 0.1, rplc = basic_pseudobiome_villages, yadjust = 1 },
-}
-
---
 -- maximum allowed difference in height for building a settlement
---
-max_height_difference = 56
---
---
---
-half_map_chunk_size = 40
---quarter_map_chunk_size = 20
+mcl_villages.max_height_difference = 56
+
+mcl_villages.half_map_chunk_size = 40
 
 --
 -- Biome based block substitutions
@@ -156,14 +79,14 @@ mcl_villages.biome_map = {
 
 	CherryGrove = "cherry",
 
-	-- no change, but try to convert MCLA material
-	-- FlowerForest = "oak",
-	-- Forest = "oak",
-	-- MushroomIsland = "oak",
-	-- Plains = "oak",
-	-- StoneBeach = "oak",
-	-- SunflowerPlains = "oak",
-	-- Swampland = "oak",
+	-- no change
+	--FlowerForest = "oak",
+	--Forest = "oak",
+	--MushroomIsland = "",
+	--Plains = "oak",
+	--StoneBeach = "",
+	--SunflowerPlains = "oak",
+	--Swampland = "oak",
 }
 
 mcl_villages.vl_to_mcla = {
@@ -190,8 +113,8 @@ mcl_villages.vl_to_mcla = {
 }
 mcl_villages.mcla_to_vl = {
 	-- oneway
-	{ '"mcl_villages:no_paths"', '"air"'}, -- TODO: support these
-	{ '"mcl_villages:path_endpoint"', '"air"'}, -- TODO: support these
+	--{ '"mcl_villages:no_paths"', '"air"'}, -- TODO: support these
+	--{ '"mcl_villages:path_endpoint"', '"air"'}, -- TODO: support these
 	{ '"mcl_villages:crop_root', '"mcl_farming:potato'}, -- TODO: support biome specific farming
 	{ '"mcl_villages:crop_grain', '"mcl_farming:wheat'}, -- TODO: support biome specific farming
 	{ '"mcl_villages:crop_gourd', '"mcl_farming:pumpkin'}, -- TODO: support biome specific farming
@@ -228,7 +151,7 @@ mcl_villages.mcla_to_vl = {
 }
 mcl_villages.material_substitions = {
 	desert = {
-		{ '"mcl_stairs:slab_oak([^"]*)"', '"mcl_stairs:slab_sandstonesmooth%1"' },
+		{ '"mcl_stairs:slab_oak([^"]*)"', '"mcl_stairs:slab_sandstonesmooth2%1"' }, -- divert from MCLA, no version 1?
 		{
 			'"mesecons_pressureplates:pressure_plate_oak_([^"]+)"',
 			'"mesecons_pressureplates:pressure_plate_birchwood_%1"',
@@ -254,7 +177,7 @@ mcl_villages.material_substitions = {
 		{ "mcl_trees:wood_oak", "mcl_core:redsandstonesmooth" },
 		{ '"mcl_fences:oak_fence([^"]*)"', '"mcl_fences:birch_fence%1"' },
 		{ '"mcl_stairs:stair_oak_bark([^"]*)"', '"mcl_stairs:stair_sandstonesmooth2%1"' },
-		{ '"mcl_stairs:stair_oak([^"]*)"', '"mcl_stairs:stair_sandstonesmooth%1"' },
+		{ '"mcl_stairs:stair_oak([^"]*)"', '"mcl_stairs:stair_sandstonesmooth2%1"' }, -- divert from MCLA, no version 1?
 	},
 	spruce = {
 		{ '"mcl_stairs:slab_oak([^"]*)"', '"mcl_stairs:slab_sprucewood%1"' },
