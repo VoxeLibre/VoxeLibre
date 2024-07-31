1916a16269ea172eb7bc0a80e986a62cc579a808
Mikita Wiśniewski <rudzik8@protonmail.com>
2024-07-31 21:20:43 +0700

    Simplify pickblock implementation and call on_rightclick

--- mods/PLAYER/vl_pickblock/init.lua
+++ mods/PLAYER/vl_pickblock/init.lua
@@ -1,25 +1,27 @@
 minetest.override_item("", {
 	on_place = function(itemstack, placer, pointed_thing)
 		if minetest.is_creative_enabled(placer:get_player_name()) then
+			mcl_util.call_on_rightclick(itemstack, placer, pointed_thing)
+
 			local node = minetest.get_node_or_nil(pointed_thing.under)
 			if not node then return end
 
 			local def = minetest.registered_nodes[node.name]
 			if not def then return end
 
-			local rnode
+			local rnode = {}
 			-- if this is an 'illegal' node and there's an explicit `_vl_pickblock` field, then return it
 			-- if the node isn't 'illegal', return it as-is
 			-- (and if it's 'illegal' and no `_vl_pickblock` is defined, well, bad luck)
 			if def.groups.not_in_creative_inventory and def.groups.not_in_creative_inventory ~= 0 then
 				if def._vl_pickblock then
-					rnode = def._vl_pickblock
+					rnode.name = def._vl_pickblock
 				end
 			else
-				rnode = node.name
+				rnode = node
 			end
 
-			return {name = rnode}
+			return rnode
 		end
 	end
 })
