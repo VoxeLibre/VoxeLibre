70b9466bbf7a45da59a67d989214afaf18210260
teknomunk <teknomunk@protonmail.com>
2024-09-08 09:41:32 -0500

    Address more review comments

--- mods/ITEMS/mcl_throwing/ender_pearl.lua
+++ mods/ITEMS/mcl_throwing/ender_pearl.lua
@@ -71,10 +71,8 @@ vl_projectile.register("mcl_throwing:ender_pearl_entity",{
 				-- Node is walkable, we have to find a place somewhere outside of that node
 				vc = vector.normalize(vc)
 
-				-- Zero-out the two axes with a lower absolute value than
-				-- the axis with the strongest force
-				local lv, ld
-				lv, ld = math.abs(vc.y), "y"
+				-- Zero-out the two axes with a lower absolute value than the axis with the strongest force
+				local lv, ld = math.abs(vc.y), "y"
 				if math.abs(vc.x) > lv then
 					lv, ld = math.abs(vc.x), "x"
 				end
@@ -122,8 +120,7 @@ vl_projectile.register("mcl_throwing:ender_pearl_entity",{
 			player:set_hp(player:get_hp() - 5, { type = "fall", from = "mod" })
 
 			-- 5% chance to spawn endermite at the player's origin
-			local r = math.random(1,20)
-			if r == 1 then
+			if math.random(1,20) == 1 then
 				minetest.add_entity(oldpos, "mobs_mc:endermite")
 			end
 		end
