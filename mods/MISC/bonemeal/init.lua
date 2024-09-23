3514fe211f5e403b37f02406b3c6232f803b4270
teknomunk <teknomunk@protonmail.com>
2024-09-23 06:31:02 -0500

    Implement more bonemeal mod shim, update bonemeal dependencies

--- mods/MISC/bonemeal/init.lua
+++ mods/MISC/bonemeal/init.lua
@@ -1,4 +1,12 @@
-bonemeal = {}
+bonemeal = {
+	item_list = {
+		bucket_water = "mcl_buckets:bucket_water",
+		bucket_empty = "mcl_buckets:bucket_empty",
+		dirt = "mcl_core:dirt",
+		torch = "mcl_torches:torch",
+		coral = "mcl_ocean:dead_horn_coral_block"
+	}
+}
 
 function bonemeal:on_use(pos, strength, node)
 	-- Fake itemstack for bone meal
@@ -10,3 +18,16 @@ function bonemeal:on_use(pos, strength, node)
 	}
 	mcl_bone_meal.use_bone_meal(itemstack, nil, pointed_thing)
 end
+
+function bonemeal:is_creative(player_name)
+	return minetest.is_creative_enabled(player_name)
+end
+
+function bonemeal:add_deco(list)
+	minetest.log("TODO: implement bonemeal:add_deco("..dump(list).."..)")
+	for i = 1,#list do
+		local item = list[i]
+	end
+end
+
+minetest.register_alias("mcl_mobitems:bone", "bonemeal:bone")
