57f72cce11f953b9470afd8a562af3859cf05ea7
kno10 <erich.schubert@gmail.com>
2024-10-26 19:05:24 +0200

    Add cherry grove biome, based on MCLA.
    Textures from pixel perfection, optimized with zopfipng. C.f., voxelibre#3463 voxelibre#4113

--- mods/ITEMS/mcl_cherry_blossom/nodes.lua
+++ mods/ITEMS/mcl_cherry_blossom/nodes.lua
@@ -101,3 +101,42 @@ mesecon.register_button(
 	true,
 	nil,
 	"mesecons_button_push_wood")
+
+minetest.register_node("mcl_cherry_blossom:pink_petals",{
+	description = S("Pink Petals"),
+	doc_items_longdesc = S("Pink Petals are ground decoration of cherry grove biomes"),
+	doc_items_hidden = false,
+	paramtype = "light",
+	paramtype2 = "facedir",
+	walkable = false,
+	sunlight_propagates = true,
+	floodable = true,
+	pointable = true,
+	drawtype = "nodebox",
+	node_box = {type = "fixed", fixed = {-1/2, -1/2, -1/2, 1/2, -7.9/16, 1/2}},
+	collision_box = {type = "fixed", fixed = {-1/2, -1/2, -1/2, 1/2, -7.9/16, 1/2}},
+	groups = {
+		shearsy=1,
+		handy=1,
+		flammable=3,
+		attached_node=1,
+		dig_by_piston=1,
+		compostability = 30,
+		deco_block=1
+		--not_in_creative_inventory=1,
+	},
+	use_texture_alpha = "clip",
+	sounds = mcl_sounds.node_sound_leaves_defaults(),
+	tiles = {
+		"mcl_cherry_blossom_pink_petals.png",
+		"mcl_cherry_blossom_pink_petals.png^[transformFY", -- mirror
+		"blank.png" -- empty
+	},
+	inventory_image = "mcl_cherry_blossom_pink_petals_inv.png",
+	_mcl_hardness = 0,
+	_mcl_blast_resistance = 0,
+	_on_bone_meal = function(_, _, _ , pos, n)
+		minetest.add_item(pos,n.name)
+	end
+})
+
---
+++ mods/MAPGEN/vl_biomes/cherrygrove.lua
@@ -0,0 +1,89 @@
+local mod_cherry_blossom = minetest.get_modpath("mcl_cherry_blossom")
+-- Cherry Grove
+vl_biomes.register_biome({
+	name = "CherryGrove",
+	node_top = "mcl_core:dirt_with_grass",
+	depth_top = 1,
+	node_filler = "mcl_core:dirt",
+	depth_filler = 2,
+	node_riverbed = "mcl_core:sand",
+	depth_riverbed = 2,
+	y_min = 18,
+	y_max = vl_biomes.overworld_max,
+	humidity_point = 41,
+	heat_point = 55,
+	_mcl_biome_type = "medium",
+	_mcl_water_temp = "ocean",
+	_mcl_grass_palette_index = 11,
+	_mcl_foliage_palette_index = 1,
+	_mcl_water_palette_index = 0,
+	_mcl_skycolor = "#78A7FF",
+	_beach = {
+		node_top = "mcl_core:sand",
+		depth_top = 2,
+		node_filler = "mcl_core:sandstone",
+		depth_filler = 2,
+		y_min = 0,
+		y_max = 2,
+	},
+	_ocean = {
+		node_top = "mcl_core:sand",
+		depth_top = 1,
+		node_filler = "mcl_core:sand",
+		depth_filler = 3,
+		y_max = -1,
+	},
+})
+
+-- Cherry trees
+for i=1,3 do
+	mcl_mapgen_core.register_decoration({
+		deco_type = "schematic",
+		place_on = {"mcl_core:dirt_with_grass"},
+		sidelen = 80,
+		noise_params = {
+			offset = 0.007,
+			scale = 0.08,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 13+i,
+			octaves = 3,
+			persist = 0.6
+		},
+		biomes = {"CherryGrove"},
+		y_min = 1,
+		y_max = mcl_vars.mg_overworld_max,
+		schematic = mod_cherry_blossom.."/schematics/mcl_cherry_blossom_tree_"..i..".mts",
+		flags = "place_center_x, place_center_z",
+		rotation = "random",
+	})
+	mcl_mapgen_core.register_decoration({
+		deco_type = "schematic",
+		place_on = {"mcl_core:dirt_with_grass"},
+		sidelen = 80,
+		noise_params = {
+			offset = 0.0005,
+			scale = 0.0001,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 32+i,
+			octaves = 3,
+			persist = 0.01
+		},
+		biomes = {"CherryGrove"},
+		y_min = 1,
+		y_max = mcl_vars.mg_overworld_max,
+		schematic = mod_cherry_blossom.."/schematics/mcl_cherry_blossom_tree_beehive_"..i..".mts",
+		flags = "place_center_x, place_center_z",
+		rotation = "random",
+	})
+end
+
+mcl_mapgen_core.register_decoration({
+	deco_type = "simple",
+	place_on = {"mcl_core:dirt_with_grass"},
+	fill_ratio = 0.6,
+	biomes = {"CherryGrove"},
+	y_min = mcl_vars.mg_overworld_min,
+	y_max = mcl_vars.mg_overworld_max,
+	decoration = "mcl_cherry_blossom:pink_petals",
+})
+
