769e223bea5dd83b38ece9f4d714041f271ecc30
kno10 <erich.schubert@gmail.com>
2024-10-31 00:52:41 +0100

    more new structures

---
+++ mods/MAPGEN/vl_extra_structures/init.lua
@@ -0,0 +1,7 @@
+local modname = minetest.get_current_modname()
+local modpath = minetest.get_modpath(modname)
+
+dofile(modpath.."/campsite.lua")
+dofile(modpath.."/obelisks.lua")
+dofile(modpath.."/spider_cocoon.lua")
+dofile(modpath.."/witches_circle.lua")
