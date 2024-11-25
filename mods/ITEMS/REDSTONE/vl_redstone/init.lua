bc0b207bcaacfc69ea57abd0721920e14c0993e9
teknomunk <teknomunk@protonmail.com>
2024-11-25 06:09:28 -0600

    Initial redstone circuit implementation of wires

---
+++ mods/ITEMS/REDSTONE/vl_redstone/init.lua
@@ -0,0 +1,9 @@
+local modname = core.get_current_modname()
+local modpath = core.get_modpath(modname)
+
+vl_redstone = {}
+
+dofile(modpath.."/wire.lua")
+dofile(modpath.."/circuit.lua")
+dofile(modpath.."/compat.lua")
+
