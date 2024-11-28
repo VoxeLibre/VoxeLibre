42b7dc9ce8a0856df926b8159c6103484ab16319
teknomunk <teknomunk@protonmail.com>
2024-11-28 10:00:45 -0600

    Switch to using not mcl_burning.is_affected_by_rain()

--- mods/ENTITIES/mcl_mobs/physics.lua
+++ mods/ENTITIES/mcl_mobs/physics.lua
@@ -653,7 +653,7 @@ function mob_class:do_env_damage()
 		local _, dim = mcl_worlds.y_to_layer(pos.y)
 		if (self.sunlight_damage ~= 0 or self.ignited_by_sunlight) and (sunlight or 0) >= minetest.LIGHT_MAX and dim == "overworld" then
 			if self.armor_list and not self.armor_list.helmet or not self.armor_list or self.armor_list and self.armor_list.helmet and self.armor_list.helmet == "" then
-				if self.ignited_by_sunlight and (not mcl_weather.rain.raining or not mcl_weather.has_rain(pos)) then
+				if self.ignited_by_sunlight and not mcl_burning.is_affected_by_rain(self.object) then
 					if (#mcl_burning.get_touching_nodes(self.object, "group:puts_out_fire", self) == 0) then
 						mcl_burning.set_on_fire(self.object, 10)
 					end
