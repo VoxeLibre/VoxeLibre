cd380a91d852803c03e980d08429234856cf7d41
teknomunk <teknomunk@protonmail.com>
2024-12-01 09:15:38 -0600

    Update whitespace in table definitions, only add _vl_allow_attach to nodes only if it isn't already defined and something would be added

--- mods/ITEMS/mcl_core/nodes_base.lua
+++ mods/ITEMS/mcl_core/nodes_base.lua
@@ -145,7 +145,7 @@ minetest.register_node("mcl_core:stone_with_redstone_lit", {
 	is_ground_content = true,
 	stack_max = 64,
 	groups = {pickaxey=4, not_in_creative_inventory=1, material_stone=1, xp=7, blast_furnace_smeltable=1},
-	_vl_allow_attach = { all = true },
+	_vl_allow_attach = {all = true},
 	drop = {
 		items = {
 			max_items = 1,
