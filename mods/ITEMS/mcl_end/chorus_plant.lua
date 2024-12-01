6564cd1b1bd4908dd9c906b56f116e59d9b86b3c
teknomunk <teknomunk@protonmail.com>
2024-12-01 06:18:20 -0600

    allow_support -> _vl_allow_support, more cleanup

--- mods/ITEMS/mcl_end/chorus_plant.lua
+++ mods/ITEMS/mcl_end/chorus_plant.lua
@@ -138,7 +138,7 @@ minetest.register_node("mcl_end:chorus_flower", {
 	selection_box = { type = "regular" },
 	sounds = mcl_sounds.node_sound_wood_defaults(),
 	groups = {handy=1,axey=1, deco_block = 1, dig_by_piston = 1, destroy_by_lava_flow = 1,chorus_plant = 1, support_attach = 1},
-	allow_attach = { torch = true },
+	_vl_allow_attach = { torch = true },
 
 	node_placement_prediction = "",
 	on_place = function(itemstack, placer, pointed_thing)
@@ -229,7 +229,7 @@ minetest.register_node("mcl_end:chorus_flower_dead", {
 	sounds = mcl_sounds.node_sound_wood_defaults(),
 	drop = "mcl_end:chorus_flower",
 	groups = {handy=1,axey=1, deco_block = 1, dig_by_piston = 1, destroy_by_lava_flow = 1,chorus_plant = 1, not_in_creative_inventory=1, support_attach = 1},
-	allow_attach = { torch = true },
+	_vl_allow_attach = { torch = true },
 	after_dig_node = mcl_end.check_detach_chorus_plant,
 	on_blast = mcl_end.check_blast_chorus_plant,
 	_mcl_blast_resistance = 0.4,
