f3f18c9683e072f500e19dbbb3d0b357aaa10da9
teknomunk <teknomunk@protonmail.com>
2024-12-01 07:13:15 -0600

    Completely replace node group with _vl_allow_attach true/false/callback logic and update node definitions and automatic group handling logic to match

--- mods/ITEMS/mcl_mobspawners/init.lua
+++ mods/ITEMS/mcl_mobspawners/init.lua
@@ -278,7 +278,8 @@ minetest.register_node("mcl_mobspawners:spawner", {
 	_tt_help = S("Makes mobs appear"),
 	_doc_items_longdesc = S("A mob spawner regularily causes mobs to appear around it while a player is nearby. Some mob spawners are disabled while in light."),
 	_doc_items_usagehelp = S("If you have a spawn egg, you can use it to change the mob to spawn. Just place the item on the mob spawner. Player-set mob spawners always spawn mobs regardless of the light level."),
-	groups = {pickaxey=1, material_stone=1, deco_block=1, support_attach=1},
+	groups = {pickaxey=1, material_stone=1, deco_block=1},
+	_vl_allow_attach = {torch = true},
 	is_ground_content = false,
 	drop = "",
 
