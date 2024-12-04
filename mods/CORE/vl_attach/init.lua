c8dff965914686d563ac4c2569db1d8f18675059
teknomunk <teknomunk@protonmail.com>
2024-12-04 07:31:39 -0600

    Replace hard-coded checks with attachment-type-global callbacks and move the hard-coded logic to these new type-global callbacks, don't override node definition unless a change was made by the autogroup callbacks, correct whitespace

--- mods/CORE/vl_attach/init.lua
+++ mods/CORE/vl_attach/init.lua
@@ -1,22 +1,22 @@
-vl_attach = {}
+local defaults = {}
+vl_attach = {
+	defaults = defaults,
+}
 
 -- Check if placement at given node is allowed
+local empty_table = {}
 function vl_attach.check_allowed(node, wdir, attach_type)
 	local def = minetest.registered_nodes[node.name]
 	if not def then return false end
 
-	-- No ceiling torches
-	if wdir == 0 then return false end
-
-	-- Allow solid, opaque, full cube collision box nodes are allowed.
-	if def.groups.solid and def.groups.opaque then return true end
+	-- Handle type-specific checks that apply to all node typesa
+	local allow_attach = defaults[attach_type] and defaults[attach_type](node, def, wdir, attach_type)
+	if allow_attach ~= nil then return allow_attach end
 
 	-- Allow nodes to define attachable device type handling
-	if not def._vl_allow_attach then return false end
-	local vl_allow_attach = def._vl_allow_attach
+	local vl_allow_attach = def._vl_allow_attach or empty_table
 
-	-- find allow/deny/callback for specified attach_type, and use "all" as a fallback
-	local allow_attach
+	-- Find allow/deny/callback for specified attach_type, and use "all" as a fallback
 	if vl_allow_attach.all ~= nil then allow_attach = vl_allow_attach.all end
 	if vl_allow_attach[attach_type] ~= nil then allow_attach = vl_allow_attach[attach_type] end
 
@@ -36,6 +36,7 @@ end
 core.register_on_mods_loaded(function()
 	for name,def in pairs(core.registered_nodes) do
 		local groups = def.groups
+		local original_allow_attach = def._vl_allow_attach or {}
 		local allow_attach = def._vl_allow_attach and table.copy(def._vl_allow_attach) or {}
 
 		-- Allow placing attachables over top buildable_to nodes
@@ -48,7 +49,17 @@ core.register_on_mods_loaded(function()
 			autogroup_callbacks[i](allow_attach, name, def)
 		end
 
-		core.override_item(name, {_vl_allow_attach = allow_attach})
+		-- Update node definition of changes to allow_attach were made
+		local has_changes = false
+		for k,v in pairs(allow_attach) do
+			if original_allow_attach[k] ~= v then has_changes = true end
+		end
+		for k,v in pairs(original_allow_attach) do
+			if allow_attach[k] ~= v then has_changes = true end
+		end
+		if has_changes then
+			core.override_item(name, {_vl_allow_attach = allow_attach})
+		end
 	end
 end)
 
