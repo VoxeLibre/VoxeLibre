bf0109a60656149eed0a06d528b68b84b7c41914
kno10 <erich.schubert@gmail.com>
2024-12-18 20:57:47 +0100

    fix translation string extraction for mcl_beds

--- mods/ITEMS/mcl_beds/functions.lua
+++ mods/ITEMS/mcl_beds/functions.lua
@@ -246,11 +246,11 @@ local function update_formspecs(finished, players)
 			form_n = form_n .. bg_sleep
 			form_n = form_n .. button_abort
 		else
-			local comment = "You will fall asleep when "
+			local comment
 			if players_in_bed_setting() == 100 then
-				comment = S(comment .. "all players are in bed.")
+				comment = S("You will fall asleep when all players are in bed.")
 			else
-				comment = S(comment .. "@1% of all players are in bed.", players_in_bed_setting())
+				comment = S("You will fall asleep when @1% of all players are in bed.", players_in_bed_setting())
 			end
 			text = text .. "\n" .. comment
 			form_n = form_n .. bg_presleep
