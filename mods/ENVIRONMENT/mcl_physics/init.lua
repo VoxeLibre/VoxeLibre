testersakage/mineclonia-jtest

Remove hardcoded gravity from item entity, fix bouancy in environmental effects

mods/ENVIRONMENT/mcl_physics/init.lua
@@ -4,34 +4,44 @@ mcl_physics = {}
 
 dofile(modpath.."/api.lua")
 
+-- Flowing water
 -- TODO: move to Flowlib
 local FLOW_SPEED = 1.39
+local BOUANCY = 3
 mcl_physics.register_environment_effect(function(pos, vel, staticdata)
 	-- Get the node and node definition
-	local node = minetest.get_node_or_nil(pos)
-	if not node then return end
-	local nodedef = minetest.registered_nodes[node.name]
-	if not nodedef then return end
+	local node = minetest.get_node_or_nil(pos); if not node then return end
+	local nodedef = minetest.registered_nodes[node.name]; if not nodedef then return end
 
 	-- Make sure we are in a liquid before continuing
 	local is_flowing = (nodedef.liquidtype == "flowing")
 	staticdata.flowing = is_flowing
-	if not is_flowing then
-		-- Apply decelleration if the entity moved from flowing water to
-		-- stationary water
-		if nodedef.liquidtype == "source" then
-			return nil,vector.new(
-				0 - vel.x * 0.9,
-				3 - vel.y * 0.9,
-				0 - vel.z * 0.9
-			)
-		end
-		return
-	end
+	if not is_flowing then return end
 
 	-- Get liquid flow direction
 	local vec = vector.multiply(flowlib.quick_flow(pos, node), FLOW_SPEED)
 	return vector.new(vec.x, -0.22, vec.z),nil
+end)
+
+-- Simple gravity and bouency
+mcl_physics.register_environment_effect(function(pos, vel, staticdata)
+	-- Get the node and node definition
+	local node = minetest.get_node_or_nil(pos);
+	local nodedef = nil
+	if node then nodedef = minetest.registered_nodes[node.name] end
+
+	if nodedef and nodedef.liquidtype == "source" then
+		-- Apply decceleration and bouancy if the entity moved from flowing water to
+		-- stationary water
+		return nil,vector.new(
+			0 - vel.x * 0.9,
+			BOUANCY - vel.y * 0.9,
+			0 - vel.z * 0.9
+		)
+	else
+		local gravity = tonumber(minetest.settings:get("movement_gravity")) or 9.81
+		return nil,vector.new(0,-gravity,0)
+	end
 end)
 
 -- Node effects

VoxeLibre/VoxeLibre

mods/ENVIRONMENT/mcl_physics/init.lua
@@ -5,34 +5,44 @@ mcl_physics = mod
 
 dofile(modpath.."/api.lua")
 
+-- Flowing water
 -- TODO: move to Flowlib
 local FLOW_SPEED = 1.39
+local BOUANCY = 3
 mod.register_environment_effect(function(pos, vel, staticdata)
 	-- Get the node and node definition
-	local node = minetest.get_node_or_nil(pos)
-	if not node then return end
-	local nodedef = minetest.registered_nodes[node.name]
-	if not nodedef then return end
+	local node = minetest.get_node_or_nil(pos); if not node then return end
+	local nodedef = minetest.registered_nodes[node.name]; if not nodedef then return end
 
 	-- Make sure we are in a liquid before continuing
 	local is_flowing = (nodedef.liquidtype == "flowing")
 	staticdata.flowing = is_flowing
-	if not is_flowing then
-		-- Apply decelleration if the entity moved from flowing water to 
-		-- stationary water
-		if nodedef.liquidtype == "source" then
-			return nil,vector.new(
-				0 - vel.x * 0.9,
-				3 - vel.y * 0.9,
-				0 - vel.z * 0.9
-			)
-		end
-		return
-	end
+	if not is_flowing then return end
 
 	-- Get liquid flow direction
 	local vec = vector.multiply(flowlib.quick_flow(pos, node), FLOW_SPEED)
 	return vector.new(vec.x, -0.22, vec.z),nil
+end)
+
+-- Simple gravity and bouency
+mod.register_environment_effect(function(pos, vel, staticdata)
+	-- Get the node and node definition
+	local node = minetest.get_node_or_nil(pos);
+	local nodedef = nil
+	if node then nodedef = minetest.registered_nodes[node.name] end
+
+	if nodedef and nodedef.liquidtype == "source" then
+		-- Apply decceleration and bouancy if the entity moved from flowing water to
+		-- stationary water
+		return nil,vector.new(
+			0 - vel.x * 0.9,
+			BOUANCY - vel.y * 0.9,
+			0 - vel.z * 0.9
+		)
+	else
+		local gravity = tonumber(minetest.settings:get("movement_gravity")) or 9.81
+		return nil,vector.new(0,-gravity,0)
+	end
 end)
 
 -- Node effects