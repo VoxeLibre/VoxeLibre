e4b5673642083531c52a9da9f8a01606d7adeed4
teknomunk <teknomunk@protonmail.com>
2025-01-06 06:36:02 -0600

    Resolve some review comments

--- mods/ENTITIES/mcl_mobs/functions.lua
+++ mods/ENTITIES/mcl_mobs/functions.lua
@@ -1,15 +1,14 @@
-
 function mcl_mobs.check_line_of_sight(origin, target)
-	local raycast = minetest.raycast(origin, target, false, true)
+	local raycast = core.raycast(origin, target, false, true)
 
 	local los_blocked = false
 	for hitpoint in raycast do
 		if hitpoint.type == "node" then
-			--TODO type object could block vision, for example chests
-			local node = minetest.get_node(minetest.get_pointed_thing_position(hitpoint))
+			--TODO: type object could block vision, for example minecarts
+			local node = core.get_node(core.get_pointed_thing_position(hitpoint))
 
 			if node.name ~= "air" then
-				local nodef = minetest.registered_nodes[node.name]
+				local nodef = core.registered_nodes[node.name]
 				if nodef and nodef.walkable then
 					los_blocked = true
 					break
