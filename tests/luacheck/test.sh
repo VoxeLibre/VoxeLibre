4485a4997e178ca743757d744c9c9af90ee98658
teknomunk <teknomunk@protonmail.com>
2025-01-11 07:11:48 -0600

    Detect lua version

--- tests/luacheck/test.sh
+++ tests/luacheck/test.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 LUACHECK=$HOME/.luarocks/bin/luacheck
-
+LUA=$( which lua || which lua5.1 || which lua5.2 || which lua5.3 || which luajit )
+if [[ -z "$LUA" ]]; then
+	echo "Unable to find lua interpreter"
+	exit 1
+fi
 (
 	find ./ -name mod.conf
 	echo EOF
-) | lua5.3 tests/luacheck/test.lua | sh
+) | $LUA tests/luacheck/test.lua | sh
 
