bf196f5d0c3818984e0e5032cae40d72caec893d
kno10 <erich.schubert@gmail.com>
2025-01-13 01:10:53 +0100

    some slight adjustments to structure frequencies

--- mods/MAPGEN/vl_extra_structures/forest_grave.lua
+++ mods/MAPGEN/vl_extra_structures/forest_grave.lua
@@ -6,7 +6,7 @@ vl_structures.register_structure("forest_grave",{
 	place_on = {"group:grass_block","group:dirt","mcl_core:dirt_with_grass"},
 	flags = "place_center_x, place_center_z",
 	prepare = { tolerance = 3, clear_bottom = 0, padding = 0, corners = 0, foundation = -2 },
-	chunk_probability = 20,
+	chunk_probability = 50,
 	y_max = mcl_vars.mg_overworld_max,
 	y_min = 1,
 	biomes = { "Forest", "RoofedForest", "BirchForest", "FlowerForest", "CherryGrove", "ExtremeHills+" }, -- TODO: also add to some other biomes?
