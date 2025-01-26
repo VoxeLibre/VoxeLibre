a0ba232ca7d4a45dc4c0edfdb6fbda696356d80b
Mikita Wiśniewski <rudzik8@protonmail.com>
2025-01-26 19:53:48 +0700

    Signs code clean-up

--- mods/ITEMS/mcl_signs/init.lua
+++ mods/ITEMS/mcl_signs/init.lua
@@ -5,26 +5,21 @@ local modname = core.get_current_modname()
 local S = core.get_translator(modname)
 local modpath = core.get_modpath(modname)
 
--- UTF-8 library from Modlib
+-- UTF-8 library from modlib
 local utf8 = dofile(modpath .. DIR_DELIM .. "utf8.lua")
 
--- Character map
+-- Character map (see API.md for reference)
 local charmap = {}
 for line in io.lines(modpath .. "/characters.tsv") do
 	local split = line:split("\t")
-	if #split > 0 then
+	if #split == 3 then
 		local char, img, _ = split[1], split[2], split[3] -- 3rd is ignored, reserved for width
 		charmap[char] = img
 	end
 end
 mcl_signs.charmap = charmap
 
-local files = {
-	"api",
-	"register",
-	"compat"
-}
-
-for _, file in pairs(files) do
+-- Load modules and share local variables
+for _, file in pairs{"api", "register", "compat"} do
 	loadfile(modpath .. DIR_DELIM .. file .. ".lua")(S, charmap, utf8)
 end
