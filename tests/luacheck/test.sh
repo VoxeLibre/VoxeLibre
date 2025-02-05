d93beb5c2504b09765a3b56a42cdec7c14853b34
teknomunk <teknomunk@protonmail.com>
2025-02-05 05:26:06 -0600

    Update test workflow, add lua-language-server check, split up test.sh

--- tests/luacheck/test.sh
+++ tests/luacheck/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+which luarocks && eval $(luarocks path)
+which luarocks-5.3 && eval $(luarocks-5.3 path)
+
 LUACHECK=$HOME/.luarocks/bin/luacheck
 LUA=$( which lua || which lua5.1 || which lua5.2 || which lua5.3 || which luajit )
 if [[ -z "$LUA" ]]; then
