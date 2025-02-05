9972c67e14803446b056ec15b47f4545605d1212
teknomunk <teknomunk@protonmail.com>
2025-02-05 05:47:00 -0600

    Update server startup test, lua language server check

--- tests/lua-language-server.sh
+++ tests/lua-language-server.sh
@@ -2,5 +2,14 @@
 
 set -e
 
+# Make sure luanti lls definitions are up-to-date
+(
+	mkdir -p /usr/share/luanti/
+	cd /usr/share/luanti
+	git clone https://codeberg.org/teknomunk/luanti-lls-definitions.git || true
+	cd luanti-lls-definitions
+	git pull
+)
+
 lua-language-server --check . --log check.log --logpath ./log | tr '\r' '\n' || true
 lua tests/display-lls-check-log.lua
