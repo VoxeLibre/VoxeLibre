7396218c83b398959ddd7e2345a0c80a38857380
Sab Pyrope <99945024+Uwuewsky@users.noreply.github.com>
2025-02-14 13:01:24 +0800

    Fix i18n: death messages

--- mods/HUD/mcl_death_messages/init.lua
+++ mods/HUD/mcl_death_messages/init.lua
@@ -1,4 +1,5 @@
 local S = minetest.get_translator(minetest.get_current_modname())
+local N = function(s) return s end
 
 local ASSIST_TIMEOUT_SEC = 5
 
@@ -7,53 +8,53 @@ mcl_death_messages = {
 	messages = {
 		in_fire = {
 			_translator = S,
-			plain = "@1 went up in flames",
-			assist = "@1 walked into fire whilst fighting @2",
+			plain = N("@1 went up in flames"),
+			assist = N("@1 walked into fire whilst fighting @2"),
 		},
 		lightning_bolt = {
 			_translator = S,
-			plain = "@1 was struck by lightning",
-			assist = "@1 was struck by lightning whilst fighting @2",
+			plain = N("@1 was struck by lightning"),
+			assist = N("@1 was struck by lightning whilst fighting @2"),
 		},
 		on_fire = {
 			_translator = S,
-			plain = "@1 burned to death",
-			assist = "@1 was burnt to a crisp whilst fighting @2",
+			plain = N("@1 burned to death"),
+			assist = N("@1 was burnt to a crisp whilst fighting @2"),
 		},
 		lava = {
 			_translator = S,
-			plain = "@1 tried to swim in lava",
-			assist = "@1 tried to swim in lava to escape @2"
+			plain = N("@1 tried to swim in lava"),
+			assist = N("@1 tried to swim in lava to escape @2")
 		},
 		hot_floor = {
 			_translator = S,
-			plain = "@1 discovered the floor was lava",
-			assist = "@1 walked into danger zone due to @2",
+			plain = N("@1 discovered the floor was lava"),
+			assist = N("@1 walked into danger zone due to @2"),
 		},
 		in_wall = {
 			_translator = S,
-			plain = "@1 suffocated in a wall",
-			assist = "@1 suffocated in a wall whilst fighting @2",
+			plain = N("@1 suffocated in a wall"),
+			assist = N("@1 suffocated in a wall whilst fighting @2"),
 		},
 		drown = {
 			_translator = S,
-			plain = "@1 drowned",
-			assist = "@1 drowned whilst trying to escape @2",
+			plain = N("@1 drowned"),
+			assist = N("@1 drowned whilst trying to escape @2"),
 		},
 		starve = {
 			_translator = S,
-			plain = "@1 starved to death",
-			assist = "@1 starved to death whilst fighting @2",
+			plain = N("@1 starved to death"),
+			assist = N("@1 starved to death whilst fighting @2"),
 		},
 		cactus = {
 			_translator = S,
-			plain = "@1 was pricked to death",
-			assist = "@1 walked into a cactus whilst trying to escape @2",
+			plain = N("@1 was pricked to death"),
+			assist = N("@1 walked into a cactus whilst trying to escape @2"),
 		},
 		fall = {
 			_translator = S,
-			plain = "@1 hit the ground too hard",
-			assist = "@1 hit the ground too hard whilst trying to escape @2",
+			plain = N("@1 hit the ground too hard"),
+			assist = N("@1 hit the ground too hard whilst trying to escape @2"),
 			-- "@1 fell from a high place" -- for fall distance > 5 blocks
 			-- "@1 fell while climbing"
 			-- "@1 fell off some twisting vines"
@@ -64,97 +65,97 @@ mcl_death_messages = {
 		},
 		fly_into_wall = {
 			_translator = S,
-			plain = "@1 experienced kinetic energy",
-			assist = "@1 experienced kinetic energy whilst trying to escape @2",
+			plain = N("@1 experienced kinetic energy"),
+			assist = N("@1 experienced kinetic energy whilst trying to escape @2"),
 		},
 		out_of_world = {
 			_translator = S,
-			plain = "@1 fell out of the world",
-			assist = "@1 didn't want to live in the same world as @2",
+			plain = N("@1 fell out of the world"),
+			assist = N("@1 didn't want to live in the same world as @2"),
 		},
 		generic = {
 			_translator = S,
-			plain = "@1 died",
-			assist = "@1 died because of @2",
+			plain = N("@1 died"),
+			assist = N("@1 died because of @2"),
 		},
 		magic = {
 			_translator = S,
-			plain = "@1 was killed by magic",
-			assist = "@1 was killed by magic whilst trying to escape @2",
-			killer = "@1 was killed by @2 using magic",
-			item = "@1 was killed by @2 using @3",
+			plain = N("@1 was killed by magic"),
+			assist = N("@1 was killed by magic whilst trying to escape @2"),
+			killer = N("@1 was killed by @2 using magic"),
+			item = N("@1 was killed by @2 using @3"),
 		},
 		dragon_breath = {
 			_translator = S,
-			plain = "@1 was roasted in dragon breath",
-			killer = "@1 was roasted in dragon breath by @2",
+			plain = N("@1 was roasted in dragon breath"),
+			killer = N("@1 was roasted in dragon breath by @2"),
 		},
 		wither = {
 			_translator = S,
-			plain = "@1 withered away",
-			escape = "@1 withered away whilst fighting @2",
+			plain = N("@1 withered away"),
+			escape = N("@1 withered away whilst fighting @2"),
 		},
 		wither_skull = {
 			_translator = S,
-			plain = "@1 was killed by magic",
-			killer = "@1 was shot by a skull from @2",
+			plain = N("@1 was killed by magic"),
+			killer = N("@1 was shot by a skull from @2"),
 		},
 		anvil = {
 			_translator = S,
-			plain = "@1 was squashed by a falling anvil",
-			escape = "@1 was squashed by a falling anvil whilst fighting @2",
+			plain = N("@1 was squashed by a falling anvil"),
+			escape = N("@1 was squashed by a falling anvil whilst fighting @2"),
 		},
 		falling_node = {
 			_translator = S,
-			plain = "@1 was squashed by a falling block",
-			assist = "@1 was squashed by a falling block whilst fighting @2",
+			plain = N("@1 was squashed by a falling block"),
+			assist = N("@1 was squashed by a falling block whilst fighting @2"),
 		},
 		mob = {
 			_translator = S,
-			killer = "@1 was slain by @2",
-			item = "@1 was slain by @2 using @3",
+			killer = N("@1 was slain by @2"),
+			item = N("@1 was slain by @2 using @3"),
 		},
 		player = {
 			_translator = S,
-			killer = "@1 was slain by @2",
-			item = "@1 was slain by @2 using @3"
+			killer = N("@1 was slain by @2"),
+			item = N("@1 was slain by @2 using @3")
 		},
 		arrow = {
 			_translator = S,
-			killer = "@1 was shot by @2",
-			item = "@1 was shot by @2 using @3",
+			killer = N("@1 was shot by @2"),
+			item = N("@1 was shot by @2 using @3"),
 		},
 		fireball = {
 			_translator = S,
-			killer = "@1 was fireballed by @2",
-			item = "@1 was fireballed by @2 using @3",
+			killer = N("@1 was fireballed by @2"),
+			item = N("@1 was fireballed by @2 using @3"),
 		},
 		thorns = {
 			_translator = S,
-			killer = "@1 was killed trying to hurt @2",
-			item = "@1 tried to hurt @2 and died by @3",
+			killer = N("@1 was killed trying to hurt @2"),
+			item = N("@1 tried to hurt @2 and died by @3"),
 		},
 		explosion = {
 			_translator = S,
-			plain = "@1 blew up",
-			killer = "@1 was blown up by @2",
-			item = "@1 was blown up by @2 using @3",
+			plain = N("@1 blew up"),
+			killer = N("@1 was blown up by @2"),
+			item = N("@1 was blown up by @2 using @3"),
 			-- "@1 was killed by [Intentional Game Design]" -- for exploding bed in nether or end
 		},
 		cramming = {
 			_translator = S,
-			plain = "@1 was squished too much",
-			assist = "@1 was squashed by @2",	-- surprisingly "escape" is actually the correct subtype
+			plain = N("@1 was squished too much"),
+			assist = N("@1 was squashed by @2"),	-- surprisingly "escape" is actually the correct subtype
 		},
 		fireworks = {
 			_translator = S,
-			plain = "@1 went off with a bang",
-			item = "@1 went off with a bang due to a firework fired by @2 from @3",
+			plain = N("@1 went off with a bang"),
+			item = N("@1 went off with a bang due to a firework fired by @2 from @3"),
 		},
 		sweet_berry = {
 			_translator = S,
-			plain = "@1 died a sweet death",
-			assist = "@1 was poked to death by a sweet berry bush whilst trying to escape @2",
+			plain = N("@1 died a sweet death"),
+			assist = N("@1 was poked to death by a sweet berry bush whilst trying to escape @2"),
 		},
 	},
 }
