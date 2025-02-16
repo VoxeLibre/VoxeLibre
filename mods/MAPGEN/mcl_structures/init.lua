0a7c413acdac299317e9a57c4b78976a59f9b056
kno10 <erich.schubert@gmail.com>
2025-02-16 17:31:38 +0100

    move desert_well and fossil to separate files

--- mods/MAPGEN/mcl_structures/init.lua
+++ mods/MAPGEN/mcl_structures/init.lua
@@ -17,45 +17,8 @@ dofile(modpath.."/geode.lua")
 dofile(modpath.."/pillager_outpost.lua")
 dofile(modpath.."/end_spawn.lua")
 dofile(modpath.."/end_city.lua")
-
-
-mcl_structures.register_structure("desert_well",{
-	place_on = {"group:sand"},
-	fill_ratio = 0.01,
-	flags = "place_center_x, place_center_z",
-	not_near = { "desert_temple_new" },
-	solid_ground = true,
-	sidelen = 4,
-	chunk_probability = 600,
-	y_max = mcl_vars.mg_overworld_max,
-	y_min = 1,
-	y_offset = -2,
-	biomes = { "Desert" },
-	filenames = { modpath.."/schematics/mcl_structures_desert_well.mts" },
-})
-
-mcl_structures.register_structure("fossil",{
-	place_on = {"group:material_stone","group:sand"},
-	fill_ratio = 0.01,
-	flags = "place_center_x, place_center_z",
-	solid_ground = true,
-	sidelen = 13,
-	chunk_probability = 1000,
-	y_offset = function(pr) return ( pr:next(1,16) * -1 ) -16 end,
-	y_max = 15,
-	y_min = mcl_vars.mg_overworld_min + 35,
-	biomes = { "Desert" },
-	filenames = {
-		modpath.."/schematics/mcl_structures_fossil_skull_1.mts", -- 4×5×5
-		modpath.."/schematics/mcl_structures_fossil_skull_2.mts", -- 5×5×5
-		modpath.."/schematics/mcl_structures_fossil_skull_3.mts", -- 5×5×7
-		modpath.."/schematics/mcl_structures_fossil_skull_4.mts", -- 7×5×5
-		modpath.."/schematics/mcl_structures_fossil_spine_1.mts", -- 3×3×13
-		modpath.."/schematics/mcl_structures_fossil_spine_2.mts", -- 5×4×13
-		modpath.."/schematics/mcl_structures_fossil_spine_3.mts", -- 7×4×13
-		modpath.."/schematics/mcl_structures_fossil_spine_4.mts", -- 8×5×13
-	},
-})
+dofile(modpath.."/desert_well.lua")
+dofile(modpath.."/fossil.lua")
 
 mcl_structures.register_structure("boulder",{
 	filenames = {
