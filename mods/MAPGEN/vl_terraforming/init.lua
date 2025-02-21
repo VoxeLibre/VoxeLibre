e1865a819d6f7795f54c32719e69b2c0248e7327
kno10 <erich.schubert@gmail.com>
2025-02-21 13:15:02 +0100

    add terraforming API

---
+++ mods/MAPGEN/vl_terraforming/init.lua
@@ -0,0 +1,7 @@
+local modpath = core.get_modpath(core.get_current_modname())
+vl_terraforming = {}
+
+dofile(modpath.."/util.lua")
+dofile(modpath.."/clearance.lua")
+dofile(modpath.."/foundation.lua")
+dofile(modpath.."/level.lua")
