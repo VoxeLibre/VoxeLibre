2e8ba97dbcb140905886022cbdeb7200efc5840d
kno10 <erich.schubert@gmail.com>
2025-02-21 13:29:41 +0100

    move fossil and geode to vl_biome decorations

--- mods/MAPGEN/mcl_terrain_features/init.lua
+++ mods/MAPGEN/mcl_terrain_features/init.lua
@@ -2,8 +2,6 @@ local modname = minetest.get_current_modname()
 local modpath = minetest.get_modpath(modname)
 
 dofile(modpath.."/fallentree.lua")
-dofile(modpath.."/fossil.lua")
-dofile(modpath.."/geode.lua")
 dofile(modpath.."/lakes.lua")
 dofile(modpath.."/basaltdelta.lua")
 
