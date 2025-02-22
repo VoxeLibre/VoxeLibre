26560dcad7654b6ad1a5be1593fee355b3094e27
kno10 <erich.schubert@gmail.com>
2025-02-22 14:51:49 +0100

    integrate mcl_terrain_features into vl_biomes

--- mods/MAPGEN/mcl_terrain_features/init.lua
+++
@@ -1,7 +0,0 @@
-local modname = minetest.get_current_modname()
-local modpath = minetest.get_modpath(modname)
-
-dofile(modpath.."/fallentree.lua")
-dofile(modpath.."/lakes.lua")
-dofile(modpath.."/basaltdelta.lua")
-
