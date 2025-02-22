0c25f371159ab21f5a479ec86a82e41f4feb39ce
kno10 <erich.schubert@gmail.com>
2025-02-22 23:27:17 +0100

    add campsite

--- mods/MAPGEN/vl_extra_structures/init.lua
+++ mods/MAPGEN/vl_extra_structures/init.lua
@@ -1,6 +1,7 @@
 local modname = core.get_current_modname()
 local modpath = core.get_modpath(modname)
 
+dofile(modpath.."/campsite.lua")
 dofile(modpath.."/desert_oasis.lua")
 dofile(modpath.."/forest_ruins.lua")
 dofile(modpath.."/graveyard.lua")
