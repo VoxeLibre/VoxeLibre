a5c3ec5b2cef90e79afc78068a5b0669455bd4cd
kno10 <erich.schubert@gmail.com>
2025-02-23 02:02:05 +0100

    new witch hut

--- mods/MAPGEN/mcl_structures/witch_hut.lua
+++ mods/MAPGEN/mcl_structures/witch_hut.lua
@@ -8,13 +8,13 @@ local function spawn_witch(p1,p2)
 	local c = minetest.find_node_near(p1,15,{"group:cauldron"})
 	if not c then return end
 	local nn = minetest.find_nodes_in_area_under_air(vector.new(p1.x,c.y-1,p1.z),vector.new(p2.x,c.y-1,p2.z),{"mcl_core:sprucewood"})
-	local witchobj = not peaceful and minetest.add_entity(vector.offset(nn[math.random(#nn)],0,1,0),"mobs_mc:witch")
+	local witchobj = not peaceful and #nn > 0 and minetest.add_entity(vector.offset(nn[math.random(#nn)],0,1,0),"mobs_mc:witch")
 	if witchobj then
 		local witch = witchobj:get_luaentity()
 		witch._home = c
 		witch.can_despawn = false
 	end
-	local catobj = minetest.add_entity(vector.offset(nn[math.random(#nn)],0,1,0),"mobs_mc:cat")
+	local catobj = #nn > 0 and minetest.add_entity(vector.offset(nn[math.random(#nn)],0,1,0),"mobs_mc:cat")
 	if catobj then
 		local cat=catobj:get_luaentity()
 		cat.object:set_properties({textures = {"mobs_mc_cat_black.png"}})
@@ -26,7 +26,10 @@ end
 
 local function hut_placement_callback(pos,def,pr,p1,p2)
 	-- p1.y is the bottom slice only, not a typo, we look for the hut legs
-	local legs = minetest.find_nodes_in_area(p1,vector.new(p2.x,p1.y,p2.z), "mcl_core:tree")
+	local legs = minetest.find_nodes_in_area(p1,vector.new(p2.x,p1.y,p2.z), "mcl_core:sprucetree")
+	if #legs == 0 then -- old schematic?
+		legs = minetest.find_nodes_in_area(p1,vector.new(p2.x,p1.y,p2.z), "mcl_core:tree")
+	end
 	local tree = {}
 	for _,leg in pairs(legs) do
 		while true do
@@ -37,8 +40,10 @@ local function hut_placement_callback(pos,def,pr,p1,p2)
 			table.insert(tree,leg)
 		end
 	end
-	minetest.bulk_swap_node(tree, {name = "mcl_core:tree", param2 = 2})
+	minetest.bulk_swap_node(tree, {name = "mcl_core:sprucetree", param2 = 2})
 	spawn_witch(p1,p2)
+	vl_structures.construct_nodes(p1, p2, {"mcl_brewing:stand_000"})
+	-- TODO: add some bottles etc. to the brewing stand?
 end
 
 vl_structures.register_structure("witch_hut",{
@@ -54,6 +59,8 @@ vl_structures.register_structure("witch_hut",{
 	y_min = -5,
 	y_offset = -1,
 	biomes = { "Swampland", "Swampland_ocean", "Swampland_shore" },
-	filenames = { modpath.."/schematics/mcl_structures_witch_hut.mts" },
+	filenames = {
+		modpath.."/schematics/mcl_structures_witch_hut.mts",
+	},
 	after_place = hut_placement_callback,
 })
