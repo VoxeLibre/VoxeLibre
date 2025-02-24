49f3417ce30bce5144fdbdd71ca9fa4cc8fc020c
teknomunk <teknomunk@protonmail.com>
2025-02-23 19:58:58 -0600

    Change checkout and test running

--- tests/unit-tests.sh
+++ tests/unit-tests.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+set -e
+
 which luarocks && eval $(luarocks path)
 which luarocks-5.3 && eval $(luarocks-5.3 path)
 
-find ./ -name 'unit-test.lua' | while read TEST; do
+find ./ -name 'unit-test.lua' | sort | while read TEST; do
 	(
+		set -e
 		cd $(dirname $TEST)
 		busted unit-test.lua
 	)
