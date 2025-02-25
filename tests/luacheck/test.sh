80a4d2bf789e9babbaaf22a16721742876434d88
teknomunk <teknomunk@protonmail.com>
2025-02-25 07:05:32 -0600

    Move lua environment detection to its own file and source in the test scripts

--- tests/luacheck/test.sh
+++ tests/luacheck/test.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-which luarocks && eval $(luarocks path)
-which luarocks-5.3 && eval $(luarocks-5.3 path)
+source tests/env.sh
+set -e
 
-if [[ -d /usr/share/minetest ]]; then
-	export LUANTI_PATH=/usr/share/minetest
-elif [[ -d /usr/share/luanti ]]; then
-	export LUANTI_PATH=/usr/share/luanti
-else
-	echo "Unable to find luanti/minetest path, cannot run tests"
-	exit 1
-fi
-
-LUACHECK=$HOME/.luarocks/bin/luacheck
-LUA=$( which lua || which lua5.1 || which lua5.2 || which lua5.3 || which luajit )
 if [[ -z "$LUA" ]]; then
 	echo "Unable to find lua interpreter"
 	exit 1
