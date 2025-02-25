269e220eeb5288e413dc02cd9bdf04d21386a4e9
teknomunk <teknomunk@protonmail.com>
2025-02-25 06:58:18 -0600

    Remove misc_helpers and use platform builtins instead, detect luanti/minetest path to access platform builtins

--- tests/luacheck/test.sh
+++ tests/luacheck/test.sh
@@ -3,6 +3,15 @@
 which luarocks && eval $(luarocks path)
 which luarocks-5.3 && eval $(luarocks-5.3 path)
 
+if [[ -d /usr/share/minetest ]]; then
+	export LUANTI_PATH=/usr/share/minetest
+elif [[ -d /usr/share/luanti ]]; then
+	export LUANTI_PATH=/usr/share/luanti
+else
+	echo "Unable to find luanti/minetest path, cannot run tests"
+	exit 1
+fi
+
 LUACHECK=$HOME/.luarocks/bin/luacheck
 LUA=$( which lua || which lua5.1 || which lua5.2 || which lua5.3 || which luajit )
 if [[ -z "$LUA" ]]; then
