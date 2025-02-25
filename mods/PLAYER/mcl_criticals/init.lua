7327f866bab41c5d1c399346a0a173d0807c87a0
potatoalienof13 <potatoalienof13@lolnoemam.dsa>
2025-02-25 13:58:26 -0500

    Sprint criticals now do extra damage, if the conditions are met.

--- mods/PLAYER/mcl_criticals/init.lua
+++ mods/PLAYER/mcl_criticals/init.lua
@@ -3,7 +3,8 @@ mcl_damage.register_modifier(function(obj, damage, reason)
 		local hitter = reason.direct
 		if mcl_sprint.is_sprinting(hitter:get_player_name()) then
 			obj:add_velocity(hitter:get_velocity())
-		elseif (hitter:get_velocity() or hitter:get_player_velocity()).y < 0 then
+		end
+		if (hitter:get_velocity() or hitter:get_player_velocity()).y < 0 then
 			local pos = mcl_util.get_object_center(obj)
 			minetest.add_particlespawner({
 				amount = 15,
