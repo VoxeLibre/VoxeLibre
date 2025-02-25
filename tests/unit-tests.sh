7a9d6b0df4e55fb5873417c8347f7fa033f2f497
teknomunk <teknomunk@protonmail.com>
2025-02-25 06:29:27 -0600

    Rework unit test runner to allow multiple unit test files per module, update paths of existing unit tests

--- tests/unit-tests.sh
+++ tests/unit-tests.sh
@@ -5,10 +5,16 @@ set -e
 which luarocks && eval $(luarocks path)
 which luarocks-5.3 && eval $(luarocks-5.3 path)
 
-find ./ -name 'unit-test.lua' | sort | while read TEST; do
+find ./ -name 'mod.conf' | sort | while read MOD; do
 	(
 		set -e
-		cd $(dirname $TEST)
-		busted unit-test.lua
+		DIR=$(dirname $MOD)
+		cd $DIR
+
+		if [[ -d test/ ]]; then
+			for TEST in test/*.lua; do
+				busted $TEST
+			done
+		fi
 	)
 done
