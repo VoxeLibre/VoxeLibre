e89d46376dedd680139a695195c0bc6228e4baff
teknomunk <teknomunk@protonmail.com>
2025-03-01 11:14:43 -0600

    Start making lua-language-server checks able to fail
    Update luachech check.lst to lastest master
    
    Additional script updates

--- tests/display-lls-check-log.lua
+++ tests/display-lls-check-log.lua
@@ -6,14 +6,36 @@ assert(file, "File `./log/check.json' doesn't exist. Did you run lua-language-se
 local str = file:read("*all")
 local data = lunajson.decode(str)
 
+local f = io.open("tests/lua-language-server/check.lst","r")
+local allow_findings = false
+if f then
+	allow_findings = {}
+	for line in f:lines() do
+		line = line:gsub("\n",""):gsub(" ","")
+		allow_findings[line] = true
+	end
+	f:close()
+end
+
 -- Display findings
 local has_finding = false
 for file,findings in pairs(data) do
+	file = file:gsub("file://","")
 	for _,finding in ipairs(findings) do
-		print(file:gsub("file://","")..":"..(finding.range.start.line + 1)..": "..finding.message)
-		has_finding = true
+		local prefix = "Warning"
+		if allow_findings and allow_findings[file] then
+			has_finding = true
+			prefix = "Error"
+		end
+		print(prefix..": "..file..":"..(finding.range.start.line + 1)..": "..finding.message)
 	end
 end
 
 -- Report success or failure
-os.exit(has_finding and 1 or 0)
+if has_finding then
+	print("At least one error has been detected")
+	os.exit(1)
+else
+	print("No errors found")
+	os.exit(0)
+end
