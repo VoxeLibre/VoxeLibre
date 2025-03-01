da6f426675e0ef4ffab7cb149f99d0eae7ac0d1b
teknomunk <teknomunk@protonmail.com>
2025-03-01 11:14:43 -0600

    Start making lua-language-server checks able to fail

--- tests/display-lls-check-log.lua
+++ tests/display-lls-check-log.lua
@@ -6,12 +6,25 @@ assert(file, "File `./log/check.json' doesn't exist. Did you run lua-language-se
 local str = file:read("*all")
 local data = lunajson.decode(str)
 
+local f = io.open("tests/lua-language-server/checks.lst","r")
+local allow_findings = false
+if f then
+	allow_findings = {}
+	for line in f:lines() do
+		allow_findings[line] = true
+	end
+	f:close()
+end
+
 -- Display findings
 local has_finding = false
 for file,findings in pairs(data) do
+	file = file:gsub("file://","")
 	for _,finding in ipairs(findings) do
-		print(file:gsub("file://","")..":"..(finding.range.start.line + 1)..": "..finding.message)
-		has_finding = true
+		print(file..":"..(finding.range.start.line + 1)..": "..finding.message)
+		if allow_findings and allow_findings[file] then
+			has_finding = true
+		end
 	end
 end
 
