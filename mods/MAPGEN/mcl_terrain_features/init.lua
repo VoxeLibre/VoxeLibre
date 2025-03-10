4294d0222e2f7a35892127ab508990b38bcd7f66
kno10 <erich.schubert@gmail.com>
2025-03-10 23:56:00 +0100

    move boulders, ice spikes, and corals to vl_biomes

--- mods/MAPGEN/mcl_terrain_features/init.lua
+++ mods/MAPGEN/mcl_terrain_features/init.lua
@@ -1,11 +1,9 @@
 local modname = minetest.get_current_modname()
 local modpath = minetest.get_modpath(modname)
 
-dofile(modpath.."/boulder.lua")
 dofile(modpath.."/fallentree.lua")
 dofile(modpath.."/fossil.lua")
 dofile(modpath.."/geode.lua")
-dofile(modpath.."/ice_spike.lua")
 dofile(modpath.."/lakes.lua")
 dofile(modpath.."/basaltdelta.lua")
 
