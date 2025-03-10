0fe24dc75162c61da0e15ba4b66330cd4219e80e
kno10 <erich.schubert@gmail.com>
2025-03-10 23:00:17 +0100

    improve lakes

--- mods/MAPGEN/mcl_terrain_features/lakes.lua
+++ mods/MAPGEN/mcl_terrain_features/lakes.lua
@@ -1,3 +1,10 @@
+-- TODO: overall, these pools tend to be very circular, can we make them more interesting?
+-- TODO: use the new vl_terraforming API?
+
+local mg_name = core.get_mapgen_setting("mg_name")
+
+local get_node_name = mcl_vars.get_node_name
+
 local adjacents = {
 	vector.new(1,0,0),
 	vector.new(1,0,1),
@@ -10,121 +17,113 @@ local adjacents = {
 	vector.new(0,-1,0)
 }
 
-local function airtower(pos,tbl,h)
-	for i=1,h do
-		table.insert(tbl,vector.offset(pos,0,i,0))
-	end
-end
-
-local function makelake(pos,size,liquid,placein,border,pr,noair)
-	local p1, p2 = vector.offset(pos,-size,-1,-size), vector.offset(pos,size,-1,size)
-	local e1, e2 = vector.offset(pos,-size,-2,-size), vector.offset(pos,size,15,size)
-	minetest.emerge_area(e1, e2, function(blockpos, action, calls_remaining, param)
+local function makelake(pos, size, liquid, placein, border, pr, noair)
+	local p1, p2 = vector.offset(pos,-size,0,-size), vector.offset(pos,size,0,size)
+	local e1, e2 = vector.offset(pos,-size,-1,-size), vector.offset(pos,size,15,size)
+	core.emerge_area(e1, e2, function(_, _, calls_remaining)
 		if calls_remaining ~= 0 then return end
-		local nn = minetest.find_nodes_in_area(p1,p2,placein)
-		if not nn[1] then return end
-		table.sort(nn,function(a, b)
+		local nn = core.find_nodes_in_area(p1, p2, placein)
+		if #nn == 0 then return end
+		table.sort(nn, function(a, b)
 		   return vector.distance(pos, a) < vector.distance(pos, b)
 		end)
-		local y = pos.y - 1
+		local y = pos.y
 		local lq, air = {}, {}
-		local r = pr:next(1,#nn)
+		local r = pr:next(1,math.ceil(#nn * 0.7)) -- circle is pi/4 of the square
 		for i=1,r do
-			airtower(nn[i],air,20)
-			table.insert(lq,nn[i])
+			table.insert(lq, nn[i])
+			for j = 1, 25 do
+				table.insert(air, vector.offset(nn[i], 0, j, 0))
+			end
 		end
-		minetest.bulk_set_node(lq,{name=liquid})
-		minetest.bulk_set_node(air,{name="air"})
-		air = {}
+		core.bulk_swap_node(lq, { name = liquid })
+		core.bulk_swap_node(air, { name = "air" })
 		local br = {}
 		for k,v in pairs(lq) do
 			for kk,vv in pairs(adjacents) do
 				local pp = vector.add(v,vv)
-				local an = minetest.get_node(pp)
-				if not border then
-					if minetest.get_item_group(an.name,"solid") > 0 then
-						border = an.name
-					elseif minetest.get_item_group(minetest.get_node(nn[1]).name,"solid") > 0 then
-						border = minetest.get_node_or_nil(nn[1]).name
-					else
-						border = "mcl_core:stone"
-					end
-					if border == nil or border == "mcl_core:dirt" then border = "mcl_core:dirt_with_grass" end
-				end
-				if not noair and an.name ~= liquid then
+				local an = get_node_name(pp)
+				if #br == 0 and core.get_item_group(an.name, "solid") > 0 then border = an end
+				if not noair and an ~= liquid then
 					table.insert(br,pp)
-					--[[ no need to have air above border:
-					local un = minetest.get_node(vector.offset(pp,0,1,0))
-					if un.name ~= liquid then
-						airtower(pp,air,20)
-					end]]--
 				end
 			end
 		end
-		minetest.bulk_set_node(br,{name=border})
-		minetest.bulk_set_node(air,{name="air"})
+		if border.name == "mcl_core:dirt_with_grass" and not border.param2 then
+			local biome = mg_name ~= "v6" and core.registered_biomes[core.get_biome_name(core.get_biome_data(nn[1]).biome)]
+			local p2 = biome and biome._mcl_grass_palette_index and biome._mcl_grass_palette_index or nil
+			border = { name = "mcl_core:dirt_with_grass", param2 = p2 } -- deliberate copy
+		end
+		core.bulk_swap_node(br, border)
 		return true
 	end)
 	return true
 end
 
-mcl_structures.register_structure("lavapool",{
-	place_on = {"group:sand", "group:dirt", "group:stone"},
+mcl_structures.register_structure("lavapool", {
+	place_on = { "group:sand", "group:dirt", "group:stone" },
 	terrain_feature = true,
 	noise_params = {
 		offset = 0,
 		scale = 0.0000022,
-		spread = {x = 250, y = 250, z = 250},
+		spread = vector.new(250, 250, 250),
 		seed = 78375213,
 		octaves = 3,
 		persist = 0.001,
 		flags = "absvalue",
 	},
-	flags = "place_center_x, place_center_z, all_floors",
+	flags = "place_center_x, place_center_z",
 	y_max = mcl_vars.mg_overworld_max,
-	y_min = minetest.get_mapgen_setting("water_level"),
-	place_func = function(pos,def,pr)
-		return makelake(pos,5,"mcl_core:lava_source",{"group:material_stone", "group:sand", "group:dirt"},"mcl_core:stone",pr)
+	y_min = core.get_mapgen_setting("water_level"),
+	place_func = function(pos, _, pr)
+		return makelake(pos, 5, "mcl_core:lava_source",
+			{ "group:material_stone", "group:sand", "group:dirt" },
+			{ name = "mcl_core:stone" }, pr)
 	end
 })
 
-mcl_structures.register_structure("water_lake",{
-	place_on = {"group:dirt","group:stone"},
+mcl_structures.register_structure("water_lake", {
+	place_on = { "group:dirt", "group:stone" },
 	terrain_feature = true,
 	noise_params = {
 		offset = 0,
 		scale = 0.000032,
-		spread = {x = 250, y = 250, z = 250},
+		spread = vector.new(250, 250, 250),
 		seed = 756641353,
 		octaves = 3,
 		persist = 0.001,
 		flags = "absvalue",
 	},
-	flags = "place_center_x, place_center_z, all_floors",
+	flags = "place_center_x, place_center_z",
 	y_max = mcl_vars.mg_overworld_max,
-	y_min = minetest.get_mapgen_setting("water_level"),
-	place_func = function(pos,def,pr)
-		return makelake(pos,5,"mcl_core:water_source",{"group:material_stone", "group:sand", "group:dirt","group:grass_block"},"mcl_core:dirt_with_grass",pr)
+	y_min = core.get_mapgen_setting("water_level"),
+	place_func = function(pos, _, pr)
+		return makelake(pos, 5, "mcl_core:water_source",
+			{ "group:material_stone", "group:sand", "group:dirt", "group:grass_block"},
+			{ name = "mcl_core:dirt_with_grass" }, pr)
 	end
 })
 
-mcl_structures.register_structure("water_lake_mangrove_swamp",{
-	place_on = {"mcl_mud:mud"},
+mcl_structures.register_structure("water_lake_mangrove_swamp", {
+	place_on = { "mcl_mud:mud" },
 	biomes = { "MangroveSwamp" },
 	terrain_feature = true,
 	noise_params = {
 		offset = 0,
 		scale = 0.0032,
-		spread = {x = 250, y = 250, z = 250},
+		spread = vector.new(250, 250, 250),
 		seed = 6343241353,
 		octaves = 3,
 		persist = 0.001,
 		flags = "absvalue",
 	},
-	flags = "place_center_x, place_center_z, all_floors",
+	flags = "place_center_x, place_center_z",
 	y_max = mcl_vars.mg_overworld_max,
-	y_min = minetest.get_mapgen_setting("water_level"),
-	place_func = function(pos,def,pr)
-		return makelake(pos,3,"mcl_core:water_source",{"group:material_stone", "group:sand", "group:dirt","group:grass_block","mcl_mud:mud"},"mcl_mud:mud",pr,true)
+	y_min = core.get_mapgen_setting("water_level"),
+	place_func = function(pos, _, pr)
+		return makelake(pos, 3, "mcl_core:water_source",
+			{ "group:material_stone", "group:sand", "group:dirt", "group:grass_block", "mcl_mud:mud"},
+			{ name = "mcl_mud:mud" }, pr, true)
 	end
 })
+
