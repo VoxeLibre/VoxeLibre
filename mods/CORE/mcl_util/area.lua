5b370358abd5336dafaa40d288305c1212382482
teknomunk <teknomunk@protonmail.com>
2025-03-14 07:13:14 -0500

    Update type annotation

--- mods/CORE/mcl_util/area.lua
+++ mods/CORE/mcl_util/area.lua
@@ -3,16 +3,16 @@
 -- Applies is_protected() to a 3D lattice of points in the defined volume. The points are spaced
 -- evenly throughout the volume and have a spacing similar to, but no larger than, "interval".
 --
--- @param pos1          A position table of the area volume's first edge.
--- @param pos2          A position table of the area volume's second edge.
--- @param player        The player performing the action.
--- @param interval	    Optional. Max spacing between checked points at the volume.
---      Default: Same as core.is_area_protected.
+-- @param pos1 vector.Vector      A position table of the area volume's first edge.
+-- @param pos2 vector.Vector      A position table of the area volume's second edge.
+-- @param player core.Player      The player performing the action.
+-- @param interval number         Optional. Max spacing between checked points at the volume.
+--                                Default: Same as core.is_area_protected.
 --
--- @return	true on protection violation detection. false otherwise.
+-- @return boolean true on protection violation detection and false otherwise.
 --
 -- @notes   *All corners and edges of the defined volume are checked.
-]]
+--]]
 function mcl_util.check_area_protection(pos1, pos2, player, interval)
 	local name = player and player:get_player_name() or ""
 
@@ -27,11 +27,11 @@ end
 
 --[[Check for a protection violation on a single position.
 --
--- @param position      A position table to check for protection violation.
--- @param player        The player performing the action.
+-- @param position vector.Vector    A position table to check for protection violation.
+-- @param player core.Player        The player performing the action.
 --
--- @return	true on protection violation detection. false otherwise.
-]]
+-- @return boolean true on protection violation detection and false otherwise.
+--]]
 function mcl_util.check_position_protection(position, player)
 	local name = player and player:get_player_name() or ""
 
@@ -52,6 +52,11 @@ if not vector.in_area then
 	end
 end
 
+---@param a0 vector.Vector
+---@param a1 vector.Vector
+---@param b0 vector.Vector
+---@param b1 vector.Vector
+---@return boolean
 function mcl_util.area_overlaps(a0, a1, b0, b1)
 	if not ( (a0.x <= b0.x and b0.x <= a1.x) or (a0.x <= b1.x and b1.x <= a1.x) ) then return false end
 	if not ( (a0.y <= b0.y and b0.y <= a1.y) or (a0.y <= b1.y and b1.y <= a1.y) ) then return false end
