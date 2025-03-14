9a6b400003034cd4e81eb1d6949169d3a484b8ed
teknomunk <teknomunk@protonmail.com>
2025-03-14 05:57:50 -0500

    Add unit tests, rename fake_metadata, add readonly support to fake metadata

---
+++ mods/CORE/mcl_util/fake_metadata.lua
@@ -0,0 +1,23 @@
+local fake_metadata_ref_metatable = {
+	__index = {
+		get_string = function(self, key)
+			return tostring(self.table[tostring(key)] or "")
+		end,
+		set_string = function(self, key, value)
+			if self.readonly then return end
+
+			self.table[tostring(key)] = tostring(value)
+			self:on_save()
+		end,
+	},
+}
+
+---@param data {table: table, on_save: fun(any)}
+---@return core.MetaDataRef
+function mcl_util.make_fake_metadata(data)
+	-- Validate argument
+	assert(type(data.table) == "table", "Fake metadata requires a 'table' field that is a table")
+	assert(type(data.on_save) == "function", "Fake metadata requires an 'on_save' field that is a function")
+
+	return setmetatable(data, fake_metadata_ref_metatable)
+end
