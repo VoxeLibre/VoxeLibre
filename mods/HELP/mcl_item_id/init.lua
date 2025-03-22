316b2c08fb1a8e0b263fe0df8e40db2e65e2c0fc
teknomunk <teknomunk@protonmail.com>
2025-03-22 11:24:55 +0100

    Don't register aliases after game has started (#5041)
    Reviewed-on: https://git.minetest.land/VoxeLibre/VoxeLibre/pulls/5041
    Reviewed-by: kno10 <kno10@noreply.git.minetest.land>
    Co-authored-by: teknomunk <teknomunk@protonmail.com>
    Co-committed-by: teknomunk <teknomunk@protonmail.com>

--- mods/HELP/mcl_item_id/init.lua
+++ mods/HELP/mcl_item_id/init.lua
@@ -5,7 +5,7 @@ mcl_item_id = {
 local game = "voxelibre"
 
 function mcl_item_id.set_mod_namespace(modname, namespace)
-    local namespace = namespace or modname
+    namespace = namespace or modname
     mcl_item_id.mod_namespaces[modname] = namespace
 end
 
@@ -36,30 +36,43 @@ local same_id = {
     },
 }
 
+---@class core.ItemDef
+---@field _mcl_item_id string
+
 tt.register_snippet(function(itemstring)
-    local def = minetest.registered_items[itemstring]
-    local item_split = itemstring:find(":")
-    local id_string = itemstring:sub(item_split)
-    local id_modname = itemstring:sub(1, item_split - 1)
-    local new_id = game .. id_string
-    local alt_id = "mineclone" .. id_string
-    local mod_namespace = mcl_item_id.get_mod_namespace(id_modname)
-    for mod, ids in pairs(same_id) do
-        for _, id in pairs(ids) do
-            if itemstring == "mcl_" .. mod .. ":" .. id  then
-                new_id = game .. ":" .. id .. "_" .. mod:gsub("s", "")
-                alt_id = "mineclone:" .. id .. "_" .. mod:gsub("s", "")
-            end
-        end
-    end
-    if mod_namespace ~= game then
-        new_id = mod_namespace .. id_string
-    end
-    if mod_namespace ~= id_modname then
-        minetest.register_alias_force(new_id, itemstring)
-        minetest.register_alias_force(alt_id, itemstring)
-    end
-    if minetest.settings:get_bool("mcl_item_id_debug", false) then
-        return new_id, "#555555"
-    end
+	if not core.settings:get_bool("mcl_item_id_debug", false) then return end
+
+	local def = core.registered_items[itemstring]
+	new_id = def._mcl_item_id
+	return new_id, "#555555"
+end)
+
+core.register_on_mods_loaded(function()
+	for itemstring,_ in pairs(core.registered_items) do
+		local item_split = itemstring:find(":")
+		if item_split then
+			local id_string = itemstring:sub(item_split)
+			local id_modname = itemstring:sub(1, item_split - 1)
+			local new_id = game .. id_string
+			local alt_id = "mineclone" .. id_string
+			local mod_namespace = mcl_item_id.get_mod_namespace(id_modname)
+			for mod, ids in pairs(same_id) do
+				for _, id in pairs(ids) do
+					if itemstring == "mcl_" .. mod .. ":" .. id  then
+						new_id = game .. ":" .. id .. "_" .. mod:gsub("s", "")
+						alt_id = "mineclone:" .. id .. "_" .. mod:gsub("s", "")
+					end
+				end
+			end
+			if mod_namespace ~= game then
+				new_id = mod_namespace .. id_string
+			end
+			if mod_namespace ~= id_modname then
+				core.register_alias_force(new_id, itemstring)
+				core.register_alias_force(alt_id, itemstring)
+			end
+
+			core.override_item(itemstring, {_mcl_item_id = new_id})
+		end
+	end
 end)
