6149b77c6ab957f78c1c19fee5250ff9501c9acc
teknomunk <teknomunk@protonmail.com>
2025-04-13 18:01:00 +0000

    Rework docker container build

--- tests/docker/build.sh
+++ tests/docker/build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-echo "Enter repo: "
-read REPO
-
-sed -i "s#REPO#$REPO#" Dockerfile
+cp tests/docker/Dockerfile .
 docker build . -t 127.0.0.1:5000/voxelibre-test:latest
-git restore Dockerfile
-
