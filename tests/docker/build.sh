7f3cd330c5a465293228cfaf2db4c04efb7cbf2c
teknomunk <teknomunk@protonmail.com>
2025-04-16 09:58:31 +0000

    Update docker image build scripts
    Checkpoint attempt to debug build-luanti-head.sh
    
    Another attempt to get all versions built
    
    Get luanti built from source building and running correctly

--- tests/docker/build.sh
+++ tests/docker/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+(
+	cd tests/docker
+	test -e luanti && ( cd luanti; git checkout master; git pull ) || git clone https://github.com/luanti-org/luanti.git luanti
+	test -e irrlichtmt && ( cd irrlichtmt; git checkout master; git pull ) || git clone https://github.com/minetest/irrlicht.git irrlichtmt
+)
 cp tests/docker/Dockerfile .
 docker build . -t 127.0.0.1:5000/voxelibre-test:latest
+#docker build . -t voxelibre-test
