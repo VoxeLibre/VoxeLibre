9df023ac84313ec01ed3cde770723a2892651890
teknomunk <teknomunk@protonmail.com>
2025-05-17 08:01:39 -0500

    Move IcePlains to its own file
    Correct filename

--- mods/MAPGEN/mcl_biomes/overworld.lua
+++ mods/MAPGEN/mcl_biomes/overworld.lua
@@ -1,3 +1,15 @@
+local modpath = core.get_modpath(core.get_current_modname())
+
+local overworld_path = modpath..DIR_DELIM.."overworld"..DIR_DELIM
+local ice_plains_spikes = dofile(overworld_path.."ice_plains_spikes.lua")
+
+local overworld_fogcolor = "#C0D8FF"
+local frozen_waterfogcolor = "#3938C9"
+local OCEAN_MIN = -15
+local ocean_skycolor = "#7BA4FF" -- This is the case for all ocean biomes except for non-deep frozen oceans! Those oceans will have their own colour instead of this one.
+local cold_waterfogcolor = "#3D57D6"
+local beach_skycolor = "#78A7FF" -- This is the case for all beach biomes except for the snowy ones! Those beaches will have their own colour instead of this one.
+
 -- List of Overworld biomes without modifiers.
 -- IMPORTANT: Don't forget to add new Overworld biomes to this list!
 local overworld_biomes = {
@@ -41,51 +53,7 @@ local overworld_biomes = {
 return {
 	biomes = overworld_biomes,
 	register_biomes = function()
-		-- Ice Plains Spikes (rare)
-		minetest.register_biome({
-			name = "IcePlainsSpikes",
-			node_top = "mcl_core:snowblock",
-			depth_top = 1,
-			node_filler = "mcl_core:dirt",
-			depth_filler = 2,
-			node_water_top = "mcl_core:ice",
-			depth_water_top = 1,
-			node_river_water = "mcl_core:ice",
-			node_riverbed = "mcl_core:sand",
-			depth_riverbed = 2,
-			y_min = 1,
-			y_max = mcl_vars.mg_overworld_max,
-			humidity_point = 24,
-			heat_point = -5,
-			_mcl_biome_type = "snowy",
-			_mcl_grass_palette_index = 2,
-			_mcl_foliage_palette_index = 2,
-			_mcl_water_palette_index = 5,
-			_mcl_waterfogcolor = frozen_waterfogcolor,
-			_mcl_skycolor = "#7FA1FF",
-			_mcl_fogcolor = overworld_fogcolor
-		})
-		minetest.register_biome({
-			name = "IcePlainsSpikes_ocean",
-			node_top = "mcl_core:gravel",
-			depth_top = 2,
-			node_filler = "mcl_core:gravel",
-			depth_filler = 3,
-			node_river_water = "mcl_core:ice",
-			node_riverbed = "mcl_core:sand",
-			depth_riverbed = 2,
-			y_min = OCEAN_MIN,
-			y_max = 0,
-			humidity_point = 24,
-			heat_point = -5,
-			_mcl_biome_type = "snowy",
-			_mcl_grass_palette_index = 2,
-			_mcl_foliage_palette_index = 2,
-			_mcl_water_palette_index = 5,
-			_mcl_waterfogcolor = frozen_waterfogcolor,
-			_mcl_skycolor = "#7FA1FF",
-			_mcl_fogcolor = overworld_fogcolor
-		})
+		ice_plains_spikes.register_biomes()
 
 		-- Cold Taiga
 		minetest.register_biome({
