fcb9ebb0ad37379fdcb62212c0a061f9f77b3ce1
the-real-herowl <wiktor_t-i@proton.me>
2025-05-18 02:57:01 +0200

    Fixed clock crash

--- mods/ITEMS/mcl_clock/init.lua
+++ mods/ITEMS/mcl_clock/init.lua
@@ -99,7 +99,7 @@ minetest.register_globalstep(function(dtime)
 		for s, stack in pairs(inv:get_list("main")) do
 			local frame
 			-- Clocks do not work in certain zones
-			if not mcl_worlds.clock_works(player:get_pos()) then
+			if not mcl_compass.compass_works(player:get_pos()) then
 				frame = random_frame
 			else
 				frame = now
