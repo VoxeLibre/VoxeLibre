2653c7f6c57b78cc3c4ee0846179f5e9201078b7
teknomunk <teknomunk@protonmail.com>
2025-05-24 13:01:26 -0500

    Change binary installation directory for multiple luanti version builds

--- tests/docker/build-luanti-versions.sh
+++ tests/docker/build-luanti-versions.sh
@@ -10,25 +10,25 @@ build-5.11.0()
 {
 	git checkout tags/5.11.0
 	cmake . -DRUN_IN_PLACE=TRUE
-	make -j$(nproc) && ln -sf $PWD/bin/luanti /usr/local/bin/luanti-5.11.0
+	make -j$(nproc) && ln -sf $PWD/bin/luanti $INSTALL_BIN/luanti-5.11.0
 }
 build-5.10.0()
 {
 	git checkout tags/5.10.0
 	cmake . -DRUN_IN_PLACE=TRUE
-	make -j$(nproc) && ln -sf $PWD/bin/luanti /usr/local/bin/luanti-5.10.0
+	make -j$(nproc) && ln -sf $PWD/bin/luanti $INSTALL_BIN/luanti-5.10.0
 }
 build-5.9.1()
 {
 	git checkout tags/5.9.1
 	cmake . -DRUN_IN_PLACE=TRUE
-	make -j$(nproc) && ln -sf $PWD/bin/minetest /usr/local/bin/luanti-5.9.1
+	make -j$(nproc) && ln -sf $PWD/bin/minetest $INSTALL_BIN/luanti-5.9.1
 }
 build-5.9.0()
 {
 	git checkout tags/5.9.0
 	cmake . -DRUN_IN_PLACE=TRUE
-	make -j$(nproc) && ln -sf $PWD/bin/minetest /usr/local/bin/luanti-5.9.0
+	make -j$(nproc) && ln -sf $PWD/bin/minetest $INSTALL_BIN/luanti-5.9.0
 }
 build-5.8.0()
 {
@@ -38,7 +38,7 @@ build-5.8.0()
 	sed -e '27i#include <algorithm>' -i src/client/sound/sound_data.cpp
 	export CXX_FLAGS=-std=c++20
 	cmake . -DRUN_IN_PLACE=TRUE
-	make -j$(nproc) && ln -sf $PWD/bin/minetest /usr/local/bin/luanti-5.8.0
+	make -j$(nproc) && ln -sf $PWD/bin/minetest $INSTALL_BIN/luanti-5.8.0
 }
 build-5.7.0()
 {
@@ -46,7 +46,7 @@ build-5.7.0()
 	ln -sf ../../../irrlichtmt lib/irrlichtmt
 	( cd lib/irrlichtmt; git checkout tags/1.9.0mt10 )
 	cmake . -DRUN_IN_PLACE=TRUE
-	make -j$(nproc) && ln -sf $PWD/bin/minetest /usr/local/bin/luanti-5.7.0
+	make -j$(nproc) && ln -sf $PWD/bin/minetest $INSTALL_BIN/luanti-5.7.0
 }
 build-5.6.1()
 {
@@ -54,26 +54,28 @@ build-5.6.1()
 	ln -sf ../../../irrlichtmt lib/irrlichtmt
 	( cd lib/irrlichtmt; git checkout tags/1.9.0mt8 )
 	cmake . -DRUN_IN_PLACE=TRUE
-	make -j$(nproc) && ln -sf $PWD/bin/minetest /usr/local/bin/luanti-5.6.1
+	make -j$(nproc) && ln -sf $PWD/bin/minetest $INSTALL_BIN/luanti-5.6.1
 }
 build-5.6.0()
 {
 	git checkout tags/5.6.0
 	ln -sf ../../../irrlichtmt lib/irrlichtmt
 	cmake . -DRUN_IN_PLACE=TRUE
-	make -j$(nproc) && ln -sf $PWD/bin/minetest /usr/local/bin/luanti-5.6.0
+	make -j$(nproc) && ln -sf $PWD/bin/minetest $INSTALL_BIN/luanti-5.6.0
 }
 
 mkdir -p bin build
+INSTALL_BIN=$PWD/bin
 for VERSION in $VERSIONS; do
 	rm -f bin/luanti-$VERSION
 	(
 		cd build/
+		rm -Rvf luanti-$VERSION || true
 		git clone ../luanti/ luanti-$VERSION
 		cd luanti-$VERSION
 		rm -Rvf games || true
 		rm -Rvf worlds || true
-		rm /usr/local/bin/luanti-$VERSION || true
+		rm $INSTALL_BIN/luanti-$VERSION || true
 		build-$VERSION
 		rm -Rvf games || true
 		ln -sf ../../games games
