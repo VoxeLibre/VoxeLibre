cb88c7bde9c78a4174fdc7e6a51924801e76a3c0
SmokeyDope <smokey@tilde.team>
2025-05-25 03:56:31 +0200

    Add function to concatenate new groups to registered nodes in vl_seasons item overrides registry

--- mods/CORE/vl_seasons/node-overrides.lua
+++ mods/CORE/vl_seasons/node-overrides.lua
@@ -1,22 +1,44 @@
-core.override_item("mcl_core:snowblock", {
-	groups = {oxidizable = 1,},
-    _mcl_oxidized_seasonal_variant = "mcl_core:dirt_with_grass",
-    _mcl_oxidized_season_disallowed = {"winter"},
-})
+-- Helper function to safely get existing groups of the node (if any) and merge with new ones (adding `oxidation = 1` in this case)
+local function add_oxidizable_group(node_name)
+    local original_node = minetest.registered_nodes[node_name]
+    if not original_node then
+        minetest.log("error", "[MODNAME] Failed to find original node '" .. node_name .. "'")
+        return {}
+    end
+
+    -- Copy existing groups into a new table
+    local merged_groups = {}
+    for k, v in pairs(original_node.groups or {}) do
+        merged_groups[k] = v
+    end
+
+    -- Add the new group
+    merged_groups.oxidizable = 1
+
+    return merged_groups
+end
+
+-- Override Registry
 core.override_item("mcl_core:dirt_with_grass", {
-	groups = {oxidizable = 1,},
+    groups = add_oxidizable_group("mcl_core:dirt_with_grass"),
     _mcl_oxidized_seasonal_variant = "mcl_core:snowblock",
     _mcl_oxidized_season_disallowed = {"spring", "summer", "fall"},
 })
 
+core.override_item("mcl_core:snowblock", {
+    groups = add_oxidizable_group("mcl_core:snowblock"),
+    _mcl_oxidized_seasonal_variant = "mcl_core:dirt_with_grass",
+    _mcl_oxidized_season_disallowed = {"winter"},
+})
 
 core.override_item("mcl_core:ice", {
-	groups = {oxidizable = 1,},
+    groups = add_oxidizable_group("mcl_core:ice"),
     _mcl_oxidized_seasonal_variant = "mcl_core:water_source",
     _mcl_oxidized_season_disallowed = {"winter"},
 })
+
 core.override_item("mcl_core:water_source", {
-	groups = {oxidizable = 1,}, 
+    groups = add_oxidizable_group("mcl_core:water_source"),
    _mcl_oxidized_seasonal_variant = "mcl_core:ice",
-    _mcl_oxidized_season_disallowed = {"spring", "summer","fall"},
+   _mcl_oxidized_season_disallowed = {"spring", "summer","fall"},
 })
