ce6988631845f6f98bb8ad44e6b43b48a9e5e95a
teknomunk <teknomunk@protonmail.com>
2025-06-12 07:05:43 -0500

    Get sh test.sh working again

--- test.sh
+++ test.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+tests/docker/build-luanti-versions.sh
+tests/docker/build-luanti-head.sh
+
+export PATH=$PATH:$PWD/bin/
+
 sh tests/server-startup.sh
 sh tests/unit-tests.sh
 sh tests/luacheck/test.sh || true
