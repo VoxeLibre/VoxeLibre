78f9d79f570f5ad1ec7dd855127c703803be24d0
teknomunk <teknomunk@protonmail.com>
2025-06-12 07:05:43 -0500

    Get sh test.sh working again

--- tests/lua-language-server.sh
+++ tests/lua-language-server.sh
@@ -5,15 +5,13 @@ set -e
 
 # Make sure luanti lls definitions are up-to-date
 (
-	mkdir -p /usr/share/luanti/
-	cd /usr/share/luanti
 	# TODO: change to https://codeberg.org/fgaz/luanti-lls-definitions after https://codeberg.org/fgaz/luanti-lls-definitions/pulls/1 is merged
 	git clone --single-branch --branch fill-out-definitions --depth 1 https://codeberg.org/teknomunk/luanti-lls-definitions.git || true
 
-	if ! test -d /usr/share/luanti/builtin; then
-		ln /usr/share/minetest/builtin /usr/share/luanti/builtin -s
-	fi
+#	if ! test -d /usr/share/luanti/builtin; then
+#		ln /usr/share/minetest/builtin /usr/share/luanti/builtin -s
+#	fi
 )
 
-lua-language-server --check . --log check.log --logpath ./log | tr '\r' '\n' || true
+lua-language-server --check . --check_format json --logpath ./log | tr '\r' '\n' || true
 $LUA tests/display-lls-check-log.lua
