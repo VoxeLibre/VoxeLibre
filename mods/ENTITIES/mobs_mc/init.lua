d6af0c2d85e34b79dbf3a86b807c3cbd6b3bc253
Thomas Conway <tomconwork@gmail.com>
2025-07-16 16:51:56 +0200

    add lesser slime drakes to mobs_mc init

--- mods/ENTITIES/mobs_mc/init.lua
+++ mods/ENTITIES/mobs_mc/init.lua
@@ -143,6 +143,7 @@ dofile(path .. "/slime+magma_cube.lua") -- Wuzzy
 dofile(path .. "/spider.lua") -- Spider by AspireMint (fishyWET (CC-BY-SA 3.0 license for texture)
 dofile(path .. "/vex.lua") -- KrupnoPavel
 dofile(path .. "/wither.lua") -- Mesh and animation by toby109tt  / https://github.com/22i
+dofile(path .. "/lesser_slime_drake.lua") -- mesh by toby109tt, textures by Thomas Conway
 
 dofile(path .. "/cod.lua")
 dofile(path .. "/salmon.lua")
