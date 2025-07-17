633daf0954dd7050b9209b4fda744f218c33e669
the-real-herowl <the-real-herowl@noreply.git.minetest.land>
2025-07-17 23:26:08 +0200

    Now /effect command can affect any player

--- mods/ITEMS/mcl_potions/commands.lua
+++ mods/ITEMS/mcl_potions/commands.lua
@@ -9,8 +9,8 @@ local S = minetest.get_translator(minetest.get_current_modname())
 
 
 minetest.register_chatcommand("effect",{
-	params = S("<effect>|heal|list|clear|remove <duration|heal-amount|effect>|INF [<level>] [<factor>] [NOPART]"),
-	description = S("Add a status effect to yourself. Arguments: <effect>: name of status effect. Passing \"list\" as effect name lists available effects. Passing \"heal\" as effect name heals (or harms) by amount designed by the next parameter. Passing \"clear\" as effect name removes all effects. Passing \"remove\" as effect name removes the effect named by the next parameter. <duration>: duration in seconds. Passing \"INF\" as duration makes the effect infinite. (<heal-amount>: amount of healing when the effect is \"heal\", passing a negative value subtracts health. <effect>: name of a status effect to be removed when using \"remove\" as the previous parameter.) <level>: effect power determinant, bigger level results in more powerful effect for effects that depend on the level (no changes for other effects), defaults to 1, pass F to use low-level factor instead. <factor>: effect strength modifier, can mean different things depending on the effect, no changes for effects that do not depend on level/factor. NOPART at the end means no particles will be shown for this effect."),
+	params = S("<effect>|heal|list|clear|remove <duration|heal-amount|effect>|INF [<level>] [<factor>] [NOPART] [<player>]"),
+	description = S("Add a status effect to yourself or another player. Arguments: <effect>: name of status effect. Passing \"list\" as effect name lists available effects. Passing \"heal\" as effect name heals (or harms) by amount designed by the next parameter. Passing \"clear\" as effect name removes all effects. Passing \"remove\" as effect name removes the effect named by the next parameter. <duration>: duration in seconds. Passing \"INF\" as duration makes the effect infinite. (<heal-amount>: amount of healing when the effect is \"heal\", passing a negative value subtracts health. <effect>: name of a status effect to be removed when using \"remove\" as the previous parameter.) <level>: effect power determinant, bigger level results in more powerful effect for effects that depend on the level (no changes for other effects), defaults to 1, pass F to use low-level factor instead. <factor>: effect strength modifier, can mean different things depending on the effect, no changes for effects that do not depend on level/factor. NOPART by the end means no particles will be shown for this effect. Appending a player name to any command will make it try to conduct the operation on that player rather than the command user. If the name doesn't exist, nothing happens."),
 	privs = {server = true},
 	func = function(name, params)
 
@@ -34,7 +34,12 @@ minetest.register_chatcommand("effect",{
 			if not hp or hp == 0 then
 				return false, S("Missing or invalid heal amount parameter!")
 			else
-				mcl_potions.healing_func(minetest.get_player_by_name(name), hp)
+				name = P[3] or name
+				local target = core.get_player_by_name(name)
+				if not target then
+					return false, S("Target player @1 not found!", name)
+				end
+				mcl_potions.healing_func(target, hp)
 				if hp > 0 then
 					if hp < 1 then hp = 1 end
 					return true, S("Player @1 healed by @2 HP.", name, hp)
@@ -44,14 +49,24 @@ minetest.register_chatcommand("effect",{
 				end
 			end
 		elseif P[1] == "clear" then
-			mcl_potions._reset_effects(minetest.get_player_by_name(name))
+			name = P[2] or name
+			local target = core.get_player_by_name(name)
+			if not target then
+				return false, S("Target player @1 not found!", name)
+			end
+			mcl_potions._reset_effects(target)
 			return true, S("Effects cleared for player @1", name)
 		elseif P[1] == "remove" then
 			if not P[2] then
 				return false, S("Missing effect parameter!")
 			end
 			if mcl_potions.registered_effects[P[2]] then
-				mcl_potions.clear_effect(minetest.get_player_by_name(name), P[2])
+				name = P[3] or name
+				local target = core.get_player_by_name(name)
+				if not target then
+					return false, S("Target player @1 not found!", name)
+				end
+				mcl_potions.clear_effect(target, P[2])
 				return true, S("Removed effect @1 from player @2", P[2], name)
 			else
 				return false, S("@1 is not an available status effect.", P[2])
@@ -84,7 +99,12 @@ minetest.register_chatcommand("effect",{
 		local def = mcl_potions.registered_effects[P[1]]
 		if def then
 			if P[3] == "F" then
-				local given = mcl_potions.give_effect(P[1], minetest.get_player_by_name(name), tonumber(P[4]), inf and "INF" or tonumber(P[2]), nopart)
+				name = P[5] or name
+				local target = core.get_player_by_name(name)
+				if not target then
+					return false, S("Target player @1 not found!", name)
+				end
+				local given = mcl_potions.give_effect(P[1], target, tonumber(P[4]), inf and "INF" or tonumber(P[2]), nopart)
 				if given then
 					if def.uses_factor then
 						return true, S("@1 effect given to player @2 for @3 seconds with factor of @4.", def.description, name, P[2], P[4])
@@ -95,7 +115,12 @@ minetest.register_chatcommand("effect",{
 					return false, S("Giving effect @1 to player @2 failed.", def.description, name)
 				end
 			else
-				local given = mcl_potions.give_effect_by_level(P[1], minetest.get_player_by_name(name), tonumber(P[3]), inf and "INF" or tonumber(P[2]), nopart)
+				name = P[4] or name
+				local target = core.get_player_by_name(name)
+				if not target then
+					return false, S("Target player @1 not found!", name)
+				end
+				local given = mcl_potions.give_effect_by_level(P[1], target, tonumber(P[3]), inf and "INF" or tonumber(P[2]), nopart)
 				if given then
 					if def.uses_factor then
 						return true, S("@1 effect on level @2 given to player @3 for @4 seconds.", def.description, P[3], name, P[2])
