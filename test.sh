1086bbc41c48d84c744ff82ece317ebbf8c137be
teknomunk <teknomunk@protonmail.com>
2025-08-18 09:41:44 -0500

    Get test.sh to run inside docker correctly

--- test.sh
+++ test.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-tests/docker/build-luanti-versions.sh
-tests/docker/build-luanti-head.sh
-
-export PATH=$PATH:$PWD/bin/
+if ! test -d /opt/luanti-HEAD; then
+	tests/docker/build-luanti-versions.sh
+	tests/docker/build-luanti-head.sh
+	export PATH=$PATH:$PWD/bin/
+fi
 
 sh tests/server-startup.sh
 sh tests/unit-tests.sh
