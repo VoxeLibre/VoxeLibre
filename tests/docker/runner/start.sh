52330a2c050d443b6f2501173d4d2ce8dd37ca3d
teknomunk <teknomunk@protonmail.com>
2025-08-30 00:34:56 +0000

    Update tests/docker/README.md, add luanti .tar.gz package build script and support files, add Forgejo runner compose.yml

--- tests/docker/runner/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker run --rm --network=host \
-	-u 1000:$(stat -c '%g' /var/run/docker.sock) \
-	-v data:/data \
-	-v /var/run/docker.sock:/var/run/docker.sock --detach=true \
-	code.forgejo.org/forgejo/runner:4.0.0 forgejo-runner daemon
