ba17aa9cee88855882b4dca50c84606d4ff1305b
teknomunk <teknomunk@protonmail.com>
2025-10-14 05:59:14 -0500

    Switch out fgaz's annotations for Archie's

--- tests/lua-language-server.sh
+++ tests/lua-language-server.sh
@@ -5,8 +5,7 @@ set -e
 
 # Make sure luanti lls definitions are up-to-date
 (
-	# TODO: change to https://codeberg.org/fgaz/luanti-lls-definitions after https://codeberg.org/fgaz/luanti-lls-definitions/pulls/1 is merged
-	git clone --single-branch --branch fill-out-definitions --depth 1 https://codeberg.org/teknomunk/luanti-lls-definitions.git || true
+	git clone --single-branch --depth 1 https://git.minetest.land/archie/luanti-api.git || true
 
 #	if ! test -d /usr/share/luanti/builtin; then
 #		ln /usr/share/minetest/builtin /usr/share/luanti/builtin -s
