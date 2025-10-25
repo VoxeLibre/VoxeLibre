e6101c1afecbe6ea0c38ddc2e72f24283aa10e8f
TANGaming <tangaming123456@outlook.com>
2025-10-25 17:52:29 +0200

    Make sculk sounds less loud + add place sound (#5414)
    Reviewed-on: https://git.minetest.land/VoxeLibre/VoxeLibre/pulls/5414
    Reviewed-by: archie <archie@noreply.git.minetest.land>
    Co-authored-by: TANGaming <tangaming123456@outlook.com>
    Co-committed-by: TANGaming <tangaming123456@outlook.com>

--- mods/ITEMS/mcl_sculk/init.lua
+++ mods/ITEMS/mcl_sculk/init.lua
@@ -7,8 +7,9 @@ local mt_sound_play = minetest.sound_play
 local spread_to = {"mcl_core:stone","mcl_core:dirt","mcl_core:sand","mcl_core:dirt_with_grass","group:grass_block","mcl_core:andesite","mcl_core:diorite","mcl_core:granite","mcl_core:mycelium","group:dirt","mcl_end:end_stone","mcl_nether:netherrack","mcl_blackstone:basalt","mcl_nether:soul_sand","mcl_blackstone:soul_soil","mcl_crimson:warped_nylium","mcl_crimson:crimson_nylium","mcl_core:gravel"}
 
 local sounds = {
-	footstep = {name = "mcl_sculk_block", },
-	dug      = {name = "mcl_sculk_block", },
+	place    = {name = "mcl_sculk_block", gain = 0.6},
+	footstep = {name = "mcl_sculk_block", gain = 0.3},
+	dug      = {name = "mcl_sculk_block", gain = 0.6},
 }
 
 local gamerule_doSculkSpreading = true
