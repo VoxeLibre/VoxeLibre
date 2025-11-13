c26287cc8c9275ac6a7312c580c2b0b46d850ea1
Delta <delta@noreply.git.minetest.land>
2025-11-13 22:01:46 +0100

    Stop underground music playing when reaching surface

--- mods/PLAYER/mcl_music/init.lua
+++ mods/PLAYER/mcl_music/init.lua
@@ -17,7 +17,7 @@ local lonely_blossom = "exhale_and_tim_unwin-lonely_blossom"
 local valley_of_ghosts = "exhale_and_tim_unwin-valley_of_ghosts"
 local farmer = "exhale_and_tim_unwin-farmer"
 
-local dimension_to_base_track = {
+local scenario_to_base_track = {
 	["overworld"]	= {pianowtune, never_grow_up, flock_of_one, gift, hailing_forest, lonely_blossom, farmer},
 	["nether"]		= {nether_tune, valley_of_ghosts},
 	["end"]			= {end_tune},
@@ -26,29 +26,22 @@ local dimension_to_base_track = {
 
 local listeners = {}
 
-local function pick_track(dimension, underground)
-	local track_key
+local function pick_track(scenario)
+	local scenario_tracks = scenario_to_base_track[scenario]
 
-	if dimension == "overworld" and underground then
-		track_key = "mining"
-	else
-		-- Pick random dimension song
-		track_key = dimension
-	end
-
-	local dimension_tracks = dimension_to_base_track[track_key]
-
-	if dimension_tracks and #dimension_tracks >= 1 then
+	if scenario_tracks and #scenario_tracks >= 1 then
 		local index = 1
-		if #dimension_tracks > 1 then
-			index = math.random(1, #dimension_tracks)
+		if #scenario_tracks > 1 then
+			index = math.random(1, #scenario_tracks)
 		end
-		local chosen_track = dimension_tracks[index]
+
+		local chosen_track = scenario_tracks[index]
 		--minetest.log("chosen_track: " .. chosen_track)
-		minetest.log("action", "[mcl_music] Playing track: " .. chosen_track .. ", for context: " .. track_key)
+		minetest.log("action", "[mcl_music] Playing track: " .. chosen_track .. ", for scenario: " .. scenario)
+
 		return chosen_track
 	else
-		-- ?
+		minetest.log("error", "[mcl_music] No tracks found for this scenario!")
 	end
 
 	return nil
@@ -74,7 +67,7 @@ local function stop_music_for_all()
 	end
 end
 
-local function play_song(track, player_name, dimension, day_count)
+local function play_song(track, player_name, scenario, day_count)
 	local spec = {
 		name  = track,
 		gain  = 0.3,
@@ -89,13 +82,14 @@ local function play_song(track, player_name, dimension, day_count)
 	local handle = minetest.sound_play(spec, parameters, false)
 	listeners[player_name] = {
 		handle     = handle,
-		dimension  = dimension,
+		scenario   = scenario,
 		day_count  = day_count,
 	}
 end
 
 local function play()
 	local time = minetest.get_timeofday()
+	-- Night time
 	if time < 0.25 or time >= 0.75 then
 		stop_music_for_all()
 		minetest.after(10, play)
@@ -105,38 +99,42 @@ local function play()
 	local day_count = minetest.get_day_count()
 	for _, player in pairs(minetest.get_connected_players()) do
 		if not player:get_meta():get("mcl_music:disable") then
+
 			local player_name = player:get_player_name()
-			local hp          = player:get_hp()
+			--local hp        = player:get_hp()
 			local pos         = player:get_pos()
 			local dimension   = mcl_worlds.pos_to_dimension(pos)
 
-			local listener      = listeners[player_name]
-			local handle = listener and listener.handle
+			-- Find current scenario
+			local scenario = dimension
+			if (dimension == "overworld" and pos and pos.y < 0) then
+				scenario = "mining"
+			end
 
-			--local old_hp			= listener and listener.hp
-			--local is_hp_changed 	= old_hp and (math.abs(old_hp - hp) > 0.00001) or false
+			local listener = listeners[player_name]
+			local handle   = listener and listener.handle
 
-			local old_dimension		= listener and listener.dimension
-			local is_dimension_changed = old_dimension and (old_dimension ~= dimension) or false
+			-- Compare with previous scenario
+			--local old_hp		   = listener and listener.hp
+			--local is_hp_changed 	   = old_hp and (math.abs(old_hp - hp) > 0.00001) or false
+			local old_scenario	   = listener and listener.scenario
+			local has_scenario_changed = old_scenario and (old_scenario ~= scenario) or false
 
 			-- minetest.log("handle: " .. dump (handle))
-			if is_dimension_changed then
+			if has_scenario_changed then
 				stop_music_for_listener_name(player_name)
 				if not listeners[player_name] then
 					listeners[player_name] = {}
 				end
-				listeners[player_name].hp = hp
-				listeners[player_name].dimension = dimension
+				--listeners[player_name].hp     = hp
+				listeners[player_name].scenario = scenario
+
+			-- Decide if music should be played
 			elseif not handle and (not listener or (listener.day_count ~= day_count)) then
-				local underground = dimension == "overworld" and pos and pos.y < 0
-				local track = pick_track(dimension, underground)
+				local track = pick_track(scenario)
 				if track then
-					play_song(track, player_name, dimension, day_count)
-				else
-					--minetest.log("no track found. weird")
+					play_song(track, player_name, scenario, day_count)
 				end
-			else
-				--minetest.log("else")
 			end
 		end
 	end
@@ -215,8 +213,8 @@ minetest.register_chatcommand("music", {
 			meta:set_int("mcl_music:disable", 1)
 			display_new_state = S("off")
 		end
-		
+
 		stop_music_for_listener_name(playername)
 		minetest.chat_send_player(sender_name, S("Set music for @1 to: @2", playername, display_new_state))
-	end, 
+	end,
 })
