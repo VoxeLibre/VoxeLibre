9176d1a4c0cd429e3ba298ed912cdafd09c6ecca
Delta <delta@noreply.git.minetest.land>
2025-12-16 21:18:16 +0100

    Changes based on review

--- mods/PLAYER/vl_cavesounds/init.lua
+++ mods/PLAYER/vl_cavesounds/init.lua
@@ -1,23 +1,22 @@
-local S = core.get_translator(core.get_current_modname())
-
 local modname = core.get_current_modname()
 local modpath = core.get_modpath(modname)
 
-local cave_sounds_enabled = core.settings:get_bool("cavesounds", true)
+local S = core.get_translator(modname)
+
+local cave_sounds_enabled = core.settings:get_bool("vl_cavesounds", true)
 
 -- Can be tweaked
 
-local seconds_in_pitch_black_before_sound = 300      -- Seconds
+local seconds_in_pitch_black_before_sound = 15      -- Seconds
 local interval = 3 			             -- Seconds
 local max_same_sound_for_other_players_distance = 50 -- Nodes
 
 --
 
 local max_light_lvl = 15
-local max_fear_per_interval = seconds_in_pitch_black_before_sound / (seconds_in_pitch_black_before_sound / interval)
-local slope = max_fear_per_interval / max_light_lvl
+local slope = interval / max_light_lvl
 
-local sounds = {"cave1", "cave2", "cave3", "cave4", "cave5", "cave6"}
+local sounds = {"cave1", "cave2", "cave3", "cave4", "cave5"}
 
 local listeners = {}
 
@@ -33,11 +32,11 @@ local function pick_sound()
 		end
 
 		local chosen_sound = sounds[index]
-		core.log("action", "[cavesounds] Playing cave sound: " .. chosen_sound)
+		core.log("action", "[vl_cavesounds] Playing cave sound: " .. chosen_sound)
 
 		return chosen_sound
 	else
-		core.log("warning", "[cavesounds] No cave sounds found!")
+		core.log("warning", "[vl_cavesounds] No cave sounds found!")
 	end
 
 	return nil
@@ -47,7 +46,7 @@ local function play_sound(sound, player_name)
 
 	local spec = {
 		name  = sound,
-		gain  = 0.3,
+		gain  = 0.4,
 		pitch = 1.0,
 	}
 	local parameters = {
@@ -63,7 +62,7 @@ local function play()
 	local time = core.get_timeofday()
 
 	for _, player1 in pairs(core.get_connected_players()) do
-		if not player1:get_meta():get("cavesounds:disable") then
+		if not player1:get_meta():get("vl_cavesounds:disable") then
 
 			local player1_name = player1:get_player_name()
 			local pos1         = player1:get_pos()
@@ -77,15 +76,17 @@ local function play()
 					local light_lvl = core.get_node_light(pos1)
 
 					-- Mapping light level to extra fear
-					new_fear = fear - slope * (light_lvl - max_light_lvl)
+					new_fear = fear + slope * (max_light_lvl - light_lvl)
 
-					if (new_fear < 0) then
+					-- Nothing special, just set new fear
+					if (new_fear < seconds_in_pitch_black_before_sound) then
+
+						listeners[player1_name].fear = new_fear
+
+					-- Play cave sound
+					else
 						reset_fear(player1_name)
 
-					elseif (new_fear >= seconds_in_pitch_black_before_sound) then
-						reset_fear(player1_name)
-
-						-- Play cave sound
 						local sound = pick_sound()
 						if sound then
 
@@ -93,11 +94,12 @@ local function play()
 
 							-- Also play the same sound for nearby players
 							for _, player2 in pairs(core.get_connected_players()) do
+								repeat
 								if player2 == player1 then
-									goto continue2
+									break
 								end
 
-								if not player2:get_meta():get("cavesounds:disable") then
+								if not player2:get_meta():get("vl_cavesounds:disable") then
 
 									local player2_name = player2:get_player_name()
 									local pos2         = player2:get_pos()
@@ -107,17 +109,11 @@ local function play()
 										play_sound(sound, player2_name)
 									end
 								end
-
-								::continue2::
+								until true
 							end
 						end
-					else
-						-- Nothing special, just set new fear
-						listeners[player1_name].fear = new_fear
 					end
 				end
-
-				--core.chat_send_player(player1_name, listeners[player1_name].fear)
 			end
 		end
 	end
@@ -126,7 +122,7 @@ local function play()
 end
 
 if cave_sounds_enabled then
-	core.log("action", "[cavesounds] Cave sounds are activated")
+	core.log("action", "[vl_cavesounds] Cave sounds are activated")
 	core.after(5, play)
 
 	core.register_on_joinplayer(function(player, last_login)
@@ -141,7 +137,7 @@ if cave_sounds_enabled then
 		reset_fear(player:get_player_name())
 	end)
 else
-	core.log("action", "[cavesounds] Cave sounds are deactivated")
+	core.log("action", "[vl_cavesounds] Cave sounds are deactivated")
 end
 
 core.register_chatcommand("cavesounds", {
@@ -163,7 +159,7 @@ core.register_chatcommand("cavesounds", {
 
 		if not playername or playername == "" or sender_name == playername then
 			target_player = sender
-			playername =sender_name
+			playername = sender_name
 		elseif not core.check_player_privs(sender, "debug") then -- Self-use handled above
 			core.chat_send_player(sender_name, S("You need the debug privilege in order to turn cave sounds on or off for somebody else!"))
 			return
@@ -180,18 +176,18 @@ core.register_chatcommand("cavesounds", {
 		local display_new_state = "unknown" -- Should never be displayed -> no translation
 
 		if action == "invert" then
-			if not meta:get("cavesounds:disable") then
-				meta:set_int("cavesounds:disable", 1)
+			if not meta:get("vl_cavesounds:disable") then
+				meta:set_int("vl_cavesounds:disable", 1)
 				display_new_state = S("off")
 			else
-				meta:set_string("cavesounds:disable", "") -- This deletes the meta value!
+				meta:set_string("vl_cavesounds:disable", "") -- This deletes the meta value!
 				display_new_state = S("on")
 			end
 		elseif action == "on" then
-			meta:set_string("cavesounds:disable", "") -- Delete
+			meta:set_string("vl_cavesounds:disable", "") -- Delete
 			display_new_state = S("on")
 		else
-			meta:set_int("cavesounds:disable", 1)
+			meta:set_int("vl_cavesounds:disable", 1)
 			display_new_state = S("off")
 		end
 
