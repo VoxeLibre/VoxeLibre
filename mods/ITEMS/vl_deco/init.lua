f41356d7ab3d296815b300ebfb3a2789e6be4473
TANGaming <tangaming123456@outlook.com>
2025-12-26 20:08:36 +0700

    Add more definitions for decorative star
    - group
    - hardness
    - sounds

--- mods/ITEMS/vl_deco/init.lua
+++ mods/ITEMS/vl_deco/init.lua
@@ -2,6 +2,8 @@ local modname = core.get_current_modname()
 
 local S = core.get_translator(modname)
 
+local mod_sounds = core.get_modpath("mcl_sounds")
+
 core.register_node("vl_deco:star", {
 	description = S("Decorative Star"),
 	drawtype = "plantlike",
@@ -10,6 +12,10 @@ core.register_node("vl_deco:star", {
 	wield_image = "vl_deco_star.png",
 	paramtype = "light",
 	light_source = core.LIGHT_MAX,
+	groups = { deco_block=1 },
+	sounds = mod_sounds and mcl_sounds.node_sound_glass_defaults() or {},
+	_mcl_hardness = 0.3,
+	_mcl_blast_resistance = 0.3,
 })
 
 core.register_craft({
