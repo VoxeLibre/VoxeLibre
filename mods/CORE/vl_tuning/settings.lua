fea9ceedf7c374668366494aab18e949391ea9fe
KutayX7 <kutayx7@noreply.git.minetest.land>
2026-01-30 08:58:30 +0100

    Adds vl_creative_instant_kill to vl_tuning
    Adds `vl_creative_instant_kill` to vl_tuning as `gamerule:creativeInstantKill`.

--- mods/CORE/vl_tuning/settings.lua
+++ mods/CORE/vl_tuning/settings.lua
@@ -2,6 +2,16 @@ local modname = minetest.get_current_modname()
 local S = minetest.get_translator(modname)
 local mod = vl_tuning
 
+local creative_instant_kill
+vl_tuning.setting("gamerule:creativeInstantKill", "bool", {
+	description = S("Allows creative players to kill mobs with only a single punch."),
+	default = core.settings:get_bool("vl_creative_instant_kill", true),
+	set = function(val)
+		creative_instant_kill = val
+		core.settings:set_bool("vl_creative_instant_kill", val)
+	end,
+	get = function() return creative_instant_kill end,
+})
 local keep_inventory
 vl_tuning.setting("gamerule:keepInventory", "bool", {
 	default = minetest.settings:get_bool("mcl_keepInventory", false),
