128a4efe32b7c14925ae5d4d225ca171f215ebd2
Andro <andro@noreply.git.minetest.land>
2026-03-12 20:35:50 -0500

    Fix mounted mobs walking on water

--- mods/ENTITIES/mobs_mc/horse.lua
+++ mods/ENTITIES/mobs_mc/horse.lua
@@ -223,7 +223,7 @@ local horse = {
 		-- if driver present and horse has a saddle allow control of horse
 		if self.driver and self._saddle then
 
-			mcl_mobs.drive(self, "walk", "stand", false, dtime)
+			mcl_mobs.drive(self, "walk", "stand", dtime, { sink_in_water = true })
 
 			return false -- skip rest of mob functions
 		end
