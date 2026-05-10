bb67a02e6b94a187af7f2bb3b5196a7ac101833e
CodingMantis <coding.mantis.ua@pm.me>
2026-05-11 00:20:30 +0300

    immediate_drop_on_dig

--- mods/ENTITIES/mcl_dripping/init.lua
+++ mods/ENTITIES/mcl_dripping/init.lua
@@ -11,9 +11,7 @@ mcl_dripping = {}
 ---@return boolean
 local function is_eligible_for_drip(pos, liquid)
 	local below = core.get_node(vector.offset(pos, 0, -1, 0)).name
-	if below == "ignore"
-			or mcl_util.is_solid_block(below)
-			or core.get_item_group(below, "slab_top") ~= 0 then
+	if below == "ignore" or mcl_util.is_solid_block(below) then
 		return false
 	end
 
@@ -27,13 +25,13 @@ end
 ---@param sound SimpleSoundSpec
 ---@param interval integer
 ---@param texture string
-local function make_drop(pos, liquid, sound, interval, texture)
+local function make_drop(pos, liquid, sound, interval, texture, immediate)
 	local pt = {
 		velocity = vector.zero(),
 		collision_removal = false,
 	}
 
-	local t = math.random() + math.random(1, interval)
+	local t = math.random() + (immediate and 0 or math.random(1, interval))
 
 	core.after(t, function()
 		if not is_eligible_for_drip(pos, liquid) then return end
@@ -94,22 +92,39 @@ function mcl_dripping.register_drop(def)
 	})
 end
 
-mcl_dripping.register_drop({
-	liquid   = "water",
-	texture  = "mcl_core_water_source_animation.png",
-	light    = 1,
-	nodes    = { "group:opaque", "group:leaves" },
-	sound    = "drippingwater_drip",
-	interval = 60.3,
-	chance   = 10,
-})
+local drop_defs = {
+	water = {
+		liquid   = "water",
+		texture  = "mcl_core_water_source_animation.png",
+		light    = 1,
+		nodes    = { "group:opaque", "group:leaves" },
+		sound    = "drippingwater_drip",
+		interval = 60.3,
+		chance   = 10,
+	},
+	lava = {
+		liquid   = "lava",
+		texture  = "mcl_core_lava_source_animation.png",
+		light    = math.max(7, core.registered_nodes["mcl_core:lava_source"].light_source - 3),
+		nodes    = { "group:opaque" },
+		sound    = "drippingwater_lavadrip",
+		interval = 110.1,
+		chance   = 10,
+	},
+}
 
-mcl_dripping.register_drop({
-	liquid   = "lava",
-	texture  = "mcl_core_lava_source_animation.png",
-	light    = math.max(7, core.registered_nodes["mcl_core:lava_source"].light_source - 3),
-	nodes    = { "group:opaque" },
-	sound    = "drippingwater_lavadrip",
-	interval = 110.1,
-	chance   = 10,
-})
+mcl_dripping.register_drop(drop_defs.water)
+mcl_dripping.register_drop(drop_defs.lava)
+
+core.register_on_dignode(function(pos)
+	local ceiling_pos = vector.offset(pos, 0, 1, 0)
+	if not mcl_util.is_solid_block(core.get_node(ceiling_pos).name) then return end
+
+	local liquid = core.get_node(vector.offset(pos, 0, 2, 0)).name
+	for liquid_name, def in pairs(drop_defs) do
+		if core.get_item_group(liquid, liquid_name) ~= 0 and is_eligible_for_drip(ceiling_pos, liquid_name) then
+			make_drop(ceiling_pos, liquid_name, def.sound, 0, def.texture, true)
+			return
+		end
+	end
+end)
