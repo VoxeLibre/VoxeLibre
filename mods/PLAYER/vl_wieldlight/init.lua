31d2346cd38edaf3d5b0b26c6ab23d52dba69315
the-real-herowl <the-real-herowl@noreply.git.minetest.land>
2026-07-05 18:21:12 +0200

    Removed testing code

--- mods/PLAYER/vl_wieldlight/init.lua
+++ mods/PLAYER/vl_wieldlight/init.lua
@@ -3,7 +3,6 @@ local players = {}
 
 local function wieldedlight(name)
 	if not name then return end
-	local start = core.get_us_time()
 	local player = core.get_player_by_name(name)
 	local pos = player:get_pos()
 	local ls = player:get_wielded_item():get_definition().light_source
@@ -13,8 +12,6 @@ local function wieldedlight(name)
 		core.fix_light(old_p[1], old_p[2])
 		old_p = nil
 	end
-	local t2 = core.get_us_time()
-	local t_loop = 0
 	if ls and ls > 0 then
 		local p1 = vector.offset(pos, -ls, -ls, -ls)
 		local p2 = vector.offset(pos, ls, ls, ls)
@@ -34,40 +31,12 @@ local function wieldedlight(name)
 			ldt[i] = d + n*16
 			i = it()
 		end
-		t_loop = (core.get_us_time() - t2)/1e6
 		lvm:set_light_data(ldt)
 		lvm:write_to_map(false)
 		lvm:close()
 		players[name] = {p1, p2}
 	end
-	return (core.get_us_time() - start)/1e6, (t2-start)/1e6, t_loop
 end
-
-core.register_chatcommand("wieldedlight", {
-	description = "Test",
-	func = function(name)
-		local counter = 0
-		local times_sum = 0
-		local t2_sum = 0
-		local t3_sum = 0
-		local f = function() end
-		local fn = function()
-			counter = counter + 1
-			local time, t2, t3 = wieldedlight(name)
-			times_sum = times_sum + time
-			t2_sum = t2_sum + t2
-			t3_sum = t3_sum + t3
-			core.log(string.format("took on average: %.5f, part1 average: %.5f, %% in part1: %.2f%%, part2 average: %.5f, %% in part2: %.2f%%", times_sum/counter, t2_sum/counter, 100*t2_sum/times_sum, t3_sum/counter, 100*t3_sum/times_sum))
-			if counter < 30 then
-				core.after(1, f)
-			end
-		end
-		f = function() fn() end
-		fn()
-
-		return true, "done"
-	end,
-})
 
 local p_queue = {}
 local i_queue = 1
