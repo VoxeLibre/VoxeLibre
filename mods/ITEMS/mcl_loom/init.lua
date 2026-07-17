cef0d5f822adfcec13513bf7b0211f2c5f1f4da8
AFCMS <afcm.contact@gmail.com>
2026-07-17 10:50:33 +0200

    Switch from node inventories to player inventories

--- mods/ITEMS/mcl_loom/init.lua
+++ mods/ITEMS/mcl_loom/init.lua
@@ -2,6 +2,8 @@ local S = minetest.get_translator(minetest.get_current_modname())
 local C = minetest.colorize
 local F = minetest.formspec_escape
 
+local formspec_name = "mcl_loom:loom"
+
 local pattern_names = {
 	"border",
 	"bricks",
@@ -67,18 +69,18 @@ local function add_layer(banner, pattern, color)
 	return banner
 end
 
-local function show_loom_formspec(pos, player)
-	local inv = minetest.get_meta(pos):get_inventory()
+local function show_loom_formspec(player)
+	local inv = player:get_inventory()
 
-	local banner = inv:get_stack("input", 1)
-	local dye = inv:get_stack("input", 2)
-	local pattern = inv:get_stack("input", 3)
+	local banner = inv:get_stack("loom_input", 1)
+	local dye = inv:get_stack("loom_input", 2)
+	local pattern = inv:get_stack("loom_input", 3)
 
 	local container_content = ""
 
 	if not banner:is_empty() and not dye:is_empty() then
 		if not pattern:is_empty() then
-			inv:set_stack("output", 1, add_layer(banner, pattern:get_name():split(":")[2]:split("_")[1], dye))
+			inv:set_stack("loom_output", 1, add_layer(banner, pattern:get_name():split(":")[2]:split("_")[1], dye))
 			local item = pattern:get_name():split(":")[2]:split("_")[1]
 			container_content = string.format("item_image[0,0;0.875,0.875;%s]", "mcl_banners:banner_preview_" .. item .. "_red")
 		else
@@ -86,7 +88,7 @@ local function show_loom_formspec(pos, player)
 		end
 	end
 
-	local output = inv:get_stack("output", 1)
+	local output = inv:get_stack("loom_output", 1)
 	local preview = mcl_banners.make_banner_texture(mcl_banners.color_reverse(output:get_name()), minetest.deserialize(output:get_meta():get_string("layers")) or {})
 
 	local banner_model = "model[9.55,0.7;1.4,2.3;keeper;amc_banner_hanging.b3d;" ..
@@ -103,18 +105,18 @@ local function show_loom_formspec(pos, player)
 		-- Banner input slot
 		mcl_formspec.get_itemslot_bg_v4(1, 1.5, 1, 1),
 		banner:is_empty() and mcl_formspec.get_itemslot_bg_v4(1, 1.5, 1, 1, 0, "mcl_loom_inventory_banner.png") or "",
-		"list[nodemeta:" .. pos.x .. "," .. pos.y .. "," .. pos.z .. ";input;1,1.5;1,1;]",
+		"list[current_player;loom_input;1,1.5;1,1;]",
 
 		-- Dye input slot
 		mcl_formspec.get_itemslot_bg_v4(2.25, 1.5, 1, 1),
 		dye:is_empty() and mcl_formspec.get_itemslot_bg_v4(2.25, 1.5, 1, 1, 0, "mcl_loom_inventory_dye.png") or "",
-		"list[nodemeta:" .. pos.x .. "," .. pos.y .. "," .. pos.z .. ";input;2.25,1.5;1,1;1]",
+		"list[current_player;loom_input;2.25,1.5;1,1;1]",
 
 		-- Pattern input slot
 		mcl_formspec.get_itemslot_bg_v4(1.625, 2.75, 1, 1),
 		pattern:is_empty() and mcl_formspec.get_itemslot_bg_v4(1.625, 2.75, 1, 1, 0, "mcl_loom_inventory_pattern.png") or
 		"",
-		"list[nodemeta:" .. pos.x .. "," .. pos.y .. "," .. pos.z .. ";input;1.625,2.75;1,1;2]",
+		"list[current_player;loom_input;1.625,2.75;1,1;2]",
 
 		-- Container background
 		"image[4.450,0.7;3.6,3.6;mcl_inventory_background9.png;2]",
@@ -134,7 +136,7 @@ local function show_loom_formspec(pos, player)
 
 		-- Output slot
 		mcl_formspec.get_itemslot_bg_v4(9.75, 3.1, 1, 1, 0.2),
-		"list[nodemeta:" .. pos.x .. "," .. pos.y .. "," .. pos.z .. ";output;9.75,3.1;1,1;]",
+		"list[current_player;loom_output;9.75,3.1;1,1;]",
 
 		-- Player inventory
 		"label[0.375,4.7;" .. F(C(mcl_formspec.label_color, S("Inventory"))) .. "]",
@@ -143,20 +145,125 @@ local function show_loom_formspec(pos, player)
 
 		mcl_formspec.get_itemslot_bg_v4(0.375, 9.05, 9, 1),
 		"list[current_player;main;0.375,9.05;9,1;]",
+
+		"listring[current_player;loom_output]",
+		"listring[current_player;main]",
+		"listring[current_player;loom_input]",
+		"listring[current_player;main]",
 	})
 
-	tt.reload_itemstack_description(inv:get_stack("output", 1))
-	minetest.show_formspec(player:get_player_name(), ("mcl_loom:%f_%f_%f"):format(pos.x, pos.y, pos.z), formspec)
+	tt.reload_itemstack_description(inv:get_stack("loom_output", 1))
+	minetest.show_formspec(player:get_player_name(), formspec_name, formspec)
 end
 
+minetest.register_on_joinplayer(function(player)
+	local inv = player:get_inventory()
+
+	inv:set_size("loom_input", 3)
+	inv:set_size("loom_output", 1)
+
+	-- The player might have items remaining in the slots from the previous join; this is likely
+	-- when the server has been shutdown and the server didn't clean up the player inventories.
+	mcl_util.move_player_list(player, "loom_input")
+	inv:set_list("loom_output", {})
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	mcl_util.move_player_list(player, "loom_input")
+	player:get_inventory():set_list("loom_output", {})
+end)
+
 minetest.register_on_player_receive_fields(function(player, formname, fields)
-	if not fields.quit and formname:find("mcl_loom") then
-		local p = formname:split(":")[2]:split("_")
-		local pos = vector.new(tonumber(p[1]), tonumber(p[2]), tonumber(p[3]))
-		local pattern; for k, _ in pairs(fields) do if k ~= "scroll" then pattern = k break end end; if not pattern then return end
-		local inv = minetest.get_meta(pos):get_inventory(); if not inv then return end
-		inv:set_stack("output", 1, add_layer(inv:get_stack("input", 1), pattern, inv:get_stack("input", 2)))
-		show_loom_formspec(pos, player)
+	if formname ~= formspec_name then return end
+
+	local inv = player:get_inventory()
+	if fields.quit then
+		mcl_util.move_player_list(player, "loom_input")
+		inv:set_list("loom_output", {})
+		return
+	end
+
+	for _, pattern in ipairs(pattern_names) do
+		if fields[pattern] then
+			inv:set_stack("loom_output", 1,
+				add_layer(inv:get_stack("loom_input", 1), pattern, inv:get_stack("loom_input", 2)))
+			show_loom_formspec(player)
+			return
+		end
+	end
+end)
+
+local function allow_loom_input(index, stack, count)
+	local name = stack:get_name()
+	if (index == 1 and not name:find("banner_item"))
+			or (index == 2 and not name:find("mcl_dye"))
+			or (index == 3 and not name:find("pattern")) then
+		return 0
+	end
+	return count
+end
+
+minetest.register_allow_player_inventory_action(function(_, action, inventory, inventory_info)
+	if action == "move" then
+		if inventory_info.to_list == "loom_output" then
+			return 0
+		end
+
+		if (inventory_info.from_list == "loom_input" or inventory_info.from_list == "loom_output")
+				and inventory_info.to_list == "loom_input" then
+			return 0
+		end
+
+		if inventory_info.to_list == "loom_input" then
+			local stack = inventory:get_stack(inventory_info.from_list, inventory_info.from_index)
+			return allow_loom_input(inventory_info.to_index, stack, inventory_info.count)
+		end
+	elseif action == "put" then
+		if inventory_info.listname == "loom_output" then
+			return 0
+		end
+
+		if inventory_info.listname == "loom_input" then
+			return allow_loom_input(inventory_info.index, inventory_info.stack,
+				inventory_info.stack:get_count())
+		end
+	end
+end)
+
+local function update_loom_slots(player)
+	player:get_inventory():set_stack("loom_output", 1, nil)
+	show_loom_formspec(player)
+end
+
+local function remove_from_loom_inputs(inventory, count)
+	local banner = inventory:get_stack("loom_input", 1)
+	banner:take_item(count)
+	inventory:set_stack("loom_input", 1, banner)
+
+	local dye = inventory:get_stack("loom_input", 2)
+	dye:take_item(count)
+	inventory:set_stack("loom_input", 2, dye)
+end
+
+minetest.register_on_player_inventory_action(function(player, action, inventory, inventory_info)
+	if action == "move" then
+		if inventory_info.from_list == "loom_output" then
+			remove_from_loom_inputs(inventory, inventory_info.count)
+			show_loom_formspec(player)
+		elseif inventory_info.to_list == "loom_input" or inventory_info.from_list == "loom_input" then
+			update_loom_slots(player)
+		end
+	elseif action == "put" then
+		if inventory_info.listname == "loom_input" then
+			update_loom_slots(player)
+		end
+	elseif action == "take" then
+		if inventory_info.listname == "loom_output" then
+			remove_from_loom_inputs(inventory, inventory_info.stack:get_count())
+			show_loom_formspec(player)
+		elseif inventory_info.listname == "loom_input" then
+			update_loom_slots(player)
+		end
 	end
 end)
 
@@ -173,57 +280,8 @@ minetest.register_node("mcl_loom:loom", {
 	groups = { axey = 2, handy = 1, deco_block = 1, material_wood = 1, flammable = 1 },
 	_mcl_blast_resistance = 2.5,
 	_mcl_hardness = 2.5,
-	on_construct = function(pos)
-		local inv = minetest.get_meta(pos):get_inventory()
-		inv:set_size("input", 3)
-		inv:set_size("output", 1)
-	end,
-	allow_metadata_inventory_put = function(pos, listname, index, stack, player)
-		if minetest.is_protected(pos, player:get_player_name()) or listname == "output" then
-			return 0
-		else
-			local name = stack:get_name()
-			if (index == 1 and not name:find("banner_item")) or (index == 2 and not name:find("mcl_dye")) or (index == 3 and not name:find("pattern")) then return 0 end
-			return stack:get_count()
-		end
-	end,
-	on_metadata_inventory_put = function(pos, _, _, _, player)
-		local inv = minetest.get_meta(pos):get_inventory()
-		if not inv:get_stack("input", 1):is_empty() and not inv:get_stack("input", 2):is_empty() then
-			show_loom_formspec(pos, player)
-		end
-	end,
-	on_metadata_inventory_take = function(pos, listname, index, stack, player)
-		local inv = minetest.get_meta(pos):get_inventory()
-		if listname == "output" then
-			local count = stack:get_count()
-			local first = inv:get_stack("input", 1); first:take_item(count); inv:set_stack("input", 1, first)
-			local second = inv:get_stack("input", 2); second:take_item(count); inv:set_stack("input", 2, second)
-		else
-			inv:set_stack("output", 1, "")
-		end
-		show_loom_formspec(pos, player)
-	end,
-	allow_metadata_inventory_move = function() return 0 end,
 	on_rightclick = function(pos, node, player, itemstack)
-		if not player:get_player_control().sneak then show_loom_formspec(pos, player) end
-	end,
-	after_dig_node = function(pos, _, oldmetadata, _)
-		local meta = minetest.get_meta(pos)
-		local meta2 = meta:to_table()
-		meta:from_table(oldmetadata)
-		local inv = meta:get_inventory()
-		for i = 1, inv:get_size("input") do
-			local stack = inv:get_stack("input", i)
-			if not stack:is_empty() then
-				minetest.add_item(vector.offset(pos,
-					math.random(0, 10) / 10 - 0.5,
-					0,
-					math.random(0, 10) / 10 - 0.5
-				), stack)
-			end
-		end
-		meta:from_table(meta2)
+		if not player:get_player_control().sneak then show_loom_formspec(player) end
 	end,
 })
 
